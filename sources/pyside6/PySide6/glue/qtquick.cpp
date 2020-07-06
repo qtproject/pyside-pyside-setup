054feeeb062c3633cc51b1cc7c214cd0ee389beb
Cristian Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2020-07-06 15:47:49 +0200

    PySide6: Add vertexDataAsPoint2D()/setVertexDataAsPoint2D()
    vertexDataAsPoint2D returns a pointer (const/non-const for
    modification) to a series of Point2D within a geometry, but since in
    Python we don't have access to it, this change replaces that signature
    for one that returns a list of Point2D elements.
    
    For Pythonicity, also add a setter setVertexDataAsPoint2D() to enable
    modification.
    
    Fixes: PYSIDE-1345
    Change-Id: I6aba2a097022637c398ad94c17f342f13b4da449
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/pyside6/PySide6/glue/qtquick.cpp
+++ sources/pyside6/PySide6/glue/qtquick.cpp
@@ -4,3 +4,24 @@
 // @snippet qtquick
 PySide::initQuickSupport(module);
 // @snippet qtquick
+
+// @snippet qsgeometry-vertexdataaspoint2d
+auto *points = %CPPSELF->vertexDataAsPoint2D();
+const Py_ssize_t vertexCount = %CPPSELF->vertexCount();
+%PYARG_0 = PyList_New(vertexCount);
+for (Py_ssize_t i = 0; i < vertexCount; ++i) {
+    QSGGeometry::Point2D p = points[i];
+    PyList_SET_ITEM(%PYARG_0, i, %CONVERTTOPYTHON[QSGGeometry::Point2D](p));
+}
+// @snippet qsgeometry-vertexdataaspoint2d
+
+// @snippet qsgeometry-setvertexdataaspoint2d
+const qsizetype vertexCount = %CPPSELF->vertexCount();
+if (vertexCount != %1.size()) {
+    PyErr_SetString(PyExc_RuntimeError, "size mismatch");
+    return {};
+}
+
+QSGGeometry::Point2D *points = %CPPSELF->vertexDataAsPoint2D();
+std::copy(%1.cbegin(), %1.cend(), points);
+// @snippet qsgeometry-setvertexdataaspoint2d
