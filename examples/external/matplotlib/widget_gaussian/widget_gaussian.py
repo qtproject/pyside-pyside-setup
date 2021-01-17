cf82fcabfd5b7d4168c9088a2c3f0236231f68ab
Sacha Schutz <sacha@labsquare.org>
2021-01-17 19:36:08 +0100

    Add new matplotlib example and rearrange dirs
    I add a widget showing a 2D gaussian with 2 inputs to adjust mu and sigma
    
    Pick-to: 6.0
    Task-number: PYSIDE-841
    Change-Id: I602b07943ebeb007332bc77c4372ef5a1db20422
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

---
+++ examples/external/matplotlib/widget_gaussian/widget_gaussian.py
@@ -0,0 +1,112 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+import sys
+
+import numpy as np
+from scipy.stats import norm
+from matplotlib import pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QDoubleSpinBox,
+    QVBoxLayout,
+    QHBoxLayout,
+)
+
+
+"""This example implements the interaction between Qt Widgets and a 2D
+matplotlib plot showing a gaussian curve with scipy"""
+
+
+class PlotWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        #  create widgets
+        self.view = FigureCanvas(Figure(figsize=(5, 3)))
+        self.axes = self.view.figure.subplots()
+        self.toolbar = NavigationToolbar2QT(self.view, self)
+        self.mu_input = QDoubleSpinBox()
+        self.std_input = QDoubleSpinBox()
+        self.mu_input.setPrefix("μ: ")
+        self.std_input.setPrefix("σ: ")
+        self.std_input.setValue(10)
+
+        #  Create layout
+        input_layout = QHBoxLayout()
+        input_layout.addWidget(self.mu_input)
+        input_layout.addWidget(self.std_input)
+        vlayout = QVBoxLayout()
+        vlayout.addWidget(self.toolbar)
+        vlayout.addWidget(self.view)
+        vlayout.addLayout(input_layout)
+        self.setLayout(vlayout)
+
+        # connect inputs with on_change method
+        self.mu_input.valueChanged.connect(self.on_change)
+        self.std_input.valueChanged.connect(self.on_change)
+
+        self.on_change()
+
+    @Slot()
+    def on_change(self):
+        """ Update the plot with the current input values """
+        mu = self.mu_input.value()
+        std = self.std_input.value()
+
+        x = np.linspace(-100, 100)
+        y = norm.pdf(x, mu, std)
+
+        self.axes.clear()
+        self.axes.plot(x, y)
+        self.view.draw()
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    w = PlotWidget()
+    w.show()
+    sys.exit(app.exec_())
