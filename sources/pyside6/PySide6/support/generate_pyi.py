71a0e10ba935a2788da4962e0f06880e3d3d0d5d
Christian Tismer <tismer@stackless.com>
2021-03-20 19:24:38 +0100

    Turn generate_pyi into a general pyi_generator tool, preparation
    generate_pyi is now split into a pyi_generator in signature.lib
    and the remaining stub in generate_pyi.py .
    
    This is the first step in the transformation.
    
    Pick-to: 6.1
    Task-number: PYSIDE-1415
    Change-Id: I7b9075d3f6d8d2b8f8efa4591234f89d4bbe3e46
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/pyside6/PySide6/support/generate_pyi.py
+++ sources/pyside6/PySide6/support/generate_pyi.py
@@ -46,221 +46,21 @@ This script generates the .pyi files for all PySide modules.
 
 import argparse
 import inspect
-import io
 import logging
 import os
-import re
 import sys
 import typing
 
-from contextlib import contextmanager
 from pathlib import Path
-from textwrap import dedent
-
-# Make sure not to get .pyc in Python2.
-sourcepath = Path(__file__).parent.resolve() / (Path(__file__).stem + ".py")
 
 # Can we use forward references?
 USE_PEP563 = sys.version_info[:2] >= (3, 7)
 
-indent = " " * 4
-is_py3 = sys.version_info[0] == 3
 is_ci = os.environ.get("QTEST_ENVIRONMENT", "") == "ci"
 is_debug = is_ci or os.environ.get("QTEST_ENVIRONMENT")
 
 logging.basicConfig(level=logging.DEBUG if is_debug else logging.INFO)
 logger = logging.getLogger("generate_pyi")
-
-
-class Writer(object):
-    def __init__(self, outfile):
-        self.outfile = outfile
-        self.history = [True, True]
-
-    def print(self, *args, **kw):
-        # controlling too much blank lines
-        if self.outfile:
-            if args == () or args == ("",):
-                # Python 2.7 glitch: Empty tuples have wrong encoding.
-                # But we use that to skip too many blank lines:
-                if self.history[-2:] == [True, True]:
-                    return
-                print("", file=self.outfile, **kw)
-                self.history.append(True)
-            else:
-                print(*args, file=self.outfile, **kw)
-                self.history.append(False)
-
-
-class Formatter(Writer):
-    """
-    Formatter is formatting the signature listing of an enumerator.
-
-    It is written as context managers in order to avoid many callbacks.
-    The separation in formatter and enumerator is done to keep the
-    unrelated tasks of enumeration and formatting apart.
-    """
-    def __init__(self, *args):
-        Writer.__init__(self, *args)
-        # patching __repr__ to disable the __repr__ of typing.TypeVar:
-        """
-            def __repr__(self):
-                if self.__covariant__:
-                    prefix = '+'
-                elif self.__contravariant__:
-                    prefix = '-'
-                else:
-                    prefix = '~'
-                return prefix + self.__name__
-        """
-        def _typevar__repr__(self):
-            return "typing." + self.__name__
-        typing.TypeVar.__repr__ = _typevar__repr__
-
-        # Adding a pattern to substitute "Union[T, NoneType]" by "Optional[T]"
-        # I tried hard to replace typing.Optional by a simple override, but
-        # this became _way_ too much.
-        # See also the comment in layout.py .
-        brace_pat = build_brace_pattern(3, ",")
-        pattern = fr"\b Union \s* \[ \s* {brace_pat} \s*, \s* NoneType \s* \]"
-        replace = r"Optional[\1]"
-        optional_searcher = re.compile(pattern, flags=re.VERBOSE)
-        def optional_replacer(source):
-            return optional_searcher.sub(replace, str(source))
-        self.optional_replacer = optional_replacer
-        # self.level is maintained by enum_sig.py
-        # self.after_enum() is a one-shot set by enum_sig.py .
-        # self.is_method() is true for non-plain functions.
-
-    @contextmanager
-    def module(self, mod_name):
-        self.mod_name = mod_name
-        self.print("# Module", mod_name)
-        self.print("import PySide6")
-        self.print("import typing")
-        self.print("from PySide6.support.signature.mapping import (")
-        self.print("    Virtual, Missing, Invalid, Default, Instance)")
-        self.print()
-        self.print("class Object(object): pass")
-        self.print()
-        self.print("from shiboken6 import Shiboken")
-        self.print("Shiboken.Object = Object")
-        self.print()
-        # This line will be replaced by the missing imports postprocess.
-        self.print("IMPORTS")
-        yield
-
-    @contextmanager
-    def klass(self, class_name, class_str):
-        spaces = indent * self.level
-        while "." in class_name:
-            class_name = class_name.split(".", 1)[-1]
-            class_str = class_str.split(".", 1)[-1]
-        self.print()
-        if self.level == 0:
-            self.print()
-        here = self.outfile.tell()
-        if self.have_body:
-            self.print(f"{spaces}class {class_str}:")
-        else:
-            self.print(f"{spaces}class {class_str}: ...")
-        yield
-
-    @contextmanager
-    def function(self, func_name, signature):
-        if self.after_enum() or func_name == "__init__":
-            self.print()
-        key = func_name
-        spaces = indent * self.level
-        if type(signature) == type([]):
-            for sig in signature:
-                self.print(f'{spaces}@typing.overload')
-                self._function(func_name, sig, spaces)
-        else:
-            self._function(func_name, signature, spaces)
-        if func_name == "__init__":
-            self.print()
-        yield key
-
-    def _function(self, func_name, signature, spaces):
-        if self.is_method() and "self" not in tuple(signature.parameters.keys()):
-            self.print(f'{spaces}@staticmethod')
-        signature = self.optional_replacer(signature)
-        self.print(f'{spaces}def {func_name}{signature}: ...')
-
-    @contextmanager
-    def enum(self, class_name, enum_name, value):
-        spaces = indent * self.level
-        hexval = hex(value)
-        self.print(f"{spaces}{enum_name:25}: {class_name} = ... # {hexval}")
-        yield
-
-
-def get_license_text():
-    with io.open(sourcepath) as f:
-        lines = f.readlines()
-        license_line = next((lno for lno, line in enumerate(lines)
-                             if "$QT_END_LICENSE$" in line))
-    return "".join(lines[:license_line + 3])
-
-
-def find_imports(text):
-    return [imp for imp in PySide6.__all__ if imp + "." in text]
-
-
-def generate_pyi(import_name, outpath, options):
-    """
-    Generates a .pyi file.
-    """
-    plainname = import_name.split(".")[-1]
-    outfilepath = Path(outpath) / (plainname + ".pyi")
-    top = __import__(import_name)
-    obj = getattr(top, plainname)
-    if not getattr(obj, "__file__", None) or Path(obj.__file__).is_dir():
-        raise ModuleNotFoundError(f"We do not accept a namespace as module {plainname}")
-    module = sys.modules[import_name]
-
-    outfile = io.StringIO()
-    fmt = Formatter(outfile)
-    fmt.print(get_license_text())  # which has encoding, already
-    need_imports = not USE_PEP563
-    if USE_PEP563:
-        fmt.print("from __future__ import annotations")
-        fmt.print()
-    fmt.print(dedent(f'''\
-        """
-        This file contains the exact signatures for all functions in module
-        {import_name}, except for defaults which are replaced by "...".
-        """
-        '''))
-    HintingEnumerator(fmt).module(import_name)
-    fmt.print()
-    fmt.print("# eof")
-    # Postprocess: resolve the imports
-    with open(outfilepath, "w") as realfile:
-        wr = Writer(realfile)
-        outfile.seek(0)
-        while True:
-            line = outfile.readline()
-            if not line:
-                break
-            line = line.rstrip()
-            # we remove the IMPORTS marker and insert imports if needed
-            if line == "IMPORTS":
-                if need_imports:
-                    for mod_name in find_imports(outfile.getvalue()):
-                        imp = "PySide6." + mod_name
-                        if imp != import_name:
-                            wr.print("import " + imp)
-                wr.print("import " + import_name)
-                wr.print()
-                wr.print()
-            else:
-                wr.print(line)
-    logger.info(f"Generated: {outfilepath}")
-    if is_py3 and (options.check or is_ci):
-        # Python 3: We can check the file directly if the syntax is ok.
-        subprocess.check_output([sys.executable, outfilepath])
 
 
 def generate_all_pyi(outpath, options):
@@ -278,6 +78,7 @@ def generate_all_pyi(outpath, options):
     import PySide6
     from PySide6.support.signature.lib.enum_sig import HintingEnumerator
     from PySide6.support.signature.lib.tool import build_brace_pattern
+    from PySide6.support.signature.lib.pyi_generator import generate_pyi
 
     # propagate USE_PEP563 to the mapping module.
     # Perhaps this can be automated?
@@ -317,5 +118,6 @@ if __name__ == "__main__":
     if outpath and not Path(outpath).exists():
         os.makedirs(outpath)
         logger.info(f"+++ Created path {outpath}")
+    options._pyside_call = True
     generate_all_pyi(outpath, options=options)
 # eof
