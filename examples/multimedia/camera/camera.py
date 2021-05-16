bb1887bcc6692f7dfece87fe3df42226d4b83008
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-05-16 10:03:31 +0200

    Fix the multimedia examples to work
    - Port to
      qtmultimedia/9a4822037def3b9d48abea8bbfd7ea20fd19849b (wip/qt6).
    - Add Slot decorators
    - Rename according to snake case conventions
    - Connect error signals of player and camera
    - Use theme icons in player
    
    Task-number: PYSIDE-1112
    Task-number: PYSIDE-1482
    Change-Id: Ib79614e56b2b1ad7ea6cd0406efa1d91ce1abdb1
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit ec71d250cd11b1bdf7eae9ddb55b79bd08360039)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/multimedia/camera/camera.py
+++ examples/multimedia/camera/camera.py
@@ -1,7 +1,7 @@
 
 #############################################################################
 ##
-## Copyright (C) 2017 The Qt Company Ltd.
+## Copyright (C) 2021 The Qt Company Ltd.
 ## Contact: http://www.qt.io/licensing/
 ##
 ## This file is part of the Qt for Python examples of the Qt Toolkit.
@@ -41,104 +41,131 @@
 
 """PySide6 Multimedia Camera Example"""
 
-import os, sys
-from PySide6.QtCore import QDate, QDir, QStandardPaths, Qt, QUrl
+import os
+import sys
+from PySide6.QtCore import QDate, QDir, QStandardPaths, Qt, QUrl, Slot
 from PySide6.QtGui import QAction, QGuiApplication, QDesktopServices, QIcon
 from PySide6.QtGui import QImage, QPixmap
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel,
     QMainWindow, QPushButton, QTabWidget, QToolBar, QVBoxLayout, QWidget)
-from PySide6.QtMultimedia import QCamera, QCameraImageCapture, QCameraInfo
-from PySide6.QtMultimediaWidgets import QCameraViewfinder
+from PySide6.QtMultimedia import (QCamera, QCameraImageCapture,
+                                  QCameraInfo, QMediaCaptureSession,
+                                  QMediaDevices)
+from PySide6.QtMultimediaWidgets import QVideoWidget
 
 
 class ImageView(QWidget):
     def __init__(self, previewImage, fileName):
         super().__init__()
 
-        self.fileName = fileName
+        self._file_name = fileName
 
-        mainLayout = QVBoxLayout(self)
-        self.imageLabel = QLabel()
-        self.imageLabel.setPixmap(QPixmap.fromImage(previewImage))
-        mainLayout.addWidget(self.imageLabel)
+        main_layout = QVBoxLayout(self)
+        self._image_label = QLabel()
+        self._image_label.setPixmap(QPixmap.fromImage(previewImage))
+        main_layout.addWidget(self._image_label)
 
-        topLayout = QHBoxLayout()
-        self.fileNameLabel = QLabel(QDir.toNativeSeparators(fileName))
-        self.fileNameLabel.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        top_layout = QHBoxLayout()
+        self._file_name_label = QLabel(QDir.toNativeSeparators(fileName))
+        self._file_name_label.setTextInteractionFlags(Qt.TextBrowserInteraction)
 
-        topLayout.addWidget(self.fileNameLabel)
-        topLayout.addStretch()
-        copyButton = QPushButton("Copy")
-        copyButton.setToolTip("Copy file name to clipboard")
-        topLayout.addWidget(copyButton)
-        copyButton.clicked.connect(self.copy)
-        launchButton = QPushButton("Launch")
-        launchButton.setToolTip("Launch image viewer")
-        topLayout.addWidget(launchButton)
-        launchButton.clicked.connect(self.launch)
-        mainLayout.addLayout(topLayout)
+        top_layout.addWidget(self._file_name_label)
+        top_layout.addStretch()
+        copy_button = QPushButton("Copy")
+        copy_button.setToolTip("Copy file name to clipboard")
+        top_layout.addWidget(copy_button)
+        copy_button.clicked.connect(self.copy)
+        launch_button = QPushButton("Launch")
+        launch_button.setToolTip("Launch image viewer")
+        top_layout.addWidget(launch_button)
+        launch_button.clicked.connect(self.launch)
+        main_layout.addLayout(top_layout)
 
+    @Slot()
     def copy(self):
-        QGuiApplication.clipboard().setText(self.fileNameLabel.text())
+        QGuiApplication.clipboard().setText(self._file_name_label.text())
 
+    @Slot()
     def launch(self):
-        QDesktopServices.openUrl(QUrl.fromLocalFile(self.fileName))
+        QDesktopServices.openUrl(QUrl.fromLocalFile(self._file_name))
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.cameraInfo = QCameraInfo.defaultCamera()
-        self.camera = QCamera(self.cameraInfo)
-        self.camera.setCaptureMode(QCamera.CaptureStillImage)
-        self.imageCapture = QCameraImageCapture(self.camera)
-        self.imageCapture.imageCaptured.connect(self.imageCaptured)
-        self.imageCapture.imageSaved.connect(self.imageSaved)
-        self.currentPreview = QImage()
+        self._capture_session = None
+        self._camera = None
+        self._camera_info = None
+        self._image_capture = None
 
-        toolBar = QToolBar()
-        self.addToolBar(toolBar)
+        available_cameras = QMediaDevices.videoInputs()
+        if available_cameras:
+            self._camera_info = available_cameras[0]
+            self._camera = QCamera(self._camera_info)
+            self._camera.errorOccurred.connect(self._camera_error)
+            self._image_capture = QCameraImageCapture(self._camera)
+            self._image_capture.imageCaptured.connect(self.image_captured)
+            self._image_capture.imageSaved.connect(self.image_saved)
+            self._image_capture.errorOccurred.connect(self._capture_error)
+            self._capture_session = QMediaCaptureSession()
+            self._capture_session.setCamera(self._camera)
+            self._capture_session.setImageCapture(self._image_capture)
 
-        fileMenu = self.menuBar().addMenu("&File")
-        shutterIcon = QIcon(os.path.join(os.path.dirname(__file__),
+        self._current_preview = QImage()
+
+        tool_bar = QToolBar()
+        self.addToolBar(tool_bar)
+
+        file_menu = self.menuBar().addMenu("&File")
+        shutter_icon = QIcon(os.path.join(os.path.dirname(__file__),
                             "shutter.svg"))
-        self.takePictureAction = QAction(shutterIcon, "&Take Picture", self,
-                                         shortcut="Ctrl+T",
-                                         triggered=self.takePicture)
-        self.takePictureAction.setToolTip("Take Picture")
-        fileMenu.addAction(self.takePictureAction)
-        toolBar.addAction(self.takePictureAction)
+        self._take_picture_action = QAction(shutter_icon, "&Take Picture", self,
+                                            shortcut="Ctrl+T",
+                                            triggered=self.take_picture)
+        self._take_picture_action.setToolTip("Take Picture")
+        file_menu.addAction(self._take_picture_action)
+        tool_bar.addAction(self._take_picture_action)
 
-        exitAction = QAction(QIcon.fromTheme("application-exit"), "E&xit",
-                             self, shortcut="Ctrl+Q", triggered=self.close)
-        fileMenu.addAction(exitAction)
+        exit_action = QAction(QIcon.fromTheme("application-exit"), "E&xit",
+                              self, shortcut="Ctrl+Q", triggered=self.close)
+        file_menu.addAction(exit_action)
 
-        aboutMenu = self.menuBar().addMenu("&About")
-        aboutQtAction = QAction("About &Qt", self, triggered=qApp.aboutQt)
-        aboutMenu.addAction(aboutQtAction)
+        about_menu = self.menuBar().addMenu("&About")
+        about_qt_action = QAction("About &Qt", self, triggered=qApp.aboutQt)
+        about_menu.addAction(about_qt_action)
 
-        self.tabWidget = QTabWidget()
-        self.setCentralWidget(self.tabWidget)
+        self._tab_widget = QTabWidget()
+        self.setCentralWidget(self._tab_widget)
 
-        self.cameraViewfinder = QCameraViewfinder()
-        self.camera.setViewfinder(self.cameraViewfinder)
-        self.tabWidget.addTab(self.cameraViewfinder, "Viewfinder")
+        self._camera_viewfinder = QVideoWidget()
+        self._tab_widget.addTab(self._camera_viewfinder, "Viewfinder")
 
-        if self.camera.status() != QCamera.UnavailableStatus:
-            name = self.cameraInfo.description()
+        if self._camera and self._camera.status() != QCamera.UnavailableStatus:
+            name = self._camera_info.description()
             self.setWindowTitle(f"PySide6 Camera Example ({name})")
-            self.statusBar().showMessage(f"Starting: '{name}'", 5000)
-            self.camera.start()
+            self.show_status_message(f"Starting: '{name}'")
+            self._capture_session.setVideoOutput(self._camera_viewfinder)
+            self._take_picture_action.setEnabled(self._image_capture.isReadyForCapture())
+            self._image_capture.readyForCaptureChanged.connect(self._take_picture_action.setEnabled)
+            self._camera.start()
         else:
             self.setWindowTitle("PySide6 Camera Example")
-            self.takePictureAction.setEnabled(False)
-            self.statusBar().showMessage("Camera unavailable", 5000)
+            self._take_picture_action.setEnabled(False)
+            self.show_status_message("Camera unavailable")
 
-    def nextImageFileName(self):
-        picturesLocation = QStandardPaths.writableLocation(QStandardPaths.PicturesLocation)
-        dateString = QDate.currentDate().toString("yyyyMMdd")
-        pattern = f"{picturesLocation}/pyside6_camera_{dateString}_{{:03d}}.jpg"
+    def show_status_message(self, message):
+        self.statusBar().showMessage(message, 5000)
+
+    def closeEvent(self, event):
+        if self._camera and self._camera.status() == QCamera.ActiveStatus:
+            self._camera.stop()
+        event.accept()
+
+    def next_image_file_name(self):
+        pictures_location = QStandardPaths.writableLocation(QStandardPaths.PicturesLocation)
+        date_string = QDate.currentDate().toString("yyyyMMdd")
+        pattern = f"{pictures_location}/pyside6_camera_{date_string}_{{:03d}}.jpg"
         n = 1
         while True:
             result = pattern.format(n)
@@ -147,26 +174,37 @@ class MainWindow(QMainWindow):
             n = n + 1
         return None
 
-    def takePicture(self):
-        self.currentPreview = QImage()
-        self.camera.searchAndLock()
-        self.imageCapture.capture(self.nextImageFileName())
-        self.camera.unlock()
+    @Slot()
+    def take_picture(self):
+        self._current_preview = QImage()
+        self._image_capture.captureToFile(self.next_image_file_name())
 
-    def imageCaptured(self, id, previewImage):
-        self.currentPreview = previewImage
+    @Slot(int, QImage)
+    def image_captured(self, id, previewImage):
+        self._current_preview = previewImage
 
-    def imageSaved(self, id, fileName):
-        index = self.tabWidget.count()
-        imageView = ImageView(self.currentPreview, fileName)
-        self.tabWidget.addTab(imageView, f"Capture #{index}")
-        self.tabWidget.setCurrentIndex(index)
+    @Slot(int, str)
+    def image_saved(self, id, fileName):
+        index = self._tab_widget.count()
+        image_view = ImageView(self._current_preview, fileName)
+        self._tab_widget.addTab(image_view, f"Capture #{index}")
+        self._tab_widget.setCurrentIndex(index)
+
+    @Slot(int, QCameraImageCapture.Error, str)
+    def _capture_error(self, id, error, error_string):
+        print(error_string, file=sys.stderr)
+        self.show_status_message(error_string)
+
+    @Slot(QCamera.Error, str)
+    def _camera_error(self, error, error_string):
+        print(error_string, file=sys.stderr)
+        self.show_status_message(error_string)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    availableGeometry = app.desktop().availableGeometry(mainWin)
-    mainWin.resize(availableGeometry.width() / 3, availableGeometry.height() / 2)
-    mainWin.show()
+    main_win = MainWindow()
+    available_geometry = main_win.screen().availableGeometry()
+    main_win.resize(available_geometry.width() / 3, available_geometry.height() / 2)
+    main_win.show()
     sys.exit(app.exec())
--- examples/multimedia/player/player.py
+++ examples/multimedia/player/player.py
@@ -1,7 +1,7 @@
 
 #############################################################################
 ##
-## Copyright (C) 2017 The Qt Company Ltd.
+## Copyright (C) 2021 The Qt Company Ltd.
 ## Contact: http://www.qt.io/licensing/
 ##
 ## This file is part of the Qt for Python examples of the Qt Toolkit.
@@ -42,12 +42,26 @@
 """PySide6 Multimedia player example"""
 
 import sys
-from PySide6.QtCore import QStandardPaths, Qt
+from PySide6.QtCore import QStandardPaths, Qt, Slot
 from PySide6.QtGui import QAction, QIcon, QKeySequence, QScreen
 from PySide6.QtWidgets import (QApplication, QDialog, QFileDialog,
     QMainWindow, QSlider, QStyle, QToolBar)
-from PySide6.QtMultimedia import QMediaPlayer, QMediaPlaylist
+from PySide6.QtMultimedia import QMediaFormat, QMediaPlayer, QMediaPlaylist
 from PySide6.QtMultimediaWidgets import QVideoWidget
+
+
+AVI = "video/x-msvideo"  # AVI
+
+
+MP4 = 'video/mp4'
+
+
+def get_supported_mime_types():
+    result = []
+    for f in QMediaFormat().supportedFileFormats(QMediaFormat.Decode):
+        mime_type = QMediaFormat(f).mimeType()
+        result.append(mime_type.name())
+    return result
 
 
 class MainWindow(QMainWindow):
@@ -55,105 +69,152 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.playlist = QMediaPlaylist()
-        self.player = QMediaPlayer()
+        self._playlist = QMediaPlaylist()
+        self._player = QMediaPlayer()
+        self._player.errorOccurred.connect(self._player_error)
 
-        toolBar = QToolBar()
-        self.addToolBar(toolBar)
+        tool_bar = QToolBar()
+        self.addToolBar(tool_bar)
 
-        fileMenu = self.menuBar().addMenu("&File")
-        openAction = QAction(QIcon.fromTheme("document-open"),
-                             "&Open...", self, shortcut=QKeySequence.Open,
-                             triggered=self.open)
-        fileMenu.addAction(openAction)
-        exitAction = QAction(QIcon.fromTheme("application-exit"), "E&xit",
-                             self, shortcut="Ctrl+Q", triggered=self.close)
-        fileMenu.addAction(exitAction)
+        file_menu = self.menuBar().addMenu("&File")
+        icon = QIcon.fromTheme("document-open")
+        open_action = QAction(icon, "&Open...", self,
+                              shortcut=QKeySequence.Open, triggered=self.open)
+        file_menu.addAction(open_action)
+        icon = QIcon.fromTheme("application-exit")
+        exit_action = QAction(icon, "E&xit", self,
+                              shortcut="Ctrl+Q", triggered=self.close)
+        file_menu.addAction(exit_action)
 
-        playMenu = self.menuBar().addMenu("&Play")
-        playIcon = self.style().standardIcon(QStyle.SP_MediaPlay)
-        self.playAction = toolBar.addAction(playIcon, "Play")
-        self.playAction.triggered.connect(self.player.play)
-        playMenu.addAction(self.playAction)
+        play_menu = self.menuBar().addMenu("&Play")
+        style = self.style()
+        icon = QIcon.fromTheme("media-playback-start.png",
+                               style.standardIcon(QStyle.SP_MediaPlay))
+        self._play_action = tool_bar.addAction(icon, "Play")
+        self._play_action.triggered.connect(self._player.play)
+        play_menu.addAction(self._play_action)
 
-        previousIcon = self.style().standardIcon(QStyle.SP_MediaSkipBackward)
-        self.previousAction = toolBar.addAction(previousIcon, "Previous")
-        self.previousAction.triggered.connect(self.previousClicked)
-        playMenu.addAction(self.previousAction)
+        icon = QIcon.fromTheme("media-skip-backward-symbolic.svg",
+                               style.standardIcon(QStyle.SP_MediaSkipBackward))
+        self._previous_action = tool_bar.addAction(icon, "Previous")
+        self._previous_action.triggered.connect(self.previous_clicked)
+        play_menu.addAction(self._previous_action)
 
-        pauseIcon = self.style().standardIcon(QStyle.SP_MediaPause)
-        self.pauseAction = toolBar.addAction(pauseIcon, "Pause")
-        self.pauseAction.triggered.connect(self.player.pause)
-        playMenu.addAction(self.pauseAction)
+        icon = QIcon.fromTheme("media-playback-pause.png",
+                               style.standardIcon(QStyle.SP_MediaPause))
+        self._pause_action = tool_bar.addAction(icon, "Pause")
+        self._pause_action.triggered.connect(self._player.pause)
+        play_menu.addAction(self._pause_action)
 
-        nextIcon = self.style().standardIcon(QStyle.SP_MediaSkipForward)
-        self.nextAction = toolBar.addAction(nextIcon, "Next")
-        self.nextAction.triggered.connect(self.playlist.next)
-        playMenu.addAction(self.nextAction)
+        icon = QIcon.fromTheme("media-skip-forward-symbolic.svg",
+                               style.standardIcon(QStyle.SP_MediaSkipForward))
+        self._next_action = tool_bar.addAction(icon, "Next")
+        self._next_action.triggered.connect(self.next_clicked)
+        play_menu.addAction(self._next_action)
 
-        stopIcon = self.style().standardIcon(QStyle.SP_MediaStop)
-        self.stopAction = toolBar.addAction(stopIcon, "Stop")
-        self.stopAction.triggered.connect(self.player.stop)
-        playMenu.addAction(self.stopAction)
+        icon = QIcon.fromTheme("media-playback-stop.png",
+                               style.standardIcon(QStyle.SP_MediaStop))
+        self._stop_action = tool_bar.addAction(icon, "Stop")
+        self._stop_action.triggered.connect(self._ensure_stopped)
+        play_menu.addAction(self._stop_action)
 
-        self.volumeSlider = QSlider()
-        self.volumeSlider.setOrientation(Qt.Horizontal)
-        self.volumeSlider.setMinimum(0)
-        self.volumeSlider.setMaximum(100)
-        self.volumeSlider.setFixedWidth(self.screen().availableGeometry().width() / 10)
-        self.volumeSlider.setValue(self.player.volume())
-        self.volumeSlider.setTickInterval(10)
-        self.volumeSlider.setTickPosition(QSlider.TicksBelow)
-        self.volumeSlider.setToolTip("Volume")
-        self.volumeSlider.valueChanged.connect(self.player.setVolume)
-        toolBar.addWidget(self.volumeSlider)
+        self._volume_slider = QSlider()
+        self._volume_slider.setOrientation(Qt.Horizontal)
+        self._volume_slider.setMinimum(0)
+        self._volume_slider.setMaximum(100)
+        available_width = self.screen().availableGeometry().width()
+        self._volume_slider.setFixedWidth(available_width / 10)
+        self._volume_slider.setValue(self._player.volume())
+        self._volume_slider.setTickInterval(10)
+        self._volume_slider.setTickPosition(QSlider.TicksBelow)
+        self._volume_slider.setToolTip("Volume")
+        self._volume_slider.valueChanged.connect(self._player.setVolume)
+        tool_bar.addWidget(self._volume_slider)
 
-        aboutMenu = self.menuBar().addMenu("&About")
-        aboutQtAct = QAction("About &Qt", self, triggered=qApp.aboutQt)
-        aboutMenu.addAction(aboutQtAct)
+        about_menu = self.menuBar().addMenu("&About")
+        about_qt_act = QAction("About &Qt", self, triggered=qApp.aboutQt)
+        about_menu.addAction(about_qt_act)
 
-        self.videoWidget = QVideoWidget()
-        self.setCentralWidget(self.videoWidget)
-        self.player.setPlaylist(self.playlist)
-        self.player.stateChanged.connect(self.updateButtons)
-        self.player.setVideoOutput(self.videoWidget)
+        self._video_widget = QVideoWidget()
+        self.setCentralWidget(self._video_widget)
+        self._player.playbackStateChanged.connect(self.update_buttons)
+        self._player.setVideoOutput(self._video_widget)
 
-        self.updateButtons(self.player.state())
+        self.update_buttons(self._player.playbackState())
+        self._mime_types = []
 
+    def closeEvent(self, event):
+        self._ensure_stopped()
+        event.accept()
+
+    @Slot()
     def open(self):
-        fileDialog = QFileDialog(self)
-        supportedMimeTypes = QMediaPlayer.supportedMimeTypes()
-        if not supportedMimeTypes:
-            supportedMimeTypes.append("video/x-msvideo")  # AVI
-        fileDialog.setMimeTypeFilters(supportedMimeTypes)
-        moviesLocation = QStandardPaths.writableLocation(QStandardPaths.MoviesLocation)
-        fileDialog.setDirectory(moviesLocation)
-        if fileDialog.exec() == QDialog.Accepted:
-            self.playlist.addMedia(fileDialog.selectedUrls()[0])
-            self.player.play()
+        self._ensure_stopped()
+        file_dialog = QFileDialog(self)
 
-    def previousClicked(self):
+        is_windows = sys.platform == 'win32'
+        if not self._mime_types:
+            self._mime_types = get_supported_mime_types()
+            if (is_windows and AVI not in self._mime_types):
+                self._mime_types.append(AVI)
+
+        file_dialog.setMimeTypeFilters(self._mime_types)
+
+        default_mimetype = AVI if is_windows else MP4
+        if default_mimetype in self._mime_types:
+            file_dialog.selectMimeTypeFilter(default_mimetype)
+
+        movies_location = QStandardPaths.writableLocation(QStandardPaths.MoviesLocation)
+        file_dialog.setDirectory(movies_location)
+        if file_dialog.exec() == QDialog.Accepted:
+            url = file_dialog.selectedUrls()[0]
+            self._playlist.addMedia(url)
+            self._player.setSource(url)
+            self._player.play()
+
+    @Slot()
+    def _ensure_stopped(self):
+        if self._player.playbackState() != QMediaPlayer.StoppedState:
+            self._player.stop()
+
+    @Slot()
+    def previous_clicked(self):
         # Go to previous track if we are within the first 5 seconds of playback
         # Otherwise, seek to the beginning.
-        if self.player.position() <= 5000:
-            self.playlist.previous()
+        if self._player.position() <= 5000:
+            self._playlist.previous()
+            self._player.setSource(self._playlist.currentMedia())
         else:
-            self.player.setPosition(0)
+            self._player.setPosition(0)
 
-    def updateButtons(self, state):
-        mediaCount = self.playlist.mediaCount()
-        self.playAction.setEnabled(mediaCount > 0
+    @Slot()
+    def next_clicked(self):
+        self._playlist.next()
+        self._player.setSource(self._playlist.currentMedia())
+
+    def update_buttons(self, state):
+        media_count = self._playlist.mediaCount()
+        self._play_action.setEnabled(media_count > 0
             and state != QMediaPlayer.PlayingState)
-        self.pauseAction.setEnabled(state == QMediaPlayer.PlayingState)
-        self.stopAction.setEnabled(state != QMediaPlayer.StoppedState)
-        self.previousAction.setEnabled(self.player.position() > 0)
-        self.nextAction.setEnabled(mediaCount > 1)
+        self._pause_action.setEnabled(state == QMediaPlayer.PlayingState)
+        self._stop_action.setEnabled(state != QMediaPlayer.StoppedState)
+        self._previous_action.setEnabled(self._player.position() > 0)
+        self._next_action.setEnabled(media_count > 1)
+
+    def show_status_message(self, message):
+        self.statusBar().showMessage(message, 5000)
+
+    @Slot(QMediaPlayer.Error, str)
+    def _player_error(self, error, error_string):
+        print(error_string, file=sys.stderr)
+        self.show_status_message(error_string)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    availableGeometry = mainWin.screen().availableGeometry()
-    mainWin.resize(availableGeometry.width() / 3, availableGeometry.height() / 2)
-    mainWin.show()
+    main_win = MainWindow()
+    available_geometry = main_win.screen().availableGeometry()
+    main_win.resize(available_geometry.width() / 3,
+                    available_geometry.height() / 2)
+    main_win.show()
     sys.exit(app.exec())
