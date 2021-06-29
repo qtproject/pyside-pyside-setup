a0bd6e79626b38973e2d2cdb0120581eb49d6d71
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2021-06-30 01:00:37 +0200

    example: add charts dynamic spline
    Task-number: PYSIDE-841
    Pick-to: 6.1
    Change-Id: I4a7ea69a6b1f9aaf190f66c464dcb9e19b20f4c5
    Reviewed-by: Christian Tismer <tismer@stackless.com>

---
+++ examples/charts/dynamicspline/chart.py
@@ -0,0 +1,89 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+import random
+
+from PySide6.QtCharts import QChart, QSplineSeries, QValueAxis
+from PySide6.QtCore import Qt, QTimer, Slot
+from PySide6.QtGui import QPen
+
+
+class Chart(QChart):
+    def __init__(self, parent=None):
+        super().__init__(QChart.ChartTypeCartesian, parent, Qt.WindowFlags())
+        self._timer = QTimer()
+        self._series = QSplineSeries(self)
+        self._titles = []
+        self._axisX = QValueAxis()
+        self._axisY = QValueAxis()
+        self._step = 0
+        self._x = 5
+        self._y = 1
+
+        self._timer.timeout.connect(self.handleTimeout)
+        self._timer.setInterval(1000)
+
+        green = QPen(Qt.red)
+        green.setWidth(3)
+        self._series.setPen(green)
+        self._series.append(self._x, self._y)
+
+        self.addSeries(self._series)
+        self.addAxis(self._axisX, Qt.AlignBottom)
+        self.addAxis(self._axisY, Qt.AlignLeft)
+
+        self._series.attachAxis(self._axisX)
+        self._series.attachAxis(self._axisY)
+        self._axisX.setTickCount(5)
+        self._axisX.setRange(0, 10)
+        self._axisY.setRange(-5, 10)
+
+        self._timer.start()
+
+    @Slot()
+    def handleTimeout(self):
+        x = self.plotArea().width() / self._axisX.tickCount()
+        y = (self._axisX.max() - self._axisX.min()) / self._axisX.tickCount()
+        self._x += y
+        self._y = random.uniform(0, 5) - 2.5
+        self._series.append(self._x, self._y)
+        self.scroll(x, 0)
+        if self._x == 100:
+            self._timer.stop()
---
+++ examples/charts/dynamicspline/doc/dynamicspline.rst
@@ -0,0 +1,15 @@
+Dynamic Spline Example
+======================
+
+This example shows how to draw dynamic data.
+
+You can check a simple animation of this example
+`here <https://qt-wiki-uploads.s3.amazonaws.com/images/thumb/f/f8/Dynamicspline.gif/300px-Dynamicspline.gif>`_.
+
+.. image:: dynamicspline1.png
+   :width: 400
+   :alt: Dynamic Spline Screenshot 1
+
+.. image:: dynamicspline2.png
+   :width: 400
+   :alt: Dynamic Spline Screenshot 2
