qtproject/pyside-pyside-setup

commit b1b2cc2ebed2fcf6e31c1fbbdd3638216e34717b

shiboken6: Fix missing functions from base classes
Add all occurrences found by name in getInheritedOverloads().
This manifested in missing overloads of QWidget::addAction()
introduced by qtbase/08e4d2db084f6abbf1840ffb694b15bd215ad069.

Pick-to: 6.1
Change-Id: I6b3bf143d32addbbb6b8332bed5e86e1677b0e28
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/shiboken6/generator/shiboken/shibokengenerator.cpp
@@ -2299,12 +2299,14 @@ AbstractMetaFunctionCList
     AbstractMetaClass *basis;
     if (func->ownerClass() && (basis = func->ownerClass()->baseClass())) {
         for (; basis; basis = basis->baseClass()) {
-            const auto inFunc = basis->findFunction(func->name());
-            if (!inFunc.isNull() && !seen->contains(inFunc->minimalSignature())) {
-                seen->insert(inFunc->minimalSignature());
-                AbstractMetaFunction *newFunc = inFunc->copy();
-                newFunc->setImplementingClass(func->implementingClass());
-                results << AbstractMetaFunctionCPtr(newFunc);
+            const auto inFunctions = basis->findFunctions(func->name());
+            for (const auto &inFunc : inFunctions) {
+                if (!seen->contains(inFunc->minimalSignature())) {
+                    seen->insert(inFunc->minimalSignature());
+                    AbstractMetaFunction *newFunc = inFunc->copy();
+                    newFunc->setImplementingClass(func->implementingClass());
+                    results << AbstractMetaFunctionCPtr(newFunc);
+                }
             }
         }
     }

sources/shiboken6/ApiExtractor/abstractmetalang.cpp
@@ -607,6 +607,17 @@ AbstractMetaFunctionCPtr AbstractMetaClass::findFunction(const QString &function
     return AbstractMetaFunction::find(d->m_functions, functionName);
 }
 
+AbstractMetaFunctionCList AbstractMetaClass::findFunctions(const QString &functionName) const
+{
+    AbstractMetaFunctionCList result;
+    std::copy_if(d->m_functions.cbegin(), d->m_functions.cend(),
+                 std::back_inserter(result),
+                 [&functionName](const AbstractMetaFunctionCPtr &f) {
+                     return f->name() == functionName;
+                 });
+    return result;
+}
+
 AbstractMetaFunctionCPtr AbstractMetaClass::findOperatorBool() const
 {
     auto it = std::find_if(d->m_functions.cbegin(), d->m_functions.cend(),

sources/shiboken6/ApiExtractor/abstractmetalang.h
@@ -95,6 +95,7 @@ public:
     void addFunction(const AbstractMetaFunctionCPtr &function);
     bool hasFunction(const QString &str) const;
     AbstractMetaFunctionCPtr findFunction(const QString& functionName) const;
+    AbstractMetaFunctionCList findFunctions(const QString& functionName) const;
     AbstractMetaFunctionCPtr findOperatorBool() const;
     // Find a Qt-style isNull() method suitable for nb_bool
     AbstractMetaFunctionCPtr findQtIsNullMethod() const;

commit 795e5b7c7521d275750590be728ed1078150f537

shiboken6: Skip base functions for which no bindings should be generated
Factor out a function for checking the bindings generation
and use that in getInheritedOverloads() as well.

Amends b1b2cc2ebed2fcf6e31c1fbbdd3638216e34717b, which caused
some modified-removed functions to be generated in derived
classes.

Pick-to: 6.1
Change-Id: Iddb2f868c360da5397279cc3058cb554f5e51b42
Reviewed-by: Christian Tismer <tismer@stackless.com>

sources/shiboken6/ApiExtractor/abstractmetafunction.cpp
@@ -419,6 +419,22 @@ bool AbstractMetaFunction::usesRValueReferences() const
     return false;
 }
 
+bool AbstractMetaFunction::generateBinding() const
+{
+    switch (d->m_functionType) {
+    case ConversionOperator:
+    case AssignmentOperatorFunction:
+    case MoveAssignmentOperatorFunction:
+        return false;
+    default:
+        break;
+    }
+    if (isPrivate() && d->m_functionType != EmptyFunction)
+        return false;
+    return d->m_name != u"qt_metacall" && !usesRValueReferences()
+        && !isModifiedRemoved();
+}
+
 QStringList AbstractMetaFunction::introspectionCompatibleSignatures(const QStringList &resolvedArguments) const
 {
     AbstractMetaArgumentList arguments = this->arguments();

sources/shiboken6/ApiExtractor/abstractmetafunction.h
@@ -280,6 +280,8 @@ public:
     void setFunctionType(FunctionType type);
 
     bool usesRValueReferences() const;
+    bool generateBinding() const;
+
     QStringList introspectionCompatibleSignatures(const QStringList &resolvedArguments = QStringList()) const;
     QString signature() const;
 

sources/shiboken6/generator/shiboken/cppgenerator.cpp
@@ -538,13 +538,8 @@ void CppGenerator::generateClass(TextStream &s, const GeneratorContext &classCon
         QSet<QString> seenSignatures;
         bool staticEncountered = false;
         for (const auto &func : it.value()) {
-            if (!func->isAssignmentOperator()
-                && !func->usesRValueReferences()
-                && !func->isConversionOperator()
-                && !func->isModifiedRemoved()
-                && (!func->isPrivate() || func->functionType() == AbstractMetaFunction::EmptyFunction)
-                && func->ownerClass() == func->implementingClass()
-                && (func->name() != QLatin1String("qt_metacall"))) {
+            if (func->ownerClass() == func->implementingClass()
+                && func->generateBinding()) {
                 // PYSIDE-331: Inheritance works correctly when there are disjoint functions.
                 // But when a function is both in a class and inherited in a subclass,
                 // then we need to search through all subclasses and collect the new signatures.

sources/shiboken6/generator/shiboken/shibokengenerator.cpp
@@ -2309,7 +2309,8 @@ AbstractMetaFunctionCList
         for (; basis; basis = basis->baseClass()) {
             const auto inFunctions = basis->findFunctions(func->name());
             for (const auto &inFunc : inFunctions) {
-                if (!seen->contains(inFunc->minimalSignature())) {
+                if (inFunc->generateBinding()
+                    && !seen->contains(inFunc->minimalSignature())) {
                     seen->insert(inFunc->minimalSignature());
                     AbstractMetaFunction *newFunc = inFunc->copy();
                     newFunc->setImplementingClass(func->implementingClass());