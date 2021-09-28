56852d48401e1dfb0a022b39dc857d39fca76445
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2021-09-28 11:28:14 +0200

    Python 3.10: add wheel identifier
    Change-Id: I97b323d5055c30f782f62b85e734eb9f0e33f7b3
    Reviewed-by: Simo Fält <simo.falt@qt.io>
    (cherry picked from commit 2fb82b5cd3ee1db9234e038759bd0e3cda9ca40b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- build_scripts/wheel_override.py
+++ build_scripts/wheel_override.py
@@ -88,7 +88,7 @@ class PysideBuildWheel(_bdist_wheel, DistUtilsCommandMixin):
         # create a properly named package.
         limited_api_enabled = OPTION["LIMITED_API"] == 'yes'
         if limited_api_enabled:
-            self.py_limited_api = "cp36.cp37.cp38.cp39"
+            self.py_limited_api = "cp36.cp37.cp38.cp39.cp310"
 
         self._package_version = get_package_version()
 
