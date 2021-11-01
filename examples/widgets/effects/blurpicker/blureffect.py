ea3851cdaf7f13f62fe9ae23b96750cd70468294
Jaime Resano <gemailpersonal02@gmail.com>
2021-11-01 23:04:19 +0100

    Add blurpicker example
    Ported from C++
    
    Change-Id: Id18068e36ecfa26ab0312cfde2de1ba1c0773223
    Task-number: PYSIDE-841
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

---
+++ examples/widgets/effects/blurpicker/blureffect.py
@@ -0,0 +1,70 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+
+from PySide6.QtCore import QRect
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import QGraphicsBlurEffect
+
+
+class BlurEffect(QGraphicsBlurEffect):
+    def __init__(self, item):
+        super().__init__()
+        self.m_base_line = 200
+        self.item = item
+
+    def adjust_for_item(self):
+        y = self.m_base_line - self.item.pos().y()
+
+        # radius = qBound(qreal(0.0), y / 32, qreal(16.0)); which is equivalent to
+        radius = max(0, min(y / 32, 16))
+
+        self.setBlurRadius(radius)
+
+    def set_base_line(self, base_line):
+        self.m_base_line = base_line
+
+    def boundingRect(self) -> QRect:
+        self.adjust_for_item()
+        return super().boundingRect()
+
+    def draw(self, painter: QPainter):
+        self.adjust_for_item()
+        super().draw(painter)
