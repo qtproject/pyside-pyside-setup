105490ccd31ae2eed54c5512b20467a1d2f88a01
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-11-19 10:28:39 +0100

    PySide6: Fix crash when connecting slot after disconnecting non-existent connection to same slot
    Add a null-check.
    
    Fixes: PYSIDE-1715
    Change-Id: I0fc8c1b051b04eacd6bd75542ceaf9f23a825cab
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit 360ef4e884db56e3cd228593bbc562727fbd41cd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/libpyside/globalreceiverv2.cpp
+++ sources/pyside6/libpyside/globalreceiverv2.cpp
@@ -299,8 +299,10 @@ void GlobalReceiverV2::notify()
     const QSet<const QObject *> objSet(m_refs.cbegin(), m_refs.cend());
     Py_BEGIN_ALLOW_THREADS
     for (const QObject *o : objSet) {
-        QMetaObject::disconnect(o, DESTROY_SIGNAL_ID, this, DESTROY_SLOT_ID);
-        QMetaObject::connect(o, DESTROY_SIGNAL_ID, this, DESTROY_SLOT_ID);
+        if (o) {
+            QMetaObject::disconnect(o, DESTROY_SIGNAL_ID, this, DESTROY_SLOT_ID);
+            QMetaObject::connect(o, DESTROY_SIGNAL_ID, this, DESTROY_SLOT_ID);
+        }
     }
     Py_END_ALLOW_THREADS
 }
