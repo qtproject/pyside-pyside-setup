3efe0b3e66450aa98fd8600bc0ac356bcbe5ad54
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-12-15 16:24:54 +0100

    PySide6: Fix QQmlApplicationEngine.load(str) to not construct a QUrl
    Specify the overload number so that string is checked first.
    
    Add a test since QQmlApplicationEngine is not yet tested.
    
    Amends 2cc81fd5e63bf931d8c9d1c8c2c66ac9e16ef087.
    
    Pick-to: 6.2
    Fixes: PYSIDE-1736
    Task-number: PYSIDE-1499
    Change-Id: I1736928f1acf17fda5671bd11d16c5017141f1ff
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

---
+++ sources/pyside6/tests/QtQml/qqmlapplicationengine_test.py
@@ -0,0 +1,63 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the test suite of Qt for Python.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+'''Test case for QQmlApplicationEngine'''
+
+import os
+import sys
+import unittest
+
+from pathlib import Path
+sys.path.append(os.fspath(Path(__file__).resolve().parents[1]))
+from init_paths import init_test_paths
+init_test_paths(False)
+
+from helper.timedqguiapplication import TimedQGuiApplication
+
+from PySide6.QtCore import QUrl, QObject, QTimer, Qt
+from PySide6.QtQml import QQmlApplicationEngine
+
+
+class TestQQmlApplicationEngine(TimedQGuiApplication):
+
+    def testQQmlApplicationEngine(self):
+        engine = QQmlApplicationEngine()
+
+        qml_file_path = Path(__file__).resolve().parent / "qqmlapplicationengine.qml"
+
+        # PYSIDE-1736: load from a string.
+        engine.load(os.fspath(qml_file_path))
+        rootObjects = engine.rootObjects()
+        self.assertTrue(rootObjects)
+        window = rootObjects[0]
+        window.setTitle("TestQQmlApplicationEngine")
+        QTimer.singleShot(100, window.close)
+
+
+if __name__ == '__main__':
+    unittest.main()
