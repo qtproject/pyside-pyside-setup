acfbffa195cd7563b6d9bf9936a72ebec4de20d3
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2021-12-15 10:23:40 +0100

    Add mimetypesexample
    - port of corelib/mimetypes/mimetypebrowser example from Qt6
    
    Task-number: PYSIDE-841
    Change-Id: Ib2f1637935662f969b264315bbf8ba036bb9420b
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit 73bf0e9732cf4945bdd7158d04d29779da55a29b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ examples/corelib/mimetypesbrowser/doc/mimetypesbrowser.rst
@@ -0,0 +1,11 @@
+MIME Type Browser Example
+=========================
+
+A Python application that demonstrates the analogous example in C++ `MIME Type
+Browser Example
+https://doc.qt.io/qt-6/qtcore-mimetypes-mimetypebrowser-example.html`_
+
+.. image:: mimetypesbrowser.png
+   :width: 400
+   :alt: mimetypebrowser screenshot
+
---
+++ examples/corelib/mimetypesbrowser/mimetypesbrowser.py
@@ -0,0 +1,61 @@
+#############################################################################
+##
+## Copyright (C) 2021 The Qt Company Ltd.
+## Contact: http://www.qt.io/licensing/
+##
+## This file is part of the Qt for Python examples of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:BSD$
+## You may use this file under the terms of the BSD license as follows:
+##
+## "Redistribution and use in source and binary forms, with or without
+## modification, are permitted provided that the following conditions are
+## met:
+##   * Redistributions of source code must retain the above copyright
+##     notice, this list of conditions and the following disclaimer.
+##   * Redistributions in binary form must reproduce the above copyright
+##     notice, this list of conditions and the following disclaimer in
+##     the documentation and/or other materials provided with the
+##     distribution.
+##   * Neither the name of The Qt Company Ltd nor the names of its
+##     contributors may be used to endorse or promote products derived
+##     from this software without specific prior written permission.
+##
+##
+## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+"""PySide6 port of the corelib/mimetypes/mimetypebrowser example from from Qt"""
+
+import argparse
+import sys
+
+from mainwindow import MainWindow
+from PySide6.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    parser = argparse.ArgumentParser(description="MimeTypesBrowser Example")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
+    args = parser.parse_args()
+
+    mainWindow = MainWindow()
+    availableGeometry = mainWindow.screen().availableGeometry()
+    mainWindow.resize(availableGeometry.width() / 3, availableGeometry.height() / 2)
+    mainWindow.show()
+
+    sys.exit(app.exec())
