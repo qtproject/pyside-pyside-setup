2511282c895d048f7eadb633219dea6f4d4d5b14
Friedemann Kleint <Friedemann.Kleint@qt.io>
2022-01-12 15:43:18 +0100

    Regenerate the example ui files
    Remove some star imports.
    
    Pick-to: 6.2
    Task-number: PYSIDE-1773
    Change-Id: I42e3034ae91bdc5779b6ed632fe4f5d234f060b0
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- examples/charts/chartthemes/ui_themewidget.py
+++ examples/charts/chartthemes/ui_themewidget.py
@@ -3,15 +3,21 @@
 ################################################################################
 ## Form generated from reading UI file 'themewidget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.2.0
+## Created by: Qt User Interface Compiler version 6.2.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import *
-from PySide6.QtGui import *
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
+    QHBoxLayout, QLabel, QSizePolicy, QSpacerItem,
+    QWidget)
 
 class Ui_ThemeWidgetForm(object):
     def setupUi(self, ThemeWidgetForm):
--- examples/webchannel/standalone/ui_dialog.py
+++ examples/webchannel/standalone/ui_dialog.py
@@ -3,18 +3,20 @@
 ################################################################################
 ## Form generated from reading UI file 'dialog.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.14.1
+## Created by: Qt User Interface Compiler version 6.2.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
-    QRect, QSize, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
-    QRadialGradient)
-from PySide6.QtWidgets import *
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QLineEdit,
+    QPlainTextEdit, QPushButton, QSizePolicy, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -40,6 +42,7 @@ class Ui_Dialog(object):
         self.output.setBackgroundVisible(False)
 
         self.gridLayout.addWidget(self.output, 0, 0, 1, 2)
+
 
         self.retranslateUi(Dialog)
 
