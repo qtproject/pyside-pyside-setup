3a16f9f0a9d8081e3dfe076b12840a857b4ee3ce
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-02-01 11:45:33 +0100

    nothing.h - why is it required?
    - added a short comment on why this file is required.
    
    Pick-to: 6.2
    Change-Id: Iad11c049ea4ce3d9a47d37b94d2cb307979e62cd
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

---
+++ sources/shiboken6/shibokenmodule/nothing.h
@@ -0,0 +1,3 @@
+// This is a placeholder file for shiboken, because shiboken expects a header file listing the
+// header files whose bindings are to be created. This file is empty because shibokenmodule
+// functions are implemented directly inside the typesystem_shiboken.xml file through CPython code.
