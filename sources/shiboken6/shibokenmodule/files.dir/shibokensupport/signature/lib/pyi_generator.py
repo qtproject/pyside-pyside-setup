f3ac53179f8a4081efa5f291c3efdeb75b2ab2f9
Christian Tismer <tismer@stackless.com>
2022-03-03 18:47:03 +0100

    generate_pyi: Fix an import timing problem on Windows, amended
    amends <293f50f40a608f085f35929b838d319c111c6122>
    
    While testing with Windows, not all changes went into the
    repository. This completes the change.
    
    The idea is: Put the PySide imports first, since they also
    initialize shiboken. It is then safe to import Shiboken,
    because the module was implicitly imported, already.
    
    Task-number: PYSIDE-535
    Change-Id: I7ecf6e4a04d20ab4c3e1c841717de4c525f83047
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 45cde5333e221388884056820a1aa99ccd773431)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
@@ -291,6 +291,12 @@ def generate_pyi(import_name, outpath, options):
             # we remove the "<<IMPORTS>>" marker and insert imports if needed
             if line == "<<IMPORTS>>":
                 text = outfile.getvalue()
+                wr.print("import " + import_name)
+                for mod_name in find_imports(text):
+                    imp = "PySide6." + mod_name
+                    if imp != import_name:
+                        wr.print("import " + imp)
+                wr.print()
                 for mod, imports in filter_from_imports(FROM_IMPORTS, text):
                     import_args = ', '.join(imports)
                     if mod is None:
@@ -298,13 +304,6 @@ def generate_pyi(import_name, outpath, options):
                         wr.print(f"import {import_args}")
                     else:
                         wr.print(f"from {mod} import {import_args}")
-                wr.print()
-                if need_imports:
-                    for mod_name in find_imports(text):
-                        imp = "PySide6." + mod_name
-                        if imp != import_name:
-                            wr.print("import " + imp)
-                wr.print("import " + import_name)
                 wr.print()
                 wr.print()
             else:
