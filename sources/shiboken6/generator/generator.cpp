qtproject/pyside-pyside-setup

commit b60cdf97b41ab6db977c5acd96d491003a5a046d

shiboken6: Refactor file name generation
Simplify and streamline the code:

- Remove virtual Generator::fileNameSuffix(), leave only a static
  function in QtDocGenerator which reuses it.
- Introduce a static helper function for fileNameForContext()
  to build the name.
- Remove the AbstractMetaClass * parameter from
  getFileNameBaseForSmartPointer() since the name is available
  from the type entry as well.
- Add a public static function to HeaderGenerator to be used
  in CppGenerator to generate the include statements instead
  of replacing ".cpp" by ".h" for this.

One goal is to no longer use GeneratorContext::metaClass() for smart
pointers so that the smart pointer classes can be removed or made
built-in.

Task-number: PYSIDE-454
Change-Id: Ie6ace5ee93467a02940e43f781b626d1c13ead20
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

sources/shiboken6/generator/generator.cpp
@@ -409,6 +409,25 @@ bool Generator::handleOption(const QString & key, const QString & /* value */)
     return false;
 }
 
+QString Generator::fileNameForContextHelper(const GeneratorContext &context,
+                                            const QString &suffix,
+                                            bool useQualifiedName)
+
+{
+    if (!context.forSmartPointer()) {
+        const AbstractMetaClass *metaClass = context.metaClass();
+        QString fileNameBase = useQualifiedName
+            ? metaClass->qualifiedCppName().toLower()
+            : metaClass->name().toLower();
+        fileNameBase.replace(u"::"_qs, u"_"_qs);
+        return fileNameBase + suffix;
+    }
+
+    const AbstractMetaType &smartPointerType = context.preciseType();
+    QString fileNameBase = getFileNameBaseForSmartPointer(smartPointerType);
+    return fileNameBase + suffix;
+}
+
 const AbstractMetaClassCList &Generator::invisibleTopNamespaces() const
 {
     return m_d->m_invisibleTopNamespaces;
@@ -483,11 +502,11 @@ bool Generator::generateFileForContext(const GeneratorContext &context)
     return true;
 }
 
-QString Generator::getFileNameBaseForSmartPointer(const AbstractMetaType &smartPointerType,
-                                                  const AbstractMetaClass *smartPointerClass)
+QString Generator::getFileNameBaseForSmartPointer(const AbstractMetaType &smartPointerType)
 {
     const AbstractMetaType innerType = smartPointerType.getSmartPointerInnerType();
-    QString fileName = smartPointerClass->qualifiedCppName().toLower();
+    smartPointerType.typeEntry()->qualifiedCppName();
+    QString fileName = smartPointerType.typeEntry()->qualifiedCppName().toLower();
     fileName.replace(QLatin1String("::"), QLatin1String("_"));
     fileName.append(QLatin1String("_"));
     fileName.append(innerType.name().toLower());

sources/shiboken6/generator/generator.h
@@ -256,6 +256,11 @@ public:
     static QString moduleName();
 
 protected:
+    /// Helper for determining the file name
+    static QString fileNameForContextHelper(const GeneratorContext &context,
+                                            const QString &suffix,
+                                            bool useQualifiedName = true);
+
     /// Returns all primitive types found by APIExtractor
     static PrimitiveTypeEntryList primitiveTypes();
 
@@ -271,8 +276,7 @@ protected:
     bool generateFileForContext(const GeneratorContext &context);
 
     /// Returns the file base name for a smart pointer.
-    static QString getFileNameBaseForSmartPointer(const AbstractMetaType &smartPointerType,
-                                                  const AbstractMetaClass *smartPointer);
+    static QString getFileNameBaseForSmartPointer(const AbstractMetaType &smartPointerType);
 
     /// Returns true if the generator should generate any code for the TypeEntry.
     static bool shouldGenerateTypeEntry(const TypeEntry *) ;
@@ -335,7 +339,6 @@ protected:
      *   for which the file name must be returned
      *   \return the file name used to write the binding code for the class
      */
-    virtual QString fileNameSuffix() const = 0;
     virtual QString fileNameForContext(const GeneratorContext &context) const = 0;
 
 

sources/shiboken6/generator/qtdoc/qtdocgenerator.cpp
@@ -116,9 +116,9 @@ QtDocGenerator::QtDocGenerator()
 
 QtDocGenerator::~QtDocGenerator() = default;
 
-QString QtDocGenerator::fileNameSuffix() const
+QString QtDocGenerator::fileNameSuffix()
 {
-    return QLatin1String(".rst");
+    return u".rst"_qs;
 }
 
 bool QtDocGenerator::shouldGenerate(const AbstractMetaClass *cls) const
@@ -129,13 +129,8 @@ bool QtDocGenerator::shouldGenerate(const AbstractMetaClass *cls) const
 
 QString QtDocGenerator::fileNameForContext(const GeneratorContext &context) const
 {
-    const AbstractMetaClass *metaClass = context.metaClass();
-    if (!context.forSmartPointer()) {
-        return metaClass->name() + fileNameSuffix();
-    }
-    const AbstractMetaType &smartPointerType = context.preciseType();
-    QString fileNameBase = getFileNameBaseForSmartPointer(smartPointerType, metaClass);
-    return fileNameBase + fileNameSuffix();
+    return fileNameForContextHelper(context, fileNameSuffix(),
+                                    false /* qualified */);
 }
 
 void QtDocGenerator::writeFormattedBriefText(TextStream &s, const Documentation &doc,

sources/shiboken6/generator/shiboken/cppgenerator.cpp
@@ -29,6 +29,7 @@
 #include <memory>
 
 #include "cppgenerator.h"
+#include "headergenerator.h"
 #include "apiextractorresult.h"
 #include "ctypenames.h"
 #include <exception.h>
@@ -211,22 +212,9 @@ static void writeOpaqueContainerCreationFuncDecl(TextStream &s, const QString &n
 
 CppGenerator::CppGenerator() = default;
 
-QString CppGenerator::fileNameSuffix() const
-{
-    return QLatin1String("_wrapper.cpp");
-}
-
 QString CppGenerator::fileNameForContext(const GeneratorContext &context) const
 {
-    const AbstractMetaClass *metaClass = context.metaClass();
-    if (!context.forSmartPointer()) {
-        QString fileNameBase = metaClass->qualifiedCppName().toLower();
-        fileNameBase.replace(QLatin1String("::"), QLatin1String("_"));
-        return fileNameBase + fileNameSuffix();
-    }
-    const AbstractMetaType &smartPointerType = context.preciseType();
-    QString fileNameBase = getFileNameBaseForSmartPointer(smartPointerType, metaClass);
-    return fileNameBase + fileNameSuffix();
+    return fileNameForContextHelper(context, u"_wrapper.cpp"_qs, true /* qualified */);
 }
 
 static bool isInplaceAdd(const AbstractMetaFunctionCPtr &func)
@@ -460,18 +448,16 @@ void CppGenerator::generateClass(TextStream &s, const GeneratorContext &classCon
     if (hasPrivateClasses())
         s << "#include \"" << getPrivateModuleHeaderFileName() << "\"\n";
 
-    QString headerfile = fileNameForContext(classContext);
-    headerfile.replace(QLatin1String(".cpp"), QLatin1String(".h"));
-    s << "\n// main header\n" << "#include \"" << headerfile << "\"\n";
+    s << "\n// main header\n" << "#include \""
+      << HeaderGenerator::headerFileNameForContext(classContext) << "\"\n";
 
     s  << '\n' << "// inner classes\n";
     const AbstractMetaClassList &innerClasses = metaClass->innerClasses();
     for (AbstractMetaClass *innerClass : innerClasses) {
         GeneratorContext innerClassContext = contextForClass(innerClass);
         if (shouldGenerate(innerClass) && !innerClass->typeEntry()->isSmartPointer()) {
-            QString headerfile = fileNameForContext(innerClassContext);
-            headerfile.replace(QLatin1String(".cpp"), QLatin1String(".h"));
-            s << "#include \"" << headerfile << "\"\n";
+            s << "#include \""
+                << HeaderGenerator::headerFileNameForContext(innerClassContext) << "\"\n";
         }
     }
 

sources/shiboken6/generator/shiboken/headergenerator.cpp
@@ -47,21 +47,14 @@
 #include <QtCore/QVariant>
 #include <QtCore/QDebug>
 
-QString HeaderGenerator::fileNameSuffix() const
+QString HeaderGenerator::headerFileNameForContext(const GeneratorContext &context)
 {
-    return QLatin1String("_wrapper.h");
+    return fileNameForContextHelper(context, u"_wrapper.h"_qs, true /* qualified */);
 }
 
 QString HeaderGenerator::fileNameForContext(const GeneratorContext &context) const
 {
-    const AbstractMetaClass *metaClass = context.metaClass();
-    if (!context.forSmartPointer()) {
-        QString fileNameBase = metaClass->qualifiedCppName().toLower();
-        fileNameBase.replace(QLatin1String("::"), QLatin1String("_"));
-        return fileNameBase + fileNameSuffix();
-    }
-    QString fileNameBase = getFileNameBaseForSmartPointer(context.preciseType(), metaClass);
-    return fileNameBase + fileNameSuffix();
+    return headerFileNameForContext(context);
 }
 
 void HeaderGenerator::writeCopyCtor(TextStream &s, const AbstractMetaClass *metaClass) const

sources/shiboken6/generator/qtdoc/qtdocgenerator.h
@@ -70,7 +70,7 @@ public:
 
 protected:
     bool shouldGenerate(const AbstractMetaClass *) const override;
-    QString fileNameSuffix() const override;
+    static QString fileNameSuffix();
     QString fileNameForContext(const GeneratorContext &context) const override;
     void generateClass(TextStream &ts, const GeneratorContext &classContext) override;
     bool finishGeneration() override;

sources/shiboken6/generator/shiboken/cppgenerator.h
@@ -56,7 +56,6 @@ public:
     const char *name() const override { return "Source generator"; }
 
 protected:
-    QString fileNameSuffix() const override;
     QString fileNameForContext(const GeneratorContext &context) const override;
     static QList<AbstractMetaFunctionCList>
         filterGroupedOperatorFunctions(const AbstractMetaClass *metaClass,

sources/shiboken6/generator/shiboken/headergenerator.h
@@ -45,8 +45,9 @@ public:
 
     const char *name() const override { return "Header generator"; }
 
+    static QString headerFileNameForContext(const GeneratorContext &context);
+
 protected:
-    QString fileNameSuffix() const override;
     QString fileNameForContext(const GeneratorContext &context) const override;
     void generateClass(TextStream &s, const GeneratorContext &classContext) override;
     bool finishGeneration() override;

commit b1b320c8b834c7821981e43744d15784f2bbb76b

shiboken6: Fix module TOC generation
Change b60cdf97b41ab6db977c5acd96d491003a5a046d introduced
a bug in that the .rst doc files were generated with lower case file
names. This upset the fancy toc formatter. As a drive-by, fix
empty columns.

Change-Id: Ic4600b5bd7a9152cce2b74ed502acbc71b8df891
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/shiboken6/generator/generator.cpp
@@ -411,14 +411,15 @@ bool Generator::handleOption(const QString & key, const QString & /* value */)
 
 QString Generator::fileNameForContextHelper(const GeneratorContext &context,
                                             const QString &suffix,
-                                            bool useQualifiedName)
+                                            FileNameFlags flags)
 
 {
     if (!context.forSmartPointer()) {
         const AbstractMetaClass *metaClass = context.metaClass();
-        QString fileNameBase = useQualifiedName
-            ? metaClass->qualifiedCppName().toLower()
-            : metaClass->name().toLower();
+        QString fileNameBase = flags.testFlag(FileNameFlag::UnqualifiedName)
+            ? metaClass->name() : metaClass->qualifiedCppName();
+        if (!flags.testFlag(FileNameFlag::KeepCase))
+            fileNameBase = fileNameBase.toLower();
         fileNameBase.replace(u"::"_qs, u"_"_qs);
         return fileNameBase + suffix;
     }

sources/shiboken6/generator/generator.h
@@ -200,6 +200,12 @@ public:
     };
     Q_DECLARE_FLAGS(Options, Option)
 
+    enum FileNameFlag {
+        UnqualifiedName = 0x1,
+        KeepCase = 0x2
+    };
+    Q_DECLARE_FLAGS(FileNameFlags, FileNameFlag)
+
     Generator();
     virtual ~Generator();
 
@@ -259,7 +265,7 @@ protected:
     /// Helper for determining the file name
     static QString fileNameForContextHelper(const GeneratorContext &context,
                                             const QString &suffix,
-                                            bool useQualifiedName = true);
+                                            FileNameFlags flags = {});
 
     /// Returns all primitive types found by APIExtractor
     static PrimitiveTypeEntryList primitiveTypes();
@@ -375,6 +381,8 @@ private:
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(Generator::Options)
+Q_DECLARE_OPERATORS_FOR_FLAGS(Generator::FileNameFlags)
+
 using GeneratorPtr = QSharedPointer<Generator>;
 using Generators = QList<GeneratorPtr>;
 

sources/shiboken6/generator/qtdoc/qtdocgenerator.cpp
@@ -130,7 +130,8 @@ bool QtDocGenerator::shouldGenerate(const TypeEntry *te) const
 QString QtDocGenerator::fileNameForContext(const GeneratorContext &context) const
 {
     return fileNameForContextHelper(context, fileNameSuffix(),
-                                    false /* qualified */);
+                                    FileNameFlag::UnqualifiedName
+                                    | FileNameFlag::KeepCase);
 }
 
 void QtDocGenerator::writeFormattedBriefText(TextStream &s, const Documentation &doc,
@@ -752,7 +753,7 @@ static void writeFancyToc(TextStream& s, const QStringList& items)
             const QString entry = QLatin1String("* :doc:`") + item + QLatin1Char('`');
             row << QtXmlToSphinx::TableCell(entry);
         }
-        if (!row.isEmpty())
+        if (row.size() > 1)
             table.appendRow(row);
     }
 

sources/shiboken6/generator/shiboken/cppgenerator.cpp
@@ -215,7 +215,7 @@ CppGenerator::CppGenerator() = default;
 
 QString CppGenerator::fileNameForContext(const GeneratorContext &context) const
 {
-    return fileNameForContextHelper(context, u"_wrapper.cpp"_qs, true /* qualified */);
+    return fileNameForContextHelper(context, u"_wrapper.cpp"_qs);
 }
 
 static bool isInplaceAdd(const AbstractMetaFunctionCPtr &func)

sources/shiboken6/generator/shiboken/headergenerator.cpp
@@ -49,7 +49,7 @@
 
 QString HeaderGenerator::headerFileNameForContext(const GeneratorContext &context)
 {
-    return fileNameForContextHelper(context, u"_wrapper.h"_qs, true /* qualified */);
+    return fileNameForContextHelper(context, u"_wrapper.h"_qs);
 }
 
 QString HeaderGenerator::fileNameForContext(const GeneratorContext &context) const