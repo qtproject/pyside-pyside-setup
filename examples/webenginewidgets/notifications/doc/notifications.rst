9fe9c110c5a951f4440015f2a36ac9bc949cd6cc
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-05-09 00:42:57 +0200

    webenginecore - QtWebEngineProfile setNotificationPresenter
    - added setNotificationPresenter(...)
    - added QtWebEngineWidgets Notifications example which uses
    setNotificationPresenter
    
    Task-number: PYSIDE-1912
    Change-Id: Ib8fdf7879d6e54bcae47dcc89be6bd5a861a8846
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit cd394249fc77063a2bd6ba7a7633fdbc10621475)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ examples/webenginewidgets/notifications/doc/notifications.rst
@@ -0,0 +1,8 @@
+WebEngine Notifications Example
+===============================
+
+Python port of C++ `WebEngine Notifications <https://doc.qt.io/qt-6/qtwebengine-webenginewidgets-notifications-example.html>`_
+
+.. image:: notifications.png
+   :width: 400
+   :alt: Notifications Example Screenshot
--- sources/pyside6/PySide6/glue/qtwebenginecore.cpp
+++ sources/pyside6/PySide6/glue/qtwebenginecore.cpp
@@ -53,3 +53,20 @@ auto callback = [callable](const QWebEngineCookieStore::FilterRequest& filterReq
 };
 %CPPSELF.%FUNCTION_NAME(callback);
 // @snippet qwebenginecookiestore-setcookiefilter
+
+// @snippet qwebengineprofile-setnotificationpresenter
+auto callable = %PYARG_1;
+auto callback = [callable](std::unique_ptr<QWebEngineNotification> webEngineNotification) -> void
+{
+    Shiboken::GilState state;
+    Shiboken::AutoDecRef arglist(PyTuple_New(1));
+    PyTuple_SET_ITEM(arglist.object(), 0,
+                    Shiboken::Conversions::pointerToPython(
+                        SbkPySide6_QtWebEngineCoreTypes[SBK_QWEBENGINENOTIFICATION_IDX],
+                        webEngineNotification.release()));
+    Py_INCREF(callable);
+    PyObject_CallObject(callable, arglist);
+    Py_DECREF(callable);
+};
+%CPPSELF.%FUNCTION_NAME(callback);
+// @snippet qwebengineprofile-setnotificationpresenter
