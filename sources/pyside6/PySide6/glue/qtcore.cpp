1448aa2ad6963efbc83b223c5c081ab599d3bce1
Christian Tismer <tismer@stackless.com>
2022-06-02 14:54:47 +0200

    Limited_API: Fix PyIndex_Check once and for all: avoid warning
    The Windows compiler is quite overzealous and complains if headers have different
    ordering in the used version, although the content seems to be identical.
    
    Instead of doing a tedious search, we simply rename the function.
    
    Task-number: PYSIDE-1797
    Change-Id: I88a8eca882357ac31b9eb932c89013c51c5c75e4
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 563613fefd81090d1947f16259394a830681492d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/PySide6/glue/qtcore.cpp
+++ sources/pyside6/PySide6/glue/qtcore.cpp
@@ -527,7 +527,7 @@ if (ret > 0 && ((strcmp(%1, SIGNAL(destroyed())) == 0) || (strcmp(%1, SIGNAL(des
 // @snippet qobject-receivers
 
 // @snippet qbytearray-mgetitem
-if (PyIndex_Check(_key)) {
+if (PepIndex_Check(_key)) {
     const Py_ssize_t _i = PyNumber_AsSsize_t(_key, PyExc_IndexError);
     if (_i < 0 || _i >= %CPPSELF.size()) {
         PyErr_SetString(PyExc_IndexError, "index out of bounds");
@@ -568,7 +568,7 @@ return %CONVERTTOPYTHON[QByteArray](ba);
 // @snippet qbytearray-mgetitem
 
 // @snippet qbytearray-msetitem
-if (PyIndex_Check(_key)) {
+if (PepIndex_Check(_key)) {
     Py_ssize_t _i = PyNumber_AsSsize_t(_key, PyExc_IndexError);
     if (_i == -1 && PyErr_Occurred())
         return -1;
