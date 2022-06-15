qtproject/pyside-pyside-setup

commit 108d82d2ca5323652a50706b37f829b9f0457367

Convert QVariant to Enum types
Before this patch, the conversion from QVariant->enum in Shiboken was
same as the conversion from QVariant->int. So, the user will have to
explicitly convert the returned integer to the original enum type.

With this patch, an enum type is considered as an Object type rather
than an int type. This enables the enum type to be considered as itself.

Added tests for all conversions of QVariant types.

Pick-to: 6.2
Fixes: PYSIDE-1798
Change-Id: I18acc08e1f59ac7df844444d0ad6950e1d70ab21
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/pyside6/PySide6/QtCore/typesystem_core_common.xml
@@ -258,7 +258,7 @@
             <add-conversion type="QByteArray" file="../glue/qtcore.cpp" snippet="conversion-qbytearray"/>
             <add-conversion type="PyFloat" check="PyFloat_CheckExact(%in)" file="../glue/qtcore.cpp" snippet="conversion-pyfloat"/>
             <add-conversion type="PyLong" check="PyLong_CheckExact(%in)"  file="../glue/qtcore.cpp" snippet="conversion-qlonglong"/>
-            <add-conversion type="SbkEnumType" file="../glue/qtcore.cpp" snippet="conversion-pyint"/>
+            <add-conversion type="SbkEnumType" file="../glue/qtcore.cpp" snippet="conversion-sbkobject"/>
             <add-conversion type="SbkObject" file="../glue/qtcore.cpp" snippet="conversion-sbkobject"/>
             <add-conversion type="PyDict" check="PyDict_CheckExact(%in)" file="../glue/qtcore.cpp" snippet="conversion-pydict"/>
             <add-conversion type="PyList" check="PyList_Check(%in)" file="../glue/qtcore.cpp" snippet="conversion-pylist"/>

sources/pyside6/tests/QtWidgets/qvariant_test.py
@@ -36,8 +36,9 @@ sys.path.append(os.fspath(Path(__file__).resolve().parents[1]))
 from init_paths import init_test_paths
 init_test_paths(False)
 
-from PySide6.QtWidgets import (QApplication, QComboBox, QGraphicsScene,
-    QGraphicsRectItem)
+from PySide6.QtCore import Qt, QObject
+from PySide6.QtWidgets import (QComboBox, QGraphicsScene,
+    QGraphicsRectItem, QComboBox)
 
 from helper.usesqapplication import UsesQApplication
 
@@ -86,6 +87,73 @@ class QGraphicsSceneOnQVariantTest(UsesQApplication):
         # PYSIDE-641
         self.combo.addItem("test", userData=Sequence())
         self.assertTrue(isinstance(self.combo.itemData(0), Sequence))
+
+
+class QVariantConversionTest(UsesQApplication):
+    """
+    Tests conversion from QVariant to supported type held by QVariant
+    """
+    def setUp(self):
+        super(QVariantConversionTest, self).setUp()
+        self.obj = QObject()
+
+    def tearDown(self):
+        del self.obj
+        super(QVariantConversionTest, self).tearDown()
+
+    def testEnum(self):
+        """
+        PYSIDE-1798: Test enum is obtained correctly when return through QVariant
+        """
+        self.obj.setProperty("test", Qt.SolidLine)
+        self.assertTrue(isinstance(self.obj.property("test"), Qt.PenStyle))
+        self.assertEqual(self.obj.property("test"), Qt.SolidLine)
+
+    def testString(self):
+        self.obj.setProperty("test", "test")
+        self.assertEqual(self.obj.property("test"), "test")
+        self.assertTrue(isinstance(self.obj.property("test"), str))
+
+    def testBytes(self):
+        byte_message = bytes("test", 'utf-8')
+        self.obj.setProperty("test", byte_message)
+        self.assertEqual(self.obj.property("test"), byte_message)
+        self.assertTrue(isinstance(self.obj.property("test"), bytes))
+
+    def testBasicTypes(self):
+        #bool
+        self.obj.setProperty("test", True)
+        self.assertEqual(self.obj.property("test"), True)
+        self.assertTrue(isinstance(self.obj.property("test"), bool))
+        #long
+        self.obj.setProperty("test", 2)
+        self.assertEqual(self.obj.property("test"), 2)
+        self.assertTrue(isinstance(self.obj.property("test"), int))
+        #float
+        self.obj.setProperty("test", 2.5)
+        self.assertEqual(self.obj.property("test"), 2.5)
+        self.assertTrue(isinstance(self.obj.property("test"), float))
+        #None
+        self.obj.setProperty("test", None)
+        self.assertEqual(self.obj.property("test"), None)
+
+    def testContainerTypes(self):
+        #list
+        self.obj.setProperty("test", [1,2,3])
+        self.assertEqual(self.obj.property("test"), [1,2,3])
+        self.assertTrue(isinstance(self.obj.property("test"), list))
+        #dict
+        self.obj.setProperty("test", {1: "one"})
+        self.assertEqual(self.obj.property("test"), {1: "one"})
+        self.assertTrue(isinstance(self.obj.property("test"), dict))
+
+    def testPyObject(self):
+        class Test:
+            pass
+        test = Test()
+        self.obj.setProperty("test", test)
+        self.assertEqual(self.obj.property("test"), test)
+        self.assertTrue(isinstance(self.obj.property("test"), Test))
 
 
 if __name__ == '__main__':

commit 20eb4f94c637d20461f1fef16942841803ada909

pyside: Modify QAbstractListModel.data(..) to consider enums
- The function under consideration returns a QVariant, which is used
internally within the C++ code. Sometimes, the value stored in this
QVariant can also be a Python enum/Shiboken enum. When such a case
occurs, the returned QVariant to the C++ side is a QVariant of Python
/Shiboken enum, which cannot be processed from C++ code.

- This is a regression from 108d82d2ca5323652a50706b37f829b9f0457367
which needs some special handling.

- The solution is the convert the python enum (Shiboken enum) to the
corresponding integer type, which is stored into the QVariant and
returned to the C++ side.

Pick-to: 6.3
Fixes: PYSIDE-1930
Change-Id: Id4e9a1caa5a318e2410e584502c6dc6e35e3a657
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/pyside6/PySide6/QtCore/typesystem_core_common.xml
@@ -1767,6 +1767,9 @@
       <include file-name="QStringList" location="global"/>
       <include file-name="QSize" location="global"/>
     </extra-includes>
+    <modify-function signature="data(const QModelIndex&amp;,int)const">
+      <inject-code class="native" position="end" file="../glue/qtcore.cpp" snippet="qabstractitemmodel_data"/>
+    </modify-function>
   </object-type>
   <value-type name="QUrlQuery" since="5.0"/>
 

sources/pyside6/PySide6/glue/qtcore.cpp
@@ -1700,3 +1700,14 @@ static PyObject *invokeMethodHelper(QObject *obj, const char *member, Qt::Connec
 %PYARG_0 = invokeMethodHelper(%1, %2, Qt::AutoConnection, %3, %4, %5, %6);
 // @snippet qmetaobject-invokemethod-return-arg
 
+// @snippet qabstractitemmodel_data
+::QVariant %0 ;
+if (Shiboken::Enum::check(%PYARG_0)) {
+    int in = %CONVERTTOCPP[int](%PYARG_0);
+    %0 = QVariant(in);
+}
+else
+{
+    pythonToCpp(pyResult, &cppResult);
+}
+// @snippet qabstractitemmodel_data