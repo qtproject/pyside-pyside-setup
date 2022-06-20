qtproject/pyside-pyside-setup

commit 20eb4f94c637d20461f1fef16942841803ada909

pyside: Modify QAbstractListModel.data(..) to consider enums
- The function under consideration returns a QVariant, which is used
internally within the C++ code. Sometimes, the value stored in this
QVariant can also be a Python enum/Shiboken enum. When such a case
occurs, the returned QVariant to the C++ side is a QVariant of Python
/Shiboken enum, which cannot be processed from C++ code.

- This is a regression from 108d82d2ca5323652a50706b37f829b9f0457367
which needs some special handling.

- The solution is the convert the python enum (Shiboken enum) to the
corresponding integer type, which is stored into the QVariant and
returned to the C++ side.

Pick-to: 6.3
Fixes: PYSIDE-1930
Change-Id: Id4e9a1caa5a318e2410e584502c6dc6e35e3a657
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/pyside6/PySide6/glue/qtcore.cpp
@@ -1700,3 +1700,14 @@ static PyObject *invokeMethodHelper(QObject *obj, const char *member, Qt::Connec
 %PYARG_0 = invokeMethodHelper(%1, %2, Qt::AutoConnection, %3, %4, %5, %6);
 // @snippet qmetaobject-invokemethod-return-arg
 
+// @snippet qabstractitemmodel_data
+::QVariant %0 ;
+if (Shiboken::Enum::check(%PYARG_0)) {
+    int in = %CONVERTTOCPP[int](%PYARG_0);
+    %0 = QVariant(in);
+}
+else
+{
+    pythonToCpp(pyResult, &cppResult);
+}
+// @snippet qabstractitemmodel_data

sources/pyside6/PySide6/QtCore/typesystem_core_common.xml
@@ -1767,6 +1767,9 @@
       <include file-name="QStringList" location="global"/>
       <include file-name="QSize" location="global"/>
     </extra-includes>
+    <modify-function signature="data(const QModelIndex&amp;,int)const">
+      <inject-code class="native" position="end" file="../glue/qtcore.cpp" snippet="qabstractitemmodel_data"/>
+    </modify-function>
   </object-type>
   <value-type name="QUrlQuery" since="5.0"/>
 

commit 46b61c8e5fb69fd098eeb0609af0cfef84c7f60e

fix if else in qabstractitemmodel_data
- adapted to follow Qt guidelines
- regression from 20eb4f94c637d20461f1fef16942841803ada909

Task-number: PYSIDE-1930
Change-Id: Idfd81e2f2f2f54937863b77951b86506f89a9a4a
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/pyside6/PySide6/glue/qtcore.cpp
@@ -1705,9 +1705,7 @@ static PyObject *invokeMethodHelper(QObject *obj, const char *member, Qt::Connec
 if (Shiboken::Enum::check(%PYARG_0)) {
     int in = %CONVERTTOCPP[int](%PYARG_0);
     %0 = QVariant(in);
-}
-else
-{
+} else {
     pythonToCpp(pyResult, &cppResult);
 }
 // @snippet qabstractitemmodel_data