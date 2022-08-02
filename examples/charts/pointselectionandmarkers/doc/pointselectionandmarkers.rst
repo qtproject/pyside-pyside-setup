1a8db65a6efc8d8d2404f11c7b66baf7bf722ddb
Keith Kyzivat <keith.kyzivat@qt.io>
2022-08-02 15:57:17 -0400

    PySide6-examples: Charts lightMarker and selection
    Provide a python implementation of the pointselectionandmarkers Charts
    example.
    
    Pick-to: 6.4
    Task-number: PYSIDE-841
    Change-Id: I9a5295cb89f9e1a400e21f6dbd90d1c8e2827e31
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

---
+++ examples/charts/pointselectionandmarkers/doc/pointselectionandmarkers.rst
@@ -0,0 +1,72 @@
+.. role:: py(code)
+   :language: python
+
+Light Markers and Points Selection Example
+==========================================
+
+The Light Markers and Points Selection example shows how to use light markers
+and point selections in a series.
+
+.. image:: pointselectionandmarkers.png
+   :width: 90%
+   :align: center
+   :alt: QChart with Light Markers shown
+
+Creating the chart and its elements
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+We start by creating a series, filling it with the data, and enabling the light marker and point selection features.
+It is important not to set points visibility to :py:`True`, because light markers functionality is an independent feature and setting both would result in undesired behavior.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :linenos:
+   :lineno-start: 20
+   :lines: 20-42
+   :emphasize-lines: 2-12
+
+Then we create the :py:`QChart`, the :py:`QChartview` and the control widget with its layout to arrange customization elements.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :lineno-start: 44
+   :lines: 44-53
+   :emphasize-lines: 1,6,9
+
+Creating UI for configuring the chart
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The next step is where we create user interface elements that allow customizing the chart, including setting light marker and selection marker images.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :linenos:
+   :lineno-start: 54
+   :lines: 54-57
+
+We create the label for the marker selection combobox and fill the combobox with the items. We then provide functionality to the combobox, allowing the user's selection to set the desired light marker image. As light markers are enabled and disabled by setting a valid QImage or setting an empty :py:`QImage()`, we need to make sure that if the user does not wish unselected points to be displayed, we do not actually set the light marker image.
+If checking isn't performed, a new :py:`QImage` will be set as the light marker and unselected points will be visible even though it has been switched off.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :linenos:
+   :lineno-start: 59
+   :lines: 59-67
+   :emphasize-lines: 1-3
+
+Almost the same procedure applies to the selected point light marker and line color. The only difference is that there is no need to check the visibility of unselected points as it doesn't affect the functionality.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :linenos:
+   :lineno-start: 70
+   :lines: 70-85
+
+A small difference comes with changing visibility of unselected points. As it was mentioned before, making light markers invisible is achieved by setting the light marker to an empty :py:`QImage()`. That is why, depending on checkbox state, selected point light marker is set to an empty :py:`QImage` or to the light marker extracted from the current index of the corresponding combobox.
+
+.. literalinclude:: ../../../../examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
+   :linenos:
+   :lineno-start: 88
+   :lines: 88-97
+   :emphasize-lines: 5-6
+
+The final part is to lay out the widgets within the main widget and set the main window size.
+
+Usage
+-----
+To use this example, change any of the comboboxes and checkboxes controlling the markers, line color, and unselected point visibility on the right. Then try clicking on points in the chart to select or deselect them.
---
+++ examples/charts/pointselectionandmarkers/pointselectionandmarkers.py
@@ -0,0 +1,127 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+"""PySide6 port of the Light Markers Points Selection example from Qt v6.2"""
+import sys
+
+from PySide6.QtCore import Slot, QPointF, Qt
+from PySide6.QtCharts import QChart, QChartView, QSplineSeries
+from PySide6.QtGui import QPainter, QImage
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QComboBox, QCheckBox, QLabel, QHBoxLayout
+
+import utilities as Utilities
+
+if __name__ == "__main__":
+
+    a = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Light Markers and Points Selection")
+
+    marker_size = 20.
+    series = QSplineSeries()
+    series.append([QPointF(0, 0),
+                    QPointF(0.5, 2.27),
+                    QPointF(1.5, 2.2),
+                    QPointF(3.3, 1.7),
+                    QPointF(4.23, 3.1),
+                    QPointF(5.3, 2.3),
+                    QPointF(6.47, 4.1)])
+    series.setMarkerSize(marker_size)
+    series.setLightMarker(Utilities.default_light_marker(marker_size))
+    series.setSelectedLightMarker(Utilities.default_selected_light_marker(marker_size))
+
+    @Slot(QPointF)
+    def toggle_selection(point):
+        try:
+            index = series.points().index(point)
+            if index != -1:
+                series.toggleSelection([index])
+        except ValueError:
+            pass
+
+    series.clicked.connect(toggle_selection)
+
+    chart = QChart()
+    chart.addSeries(series)
+    chart.createDefaultAxes()
+    chart.legend().setVisible(False)
+
+    chart_view = QChartView(chart)
+    chart_view.setRenderHint(QPainter.Antialiasing)
+
+    control_widget = QWidget(window)
+    control_layout = QGridLayout(control_widget)
+    char_point_combobox = QComboBox()
+    char_point_selected_combobox = QComboBox()
+    line_color_combobox = QComboBox()
+    show_unselected_points_checkbox = QCheckBox()
+
+    @Slot(int)
+    def set_light_marker(index):
+        if show_unselected_points_checkbox.isChecked():
+            series.setLightMarker(Utilities.get_point_representation(
+                Utilities.point_type(index), marker_size))
+
+    char_point = QLabel("Char point: ")
+    char_point_combobox.addItems(["Red rectangle", "Green triangle", "Orange circle"])
+    char_point_combobox.currentIndexChanged.connect(set_light_marker)
+
+
+    @Slot(int)
+    def set_selected_light_marker(index):
+        series.setSelectedLightMarker(Utilities.get_selected_point_representation(Utilities.selected_point_type(index), marker_size))
+
+    char_point_selected = QLabel("Char point selected: ")
+    char_point_selected_combobox.addItems(["Blue triangle", "Yellow rectangle", "Lavender circle"])
+    char_point_selected_combobox.currentIndexChanged.connect(set_selected_light_marker)
+
+
+    @Slot(int)
+    def set_line_color(index):
+        series.setColor(Utilities.make_line_color(Utilities.line_color(index)))
+
+    line_color_label = QLabel("Line color: ")
+    line_color_combobox.addItems(["Blue", "Black", "Mint"])
+    line_color_combobox.currentIndexChanged.connect(set_line_color)
+
+
+    @Slot(int)
+    def display_unselected_points(checkbox_state):
+        if checkbox_state:
+            series.setLightMarker(Utilities.get_point_representation(Utilities.point_type(char_point_combobox.currentIndex()), marker_size))
+        else:
+            series.setLightMarker(QImage())
+
+    show_unselected_points_label = QLabel("Display unselected points: ")
+    show_unselected_points_checkbox.setChecked(True)
+    show_unselected_points_checkbox.stateChanged.connect(display_unselected_points)
+
+
+    control_label = QLabel("Marker and Selection Controls")
+    control_label.setAlignment(Qt.AlignHCenter)
+    control_label_font = control_label.font()
+    control_label_font.setBold(True)
+    control_label.setFont(control_label_font)
+    control_layout.addWidget(control_label, 0, 0, 1, 2)
+    control_layout.addWidget(char_point, 1, 0)
+    control_layout.addWidget(char_point_combobox, 1, 1)
+
+    control_layout.addWidget(char_point_selected, 2, 0)
+    control_layout.addWidget(char_point_selected_combobox, 2, 1)
+
+    control_layout.addWidget(line_color_label, 3, 0)
+    control_layout.addWidget(line_color_combobox, 3, 1)
+
+    control_layout.addWidget(show_unselected_points_label, 4, 0)
+    control_layout.addWidget(show_unselected_points_checkbox, 4, 1, 1, 2)
+    control_layout.setRowStretch(5, 1)
+
+    main_widget = QWidget(window)
+    main_layout = QHBoxLayout(main_widget)
+    main_layout.addWidget(chart_view)
+    main_layout.addWidget(control_widget)
+
+    window.setCentralWidget(main_widget)
+    window.resize(1080, 720)
+    window.show()
+    sys.exit(a.exec())
