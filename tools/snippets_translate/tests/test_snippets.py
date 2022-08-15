qtproject/pyside-pyside-setup

commit c4a266e38fe5bdce707ad6b123fa88bb4f10dff3

snippets_translate: Add a way of using snippets from Python
Split a helper off the snippet extraction function taking the comment
pattern and let it return a dict by id to make it possible to replace
snippets by id.

Prototypically use it for the  modelview tutorial.

Fixes: PYSIDE-1984
Task-number: PYSIDE-1952
Pick-to: 6.3 6.2
Change-Id: I05dbc3e36825761fe2968d6507880cd6f588682d
Reviewed-by: Christian Tismer <tismer@stackless.com>

tools/snippets_translate/snippets_translate.pyproject
@@ -1,3 +1,3 @@
 {
-    "files": ["main.py", "converter.py", "handlers.py", "tests/test_converter.py"]
+    "files": ["main.py", "converter.py", "handlers.py", "override.py", "tests/test_converter.py"]
 }

examples/widgets/tutorials/modelview/1_readonly.py
@@ -9,6 +9,7 @@ from PySide6.QtWidgets import QApplication, QTableView
 """PySide6 port of the widgets/tutorials/modelview/1_readonly example from Qt v6.x"""
 
 
+#! [1]
 class MyModel(QAbstractTableModel):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -25,6 +26,7 @@ class MyModel(QAbstractTableModel):
             column = index.column() + 1
             return f"Row{row}, Column{column}"
         return None
+#! [1]
 
 
 if __name__ == '__main__':

examples/widgets/tutorials/modelview/2_formatting.py
@@ -20,6 +20,7 @@ class MyModel(QAbstractTableModel):
     def columnCount(self, parent=None):
         return 3
 
+#! [1]
     def data(self, index, role=Qt.DisplayRole):
         row = index.row()
         col = index.column()
@@ -52,6 +53,7 @@ class MyModel(QAbstractTableModel):
                 return Qt.Checked
 
         return None
+#! [1]
 
 
 if __name__ == '__main__':

examples/widgets/tutorials/modelview/3_changingmodel.py
@@ -10,12 +10,14 @@ from PySide6.QtWidgets import QApplication, QTableView
 
 
 class MyModel(QAbstractTableModel):
+#! [1]
     def __init__(self, parent=None):
         super().__init__(parent)
         self._timer = QTimer(self)
         self._timer.setInterval(1000)
         self._timer.timeout.connect(self.timer_hit)
         self._timer.start()
+#! [1]
 
     def rowCount(self, parent=None):
         return 2
@@ -23,20 +25,23 @@ class MyModel(QAbstractTableModel):
     def columnCount(self, parent=None):
         return 3
 
+#! [2]
     def data(self, index, role=Qt.DisplayRole):
         row = index.row()
         col = index.column()
         if role == Qt.DisplayRole and row == 0 and col == 0:
             return QTime.currentTime().toString()
         return None
+#! [2]
 
+#! [3]
     @Slot()
     def timer_hit(self):
         # we identify the top left cell
         top_left = self.createIndex(0, 0)
         # emit a signal to make the view reread identified data
         self.dataChanged.emit(top_left, top_left, [Qt.DisplayRole])
-
+#! [3]
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

examples/widgets/tutorials/modelview/4_headers.py
@@ -26,10 +26,12 @@ class MyModel(QAbstractTableModel):
             return f"Row{row}, Column{column}"
         return None
 
+#! [1]
     def headerData(self, section, orientation, role):
         if role == Qt.DisplayRole and orientation == Qt.Horizontal:
             return ["first", "second", "third"][section]
         return None
+#! [1]
 
 
 if __name__ == '__main__':

examples/widgets/tutorials/modelview/5_edit.py
@@ -33,6 +33,7 @@ class MyModel(QAbstractTableModel):
             return self._grid_data[index.row()][index.column()]
         return None
 
+#! [1]
     def setData(self, index, value, role):
         if role != Qt.EditRole or not self.checkIndex(index):
             return False
@@ -42,9 +43,12 @@ class MyModel(QAbstractTableModel):
         result = " ".join(chain(*self._grid_data))
         self.editCompleted.emit(result)
         return True
+#! [1]
 
+#! [2]
     def flags(self, index):
         return Qt.ItemIsEditable | super().flags(index)
+#! [2]
 
 
 class MainWindow(QMainWindow):

examples/widgets/tutorials/modelview/6_treeview.py
@@ -9,7 +9,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QTreeView
 
 """PySide6 port of the widgets/tutorials/modelview/6_treeview example from Qt v6.x"""
 
-
+#! [1]
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -32,6 +32,7 @@ class MainWindow(QMainWindow):
     def prepare_row(self, first, second, third):
         return [QStandardItem(first), QStandardItem(second),
                 QStandardItem(third)]
+#! [1]
 
 
 if __name__ == '__main__':

examples/widgets/tutorials/modelview/7_selections.py
@@ -9,7 +9,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QTreeView
 
 """PySide6 port of the widgets/tutorials/modelview/7_selections example from Qt v6.x"""
 
-
+#! [1]
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -45,7 +45,9 @@ class MainWindow(QMainWindow):
         # selection changes shall trigger a slot
         selection_model = self._tree_view.selectionModel()
         selection_model.selectionChanged.connect(self.selection_changed_slot)
+#! [1]
 
+#! [2]
     @Slot(QItemSelection, QItemSelection)
     def selection_changed_slot(self, new_selection, old_selection):
         # get the text of the selected item
@@ -58,6 +60,7 @@ class MainWindow(QMainWindow):
             seek_root = seek_root.parent()
             hierarchy_level += 1
         self.setWindowTitle(f"{selected_text}, Level {hierarchy_level}")
+#! [2]
 
 
 if __name__ == '__main__':

commit 780c7a9314672da4c917650f0a9548a537515fd8

snippets_translate: Fix the tests
Change c4a266e38fe5bdce707ad6b123fa88bb4f10dff3 broke
the test by changing the function values and signature.

Pick-to: 6.3 6.2
Task-number: PYSIDE-1984
Task-number: PYSIDE-2030
Change-Id: I88412f3f4398aa7fd0748d9ad3b7ba2a75cf9ff0
Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>
Reviewed-by: Christian Tismer <tismer@stackless.com>

tools/snippets_translate/snippets_translate.pyproject
@@ -1,3 +1,4 @@
 {
-    "files": ["main.py", "converter.py", "handlers.py", "override.py", "tests/test_converter.py"]
+    "files": ["main.py", "converter.py", "handlers.py", "override.py",
+              "tests/test_converter.py", "tests/test_snippets.py"]
 }

tools/snippets_translate/tests/test_snippets.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-from main import get_snippets, get_snippet_ids
+from main import _get_snippets, get_snippet_ids, CPP_SNIPPET_PATTERN
 
 
 def test_stacking():
@@ -12,13 +12,23 @@ def test_stacking():
         "//! [C] //! [A] ",
         "//! [B] //! [D] //! [E]",
     ]
-    snippets = get_snippets(lines)
+    snippets = _get_snippets(lines, CPP_SNIPPET_PATTERN)
     assert len(snippets) == 5
-    assert len(snippets[0]) == 4  # A starts at line 0 and ends at line 3
-    assert len(snippets[1]) == 5  # B starts at line 0 and ends at line 4
-    assert len(snippets[2]) == 3  # C starts at line 1 and ends at line 3
-    assert len(snippets[3]) == 4  # D starts at line 1 and ends at line 4
-    assert len(snippets[4]) == 4  # E starts at line 1 and ends at line 4
+
+    snippet_a = snippets["A"]
+    assert len(snippet_a) == 4  # A starts at line 0 and ends at line 3
+
+    snippet_b = snippets["B"]
+    assert len(snippet_b) == 5  # B starts at line 0 and ends at line 4
+
+    snippet_c = snippets["C"]
+    assert len(snippet_c) == 3  # C starts at line 1 and ends at line 3
+
+    snippet_d = snippets["D"]
+    assert len(snippet_d) == 4  # D starts at line 1 and ends at line 4
+
+    snippet_e = snippets["E"]
+    assert len(snippet_e) == 4  # E starts at line 1 and ends at line 4
 
 
 def test_nesting():
@@ -31,17 +41,20 @@ def test_nesting():
         "//! [C]",
         "//! [B]",
     ]
-    snippets = get_snippets(lines)
+    snippets = _get_snippets(lines, CPP_SNIPPET_PATTERN)
     assert len(snippets) == 3
 
-    assert len(snippets[0]) == 5
-    assert snippets[0] == lines[:5]
+    snippet_a = snippets["A"]
+    assert len(snippet_a) == 5
+    assert snippet_a == lines[:5]
 
-    assert len(snippets[1]) == 6
-    assert snippets[1] == lines[1:]
+    snippet_b = snippets["B"]
+    assert len(snippet_b) == 6
+    assert snippet_b == lines[1:]
 
-    assert len(snippets[2]) == 4
-    assert snippets[2] == lines[2:6]
+    snippet_c = snippets["C"]
+    assert len(snippet_c) == 4
+    assert snippet_c == lines[2:6]
 
 
 def test_overlapping():
@@ -57,17 +70,20 @@ def test_overlapping():
         "posttext",
         "//! [C]",
     ]
-    snippets = get_snippets(lines)
+    snippets = _get_snippets(lines, CPP_SNIPPET_PATTERN)
     assert len(snippets) == 3
 
-    assert len(snippets[0]) == 4
-    assert snippets[0] == lines[1:5]
+    snippet_a = snippets["A"]
+    assert len(snippet_a) == 4
+    assert snippet_a == lines[1:5]
 
-    assert len(snippets[1]) == 7
-    assert snippets[1] == lines[3:]
+    snippet_c = snippets["C"]
+    assert len(snippet_c) == 7
+    assert snippet_c == lines[3:]
 
-    assert len(snippets[2]) == 4
-    assert snippets[2] == lines[4:8]
+    snippet_b = snippets["B"]
+    assert len(snippet_b) == 4
+    assert snippet_b == lines[4:8]
 
 
 def test_snippets():
@@ -82,20 +98,27 @@ def test_snippets():
         "posttext"
     ]
 
-    snippets = get_snippets(lines)
+    snippets = _get_snippets(lines, CPP_SNIPPET_PATTERN)
     assert len(snippets) == 2
 
-    assert len(snippets[0]) == 3
-    assert snippets[0] == lines[1:4]
+    snippet_a = snippets["A"]
+    assert len(snippet_a) == 3
+    assert snippet_a == lines[1:4]
 
-    assert len(snippets[1]) == 4
-    assert snippets[1] == lines[3:7]
+    snippet_b = snippets["B"]
+    assert len(snippet_b) == 4
+    assert snippet_b == lines[3:7]
 
 
 def test_snippet_ids():
-    assert get_snippet_ids("") == []
-    assert get_snippet_ids("//! ") == []  # Invalid id
-    assert get_snippet_ids("//! [some name]") == ["some name"]
-    assert get_snippet_ids("//! [some name] [some other name]") == ["some name"]
-    assert get_snippet_ids("//! [some name] //! ") == ["some name"]  # Invalid id
-    assert get_snippet_ids("//! [some name] //! [some other name]") == ["some name", "some other name"]
+    assert get_snippet_ids("", CPP_SNIPPET_PATTERN) == []
+    assert get_snippet_ids("//! ",
+                           CPP_SNIPPET_PATTERN) == []  # Invalid id
+    assert get_snippet_ids("//! [some name]",
+                           CPP_SNIPPET_PATTERN) == ["some name"]
+    assert get_snippet_ids("//! [some name] [some other name]",
+                           CPP_SNIPPET_PATTERN) == ["some name"]
+    assert get_snippet_ids("//! [some name] //! ",
+                           CPP_SNIPPET_PATTERN) == ["some name"]  # Invalid id
+    assert get_snippet_ids("//! [some name] //! [some other name]",
+                           CPP_SNIPPET_PATTERN) == ["some name", "some other name"]