479819fe8900cea1decb9c59956dd2bf25495382
Christian Tismer <tismer@stackless.com>
2022-10-08 17:38:02 +0200

    PySide6: Fix enum to QVariant conversion for the new enums
    Use the existing library function to handle new enums
    correctly.
    
    Amends 20eb4f94c637d20461f1fef16942841803ada909,
    66e9e9e0674dff4ac0715faf17bf039eeb287df7 .
    
    Change-Id: I93b4ffe60d0386cbeb4a7e84782755e77584dc32
    Task-number: PYSIDE-1930
    Task-number: PYSIDE-1870
    Task-number: PYSIDE-1735
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    Reviewed-by: Christian Tismer <tismer@stackless.com>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit 5d65d73f2006e94006c999249d88898544850cb4)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/PySide6/glue/qtcore.cpp
+++ sources/pyside6/PySide6/glue/qtcore.cpp
@@ -1593,10 +1593,8 @@ QDebug(&result).nospace() << "<PySide6.QtCore.QEvent(" << %CPPSELF->type() << ")
 // @snippet repr-qevent
 
 // @snippet qmetaproperty_write_enum
-if (Shiboken::Enum::check(%PYARG_2)) {
-    int in = %CONVERTTOCPP[int](%PYARG_2);
-    cppArg1 = QVariant(in);
-}
+if (Shiboken::Enum::check(%PYARG_2))
+    cppArg1 = QVariant(int(Shiboken::Enum::getValue(%PYARG_2)));
 // @snippet qmetaproperty_write_enum
 
 // @snippet qdatastream-read-bytes
@@ -1620,13 +1618,11 @@ if (dataChar == nullptr) {
 // @snippet qloggingcategory_to_cpp
 
 // @snippet qabstractitemmodel_data
-::QVariant %0 ;
-if (Shiboken::Enum::check(%PYARG_0)) {
-    int in = %CONVERTTOCPP[int](%PYARG_0);
-    %0 = QVariant(in);
-} else {
+::QVariant %0;
+if (Shiboken::Enum::check(%PYARG_0))
+    %0 = QVariant(int(Shiboken::Enum::getValue(%PYARG_0)));
+else
     pythonToCpp(pyResult, &cppResult);
-}
 // @snippet qabstractitemmodel_data
 
 // @snippet keycombination-from-keycombination
