55993006f96e5d9d668b33eb4befa31f50e931a4
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-10-14 10:44:13 +0200

    Deploy Tool: Split classes and functions
    - As deploy.py was getting bigger, each class is split into
      respective modules inside psyide-tools/deploy/
    
    Pick-to: 6.4.0
    Task-number: PYSIDE-1912
    Change-Id: I465a25773343cb842cbfd603941476ed6d575321
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>

---
+++ sources/pyside-tools/deploy/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+from .commands import run_command
+from .nuitka_helper import Nuitka
+from .config import Config
+from .python_helper import PythonExecutable
