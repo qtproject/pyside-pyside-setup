98014a091c4fa3b8076fec8e25ef4a8972fd0d0c
Friedemann Kleint <Friedemann.Kleint@qt.io>
2022-10-19 09:27:10 +0200

    Fix crashes when not implementing abstract method
    Ensure the PyErr_ methods are called with GIL held.
    
    Fixes: PYSIDE-2089
    Change-Id: Ibe3aa95e17875e3acc47094b1f9211dacf1b59c5
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>
    (cherry picked from commit fb00686b6df1cb7514c038e429c12c2c5f3302d6)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/generator/shiboken/cppgenerator.h
+++ sources/shiboken6/generator/shiboken/cppgenerator.h
@@ -82,7 +82,7 @@ private:
     void writeVirtualMethodCppCall(TextStream &s, const AbstractMetaFunctionCPtr &func,
                                    const QString &funcName, const QList<CodeSnip> &snips,
                                    const AbstractMetaArgument *lastArg, const TypeEntry *retType,
-                                   const QString &returnStatement) const;
+                                   const QString &returnStatement, bool hasGil) const;
     static QString virtualMethodReturn(TextStream &s, const ApiExtractorResult &api,
                                        const AbstractMetaFunctionCPtr &func,
                                        const FunctionModificationList &functionModifications);
