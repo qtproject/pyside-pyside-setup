f9ca52cc893270aab1725c286dee6b28fd89ab08
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-10-25 14:13:59 +0200

    Deploy Tool Fix: os.fspath instead of pathlib
    - for subprocess calls use os.fspath instead of pathlib
    
    Change-Id: I7f0a47d4c8a0d9daa55d6b432f9bac9ed2019a91
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 3a3eb76b59615dd8db2a9220f26c46cf3d839b59)

--- sources/pyside-tools/deploy/python_helper.py
+++ sources/pyside-tools/deploy/python_helper.py
@@ -20,7 +20,8 @@ class PythonExecutable:
         self.dry_run = dry_run
         if create_venv:
             self.__create_venv()
-        self.nuitka = Nuitka(nuitka=[self.exe, "-m", "nuitka"])
+
+        self.nuitka = Nuitka(nuitka=[os.fspath(self.exe), "-m", "nuitka"])
 
     @property
     def exe(self):
