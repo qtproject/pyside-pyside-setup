d40bb5edc0ca96159ac026a65235306c513b3143
Friedemann Kleint <Friedemann.Kleint@qt.io>
2022-10-28 15:39:46 +0200

    Simplify enum signal test
    Amends df87bd0d6c55e0ab11aa68f7db99337860134510.
    
    Task-number: PYSIDE-2095
    Pick-to: 6.3 6.4
    Change-Id: I0a18abc35b1f6519fd8b82f98410240d8907652d
    Reviewed-by: Christian Tismer <tismer@stackless.com>

--- sources/pyside6/tests/signals/signal_newenum_test.py
+++ sources/pyside6/tests/signals/signal_newenum_test.py
@@ -10,51 +10,40 @@ sys.path.append(os.fspath(Path(__file__).resolve().parents[1]))
 from init_paths import init_test_paths
 init_test_paths(False)
 
-from PySide6.QtCore import QCoreApplication, QTimer, QEvent, Qt, Signal
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtGui import QKeyEvent
+from PySide6.QtCore import QObject, Qt, Slot, Signal
 
 
-class Window(QWidget):
-    test_sig1 = Signal(Qt.AlignmentFlag)
-    test_sig2 = Signal(Qt.AlignmentFlag, str)
+class Receiver(QObject):
+    def __init__(self):
+        super().__init__()
+        self.result = 0
+
+    @Slot(Qt.Alignment, str)
+    def handler(self, e, s):
+        print('handler', e, "type=", type(e).__name__, s)
+        self.result += 1
+
+
+class Sender(QObject):
+    test_sig = Signal(Qt.AlignmentFlag, str)
 
     def __init__(self):
         super().__init__()
 
-    def keyPressEvent(self, e):
-        self.test_sig1.emit(Qt.AlignLeft)
-        self.test_sig2.emit(Qt.AlignLeft, "bla")
-
-    def handler1(self, e):
-        print('\nhandler1', e, "type=", type(e).__name__)
-        self.result += 1
-
-    def handler2(self, e, s):
-        print('handler2', e, "type=", type(e).__name__, s)
-        self.result += 2
+    def emit_test_sig(self):
+        self.test_sig.emit(Qt.AlignLeft, "bla")
 
 
 class TestSignalNewEnum(unittest.TestCase):
+    """Test for PYSIDE-2095, signals with new enums in Python 3.11."""
 
     def testIt(self):
-        app = QApplication()
-        self.window = window = Window()
-        window.result = 0
-        window.show()
+        sender = Sender()
+        receiver = Receiver()
+        sender.test_sig.connect(receiver.handler)
 
-        window.test_sig1.connect(window.handler1)
-        window.test_sig2.connect(window.handler2)
-
-        QTimer.singleShot(0, self.sendKbdEvent)
-        QTimer.singleShot(100, app.quit)
-        app.exec()
-
-        self.assertEqual(window.result, 3)
-
-    def sendKbdEvent(self):
-        ev = QKeyEvent(QEvent.KeyPress, Qt.Key_A, Qt.NoModifier, 'a')
-        QCoreApplication.sendEvent(self.window, ev)
+        sender.emit_test_sig()
+        self.assertEqual(receiver.result, 1)
 
 
 if __name__ == '__main__':
