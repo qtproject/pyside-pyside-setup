2ac52079a9289049f965b10cad7c9e6d7966b72e
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2022-10-31 11:12:59 +0100

    Deploy: Use type hints from typing
    - list[Path] to List[Path]
    
    Change-Id: Ie8c9d8699ded5759763ec1c6ef643a873f6b3977
    Reviewed-by: Christian Tismer <tismer@stackless.com>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 15c994b665845560e5f4425355a8fe9dd8310127)

--- sources/pyside-tools/deploy/nuitka_helper.py
+++ sources/pyside-tools/deploy/nuitka_helper.py
@@ -4,7 +4,9 @@
 import os
 import sys
 from pathlib import Path
-from .utils import run_command
+from typing import List
+
+from . import run_command
 
 
 class Nuitka:
@@ -16,7 +18,7 @@ class Nuitka:
         self.nuitka = nuitka
 
     def create_executable(
-        self, source_file: Path, extra_args: str, qml_files: list[Path], dry_run: bool
+        self, source_file: Path, extra_args: str, qml_files: List[Path], dry_run: bool
     ):
         extra_args = extra_args.split()
         qml_args = []
