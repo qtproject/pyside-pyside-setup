f091bb7237c4bcd01b1e173bbab46803a68b37a6
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-03-15 09:43:19 +0100

    graphgallery example: Fix license
    Amends 558adfebf001bce0da4b972626fd8a889dd7e757.
    
    Task-number: PYSIDE-2206
    Task-number: QTBUG-110698
    Change-Id: I36feacf585d5908fa393f5560bc704e8fe52731c
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>

--- examples/datavisualization/graphgallery/main.py
+++ examples/datavisualization/graphgallery/main.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2023 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 """PySide6 port of the Qt DataVisualization graphgallery example from Qt v6.x"""
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 import os
 import sys
