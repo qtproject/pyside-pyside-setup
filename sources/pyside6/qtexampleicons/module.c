559fb65547236694dc04973f7fb6b39bac75d158
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-03-17 13:30:54 +0100

    Add a Python module for example assets
    Task-number: PYSIDE-2206
    Task-number: QTBUG-110428
    Change-Id: I47b9ae8fa5357a9d7517d046ba47bd3baf3e91e3
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>

---
+++ sources/pyside6/qtexampleicons/module.c
@@ -0,0 +1,39 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+#include <Python.h>
+
+#if defined _WIN32
+#  define MODULE_EXPORT __declspec(dllexport)
+#else
+#  define MODULE_EXPORT __attribute__ ((visibility("default")))
+#endif
+
+static PyMethodDef QtExampleIconsMethods[] = {
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef moduleDef = {
+    /* m_base     */ PyModuleDef_HEAD_INIT,
+    /* m_name     */ "QtExampleIcons",
+    /* m_doc      */ NULL,
+    /* m_size     */ -1,
+    /* m_methods  */ QtExampleIconsMethods,
+    /* m_reload   */ NULL,
+    /* m_traverse */ NULL,
+    /* m_clear    */ NULL,
+    /* m_free     */ NULL
+};
+
+MODULE_EXPORT PyObject *PyInit_QtExampleIcons(void)
+{
+    return PyModule_Create(&moduleDef);
+}
+
+int main(int argc, char *argv[])
+{
+    Py_SetProgramName(L"module-test");
+    Py_Initialize();
+    PyInit_QtExampleIcons();
+    return 0;
+}
