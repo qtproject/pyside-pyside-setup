qtproject/pyside-pyside-setup

commit 43109854a2966afe2e0cf29961157a6f54d5775c

PySideTools: install tool only if corresponding Qt tool exists
- PySide tools which are wrappers around Qt tools are now only installed
  if the corresponding Qt tool exists.
- PySide6 entry points for the Qt tool are now only created if the Qt
  tool exists in the corresponding Qt installation.
- Incase the console entrypoint still exists and the corresponding Qt
  tool does not exist, the tool would exit stating that the Qt tool does
  not exist.
  eg: 'pyside6-uic' is run and 'uic' does not exist. The the tool
      outputs that the 'uic' does not exist.

  Ideally as per this change, PySide6 entrypoints for missing Qt tools
  should not exist at all.
- versions.py deleted and contents moved to __init__.py.
- Adds warning from Python incase if the tool does not exist. This
  is in addition to the CMake warning.

Fixes: PYSIDE-2097
Pick-to: 6.4 6.2
Change-Id: I3f1b26d103679f7425d9ad85dfed8d9ad17f6fbf
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

build_scripts/__init__.py
@@ -1,0 +1,27 @@
+# Copyright (C) 2022 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+PYSIDE = 'pyside6'
+PYSIDE_MODULE = 'PySide6'
+SHIBOKEN = 'shiboken6'
+
+PYSIDE_PYTHON_TOOLS = ["metaobjectdump",
+                       "deploy",
+                       "project",
+                       "qml",
+                       "qtpy2cpp",
+                       "genpyi"]
+PYSIDE_LINUX_BIN_TOOLS = ["lupdate",
+                          "lrelease",
+                          "qmllint",
+                          "qmlformat",
+                          "qmlls",
+                          "assistant",
+                          "designer",
+                          "linguist"]
+PYSIDE_LINUX_LIBEXEC_TOOLS = ["uic",
+                              "rcc",
+                              "qmltyperegistrar",
+                              "qmlimportscanner"]
+# all Qt tools are in 'bin' folder in Windows
+PYSIDE_WINDOWS_BIN_TOOLS = PYSIDE_LINUX_LIBEXEC_TOOLS + PYSIDE_LINUX_BIN_TOOLS

build_scripts/utils.py
@@ -18,6 +18,8 @@ from pathlib import Path
 from textwrap import dedent, indent
 
 from .log import log
+from . import (PYSIDE_PYTHON_TOOLS, PYSIDE_LINUX_BIN_TOOLS, PYSIDE_LINUX_LIBEXEC_TOOLS,
+               PYSIDE_WINDOWS_BIN_TOOLS)
 
 try:
     # Using the distutils implementation within setuptools
@@ -34,6 +36,7 @@ try:
 except NameError:
     WindowsError = None
 
+
 def which(name):
     """
     Like shutil.which, but accepts a string or a PathLike and returns a Path
@@ -49,6 +52,7 @@ def which(name):
     except TypeError as e:
         log.error(f"{name} was not found in PATH: {e}")
     return path
+
 
 def is_64bit():
     return sys.maxsize > 2147483647
@@ -1350,3 +1354,47 @@ def parse_cmake_project_message_info(output):
             value = found.group(3).strip()
             result[category][key] = str(value)
     return result
+
+
+def available_pyside_tools(qt_tools_path: Path, package_for_wheels: bool = False):
+    pyside_tools = PYSIDE_PYTHON_TOOLS.copy()
+    lib_exec_path = qt_tools_path / "libexec"
+
+    if package_for_wheels:
+        # Qt wrappers in build/{python_env_name}/package_for_wheels/PySide6
+        bin_path = qt_tools_path
+    else:
+        bin_path = qt_tools_path / "bin"
+
+    def tool_exist(tool_path: Path):
+        if tool_path.exists():
+            return True
+        else:
+            log.warning(f"{tool_path} not found. pyside-{tool_path.name} not included.")
+            return False
+
+    if sys.platform == 'win32':
+        pyside_tools.extend([tool for tool in PYSIDE_WINDOWS_BIN_TOOLS
+                             if tool_exist(bin_path / f"{tool}.exe")])
+    else:
+        pyside_tools.extend([tool for tool in PYSIDE_LINUX_LIBEXEC_TOOLS
+                             if tool_exist(lib_exec_path / tool)])
+        pyside_tools.extend([tool for tool in PYSIDE_LINUX_BIN_TOOLS
+                             if tool_exist(bin_path / tool)])
+
+    return pyside_tools
+
+
+def find_qt_install_path() -> Path:
+    """
+        Find Qt installation path
+    """
+
+    def where_is(x):
+        return Path(which(x))
+
+    qtpaths = where_is("qtpaths") if where_is("qtpaths") else where_is("qtpaths6")
+    if not qtpaths:
+        raise RuntimeError("qtpaths not found")
+    else:
+        return qtpaths.parents[1]

build_scripts/build_scripts.pyproject
@@ -2,8 +2,7 @@
     "files": ["main.py", "__init__.py", "build_info_collector.py",
               "config.py", "options.py", "qtinfo.py",
               "setup_runner.py", "utils.py",
-              "wheel_files.py", "wheel_override.py",
-              "versions.py", "wheel_utils.py",
+              "wheel_files.py", "wheel_override.py", "wheel_utils.py",
               "platforms/__init__.py", "platforms/linux.py",
               "platforms/macos.py", "platforms/unix.py",
               "platforms/windows_desktop.py",

build_scripts/config.py
@@ -6,7 +6,8 @@ import os
 from .log import log
 from pathlib import Path
 
-from .versions import PYSIDE, PYSIDE_MODULE, SHIBOKEN
+from . import PYSIDE, PYSIDE_MODULE, SHIBOKEN
+from .utils import available_pyside_tools
 
 
 class Config(object):
@@ -78,7 +79,8 @@ class Config(object):
                     ext_modules=None,
                     setup_script_dir=None,
                     cmake_toolchain_file=None,
-                    quiet=False):
+                    quiet=False,
+                    qt_install_path: Path = None):
         """
         Sets up the global singleton config which is used in many parts
         of the setup process.
@@ -199,11 +201,7 @@ class Config(object):
             setup_kwargs['install_requires'] = [
                 f"{self.shiboken_module_st_name}=={package_version}"
             ]
-            _pyside_tools = ["uic", "rcc", "assistant", "designer", "linguist",
-                             "lupdate", "lrelease", "genpyi", "metaobjectdump",
-                             "project", "qml", "qmltyperegistrar", "qmllint", "qmlformat", "qmlls",
-                             "qtpy2cpp", "deploy", "qmlimportscanner"]
-
+            _pyside_tools = available_pyside_tools(qt_tools_path=qt_install_path)
             setup_kwargs['entry_points'] = {
                 'console_scripts': [f'{PYSIDE}-{tool} = {package_name}.scripts.pyside_tool:{tool}'
                                     for tool in _pyside_tools]

build_scripts/main.py
@@ -43,7 +43,7 @@ from .utils import (copydir, copyfile, detect_clang, filter_match,
                     linux_fix_rpaths_for_library, macos_fix_rpaths_for_library,
                     platform_cmake_options, remove_tree, run_process,
                     run_process_output, update_env_path, which)
-from .versions import PYSIDE, PYSIDE_MODULE, SHIBOKEN
+from . import PYSIDE, PYSIDE_MODULE, SHIBOKEN
 from .wheel_override import get_bdist_wheel_override, wheel_module_exists
 from .wheel_utils import (get_package_timestamp, get_package_version,
                           macos_plat_name, macos_pyside_min_deployment_target)

build_scripts/platforms/linux.py
@@ -8,7 +8,7 @@ from ..config import config
 from ..options import OPTION
 from ..utils import (copy_icu_libs, copydir, copyfile, find_files_using_glob,
                      linux_patch_executable)
-from ..versions import PYSIDE
+from .. import PYSIDE
 
 
 def prepare_standalone_package_linux(pyside_build, _vars, cross_build=False):

build_scripts/platforms/macos.py
@@ -9,7 +9,7 @@ from ..config import config
 from ..options import OPTION
 from ..utils import (copydir, copyfile, macos_add_rpath,
                      macos_fix_rpaths_for_library)
-from ..versions import PYSIDE
+from .. import PYSIDE
 
 
 def _macos_patch_executable(name, _vars=None):

build_scripts/platforms/unix.py
@@ -8,7 +8,7 @@ import sys
 from ..config import config
 from ..options import OPTION
 from ..utils import copydir, copyfile, makefile
-from ..versions import PYSIDE, SHIBOKEN
+from .. import PYSIDE, SHIBOKEN
 from .linux import prepare_standalone_package_linux
 from .macos import prepare_standalone_package_macos
 

commit a48de6afbf127831aa46c1c006d777861bbbe9cb

PySideTools: Fix non-existing macOS tools
- designer, assistant and linguist were not existing due to a
  regression from 43109854a2966afe2e0cf29961157a6f54d5775c

Pick-to: 6.5
Change-Id: I7f92ab2eca2fd98eb049e8e81a9efa56ebec3459
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

build_scripts/__init__.py
@@ -12,20 +12,29 @@ PYSIDE_PYTHON_TOOLS = ["metaobjectdump",
                        "qml",
                        "qtpy2cpp",
                        "genpyi"]
-PYSIDE_LINUX_BIN_TOOLS = ["lupdate",
-                          "lrelease",
-                          "qmllint",
-                          "qmlformat",
-                          "qmlls",
-                          "assistant",
-                          "designer",
-                          "linguist"]
-PYSIDE_LINUX_LIBEXEC_TOOLS = ["uic",
-                              "rcc",
-                              "qmltyperegistrar",
-                              "qmlimportscanner"]
+PYSIDE_UNIX_BIN_TOOLS = ["lupdate",
+                         "lrelease",
+                         "qmllint",
+                         "qmlformat",
+                         "qmlls"]
+
+# tools that are bundled as .app in macOS
+# keys represent tool name
+# value represents the path to the tool in the macOS app bundle
+PYSIDE_UNIX_BUNDLED_TOOLS = {name: f"{name.capitalize()}.app/Contents/MacOS/{name.capitalize()}"
+                             for name in ["assistant",
+                                          "designer",
+                                          "linguist"]}
+
+PYSIDE_LINUX_BIN_TOOLS = PYSIDE_UNIX_BIN_TOOLS + [name for name in PYSIDE_UNIX_BUNDLED_TOOLS.keys()]
+
+PYSIDE_UNIX_LIBEXEC_TOOLS = ["uic",
+                             "rcc",
+                             "qmltyperegistrar",
+                             "qmlimportscanner"]
+
 # all Qt tools are in 'bin' folder in Windows
-PYSIDE_WINDOWS_BIN_TOOLS = PYSIDE_LINUX_LIBEXEC_TOOLS + PYSIDE_LINUX_BIN_TOOLS
+PYSIDE_WINDOWS_BIN_TOOLS = PYSIDE_UNIX_LIBEXEC_TOOLS + PYSIDE_LINUX_BIN_TOOLS
 
 ANDROID_ESSENTIALS = ["Core", "Gui", "Widgets", "Network", "OpenGL", "Qml", "Quick",
                       "QuickControls2"]

build_scripts/utils.py
@@ -18,8 +18,8 @@ from pathlib import Path
 from textwrap import dedent, indent
 
 from .log import log
-from . import (PYSIDE_PYTHON_TOOLS, PYSIDE_LINUX_BIN_TOOLS, PYSIDE_LINUX_LIBEXEC_TOOLS,
-               PYSIDE_WINDOWS_BIN_TOOLS)
+from . import (PYSIDE_PYTHON_TOOLS, PYSIDE_LINUX_BIN_TOOLS, PYSIDE_UNIX_LIBEXEC_TOOLS,
+               PYSIDE_WINDOWS_BIN_TOOLS, PYSIDE_UNIX_BIN_TOOLS, PYSIDE_UNIX_BUNDLED_TOOLS)
 
 try:
     # Using the distutils implementation within setuptools
@@ -1144,10 +1144,16 @@ def available_pyside_tools(qt_tools_path: Path, package_for_wheels: bool = False
     else:
         lib_exec_path = qt_tools_path / "Qt" / "libexec" if package_for_wheels \
                         else qt_tools_path / "libexec"
-        pyside_tools.extend([tool for tool in PYSIDE_LINUX_LIBEXEC_TOOLS
+        pyside_tools.extend([tool for tool in PYSIDE_UNIX_LIBEXEC_TOOLS
                              if tool_exist(lib_exec_path / tool)])
-        pyside_tools.extend([tool for tool in PYSIDE_LINUX_BIN_TOOLS
-                             if tool_exist(bin_path / tool)])
+        if sys.platform == 'darwin':
+            pyside_tools.extend([tool for tool in PYSIDE_UNIX_BIN_TOOLS
+                                if tool_exist(bin_path / tool)])
+            pyside_tools.extend([tool_name for tool_name, tool_path in PYSIDE_UNIX_BUNDLED_TOOLS.items()
+                                if tool_exist(bin_path / tool_path)])
+        else:
+            pyside_tools.extend([tool for tool in PYSIDE_LINUX_BIN_TOOLS
+                                if tool_exist(bin_path / tool)])
 
     return pyside_tools
 