c1e786ab8707ec994e0d9d9301e3d0069df87fe5
Cristián Maureira-Fredes <cristian.maureira-fredes@qt.io>
2023-04-05 16:48:58 +0200

    build: remove unused imports and obsolete comment
    Change-Id: I52ce8b449e5a0c4f1d94872becaa391c1c9ae605
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit 23cc8b6f01205567904300c494854f0bbaad11e0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- build_scripts/platforms/unix.py
+++ build_scripts/platforms/unix.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2018 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-import fnmatch
 import sys
 from pathlib import Path
 
--- build_scripts/utils.py
+++ build_scripts/utils.py
@@ -4,7 +4,6 @@
 import errno
 import fnmatch
 import glob
-import itertools
 import os
 import re
 import shutil
