b32e2a5924ddf0a0be823597f067966fa37a00e4
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-04-25 14:01:45 +0200

    shiboken6/clang: Fix build with clang 16
    clang 16 returns more elaborated types instead of fully qualified type
    names. Qualify elaborated types when retrieving the type name.
    
    [ChangeLog][shiboken6] Support for libclang version 16 has been added.
    
    Task-number: PYSIDE-2288
    Change-Id: Ibd428280180967f11d82a72159e744c016afc927
    Reviewed-by: Christian Tismer <tismer@stackless.com>
    (cherry picked from commit 44ef1859214c66861a251d4a0faf5c38dc050850)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/ApiExtractor/clangparser/clangutils.h
+++ sources/shiboken6/ApiExtractor/clangparser/clangutils.h
@@ -26,6 +26,7 @@ QString getCursorKindName(CXCursorKind cursorKind);
 QString getCursorSpelling(const CXCursor &cursor);
 QString getCursorDisplayName(const CXCursor &cursor);
 QString getTypeName(const CXType &type);
+QString getResolvedTypeName(const CXType &type);
 inline QString getCursorTypeName(const CXCursor &cursor)
     { return getTypeName(clang_getCursorType(cursor)); }
 inline QString getCursorResultTypeName(const CXCursor &cursor)
