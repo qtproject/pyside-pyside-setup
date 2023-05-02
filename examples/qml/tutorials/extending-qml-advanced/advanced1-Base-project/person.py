5fed0abe1c06b21358c5cd5a8ff8fdd10a073620
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-05-02 10:41:54 +0200

    QML reference examples: Add the add advanced1-Base-project
    Task-number: PYSIDE-2206
    Task-number: QTBUG-111033
    Change-Id: I8b162f2f5cb6e7099c74e751bcdf95dea5d7d118
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit 58c304ca83d5fc66707cddc0998b2b63b1148508)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ examples/qml/tutorials/extending-qml-advanced/advanced1-Base-project/person.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2023 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+from PySide6.QtCore import QObject, Property, Signal
+from PySide6.QtQml import QmlElement
+
+# To be used on the @QmlElement decorator
+# (QML_IMPORT_MINOR_VERSION is optional)
+QML_IMPORT_NAME = "People"
+QML_IMPORT_MAJOR_VERSION = 1
+
+
+@QmlElement
+class Person(QObject):
+    name_changed = Signal()
+    shoe_size_changed = Signal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._name = ''
+        self._shoe_size = 0
+
+    @Property(str, notify=name_changed, final=True)
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, n):
+        if self._name != n:
+            self._name = n
+            self.name_changed.emit()
+
+    @Property(int, notify=shoe_size_changed, final=True)
+    def shoe_size(self):
+        return self._shoe_size
+
+    @shoe_size.setter
+    def shoe_size(self, s):
+        if self._shoe_size != s:
+            self._shoe_size = s
+            self.shoe_size_changed.emit()
