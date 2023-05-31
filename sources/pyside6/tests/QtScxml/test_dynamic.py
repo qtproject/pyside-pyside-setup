be920de29af70012dcb78ae6f9694c375508431d
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-05-31 12:12:41 +0200

    Fix crash when using QScxmlStateMachine::connectToEvent()
    Change (gadget) QScxmlEvent to be a value type.
    Refactor the test to check for events as well.
    
    Fixes: PYSIDE-2340
    Change-Id: I0e37de05740f17d41794ebc74c81e49c49753495
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit e3d5872887442793735cb85f5e8bee26eee4d88d)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/tests/QtScxml/test_dynamic.py
+++ sources/pyside6/tests/QtScxml/test_dynamic.py
@@ -11,25 +11,42 @@ from init_paths import init_test_paths
 init_test_paths(False)
 
 from helper.timedqapplication import TimedQApplication
-from PySide6.QtCore import QObject, SIGNAL
-from PySide6.QtScxml import QScxmlStateMachine
+from PySide6.QtCore import QCoreApplication, QObject, Slot, SIGNAL, SLOT
+from PySide6.QtScxml import QScxmlStateMachine, QScxmlEvent
+
+
+class Receiver(QObject):
+    def __init__(self):
+        super().__init__()
+        self.eventReceived = False
+        self.reachedStable = False
+
+    @Slot(QScxmlEvent)
+    def handleEvent(self, event):
+        self.eventReceived = True
+
+    @Slot()
+    def slotReachedStable(self):
+        self.reachedStable = True
 
 
 class testDynamicStateMachine(TimedQApplication):
     def setUp(self):
-        super(testDynamicStateMachine, self).setUp()
-        filePath = os.path.join(os.path.dirname(__file__), 'trafficlight.scxml')
-        self.assertTrue(os.path.exists(filePath))
-        self._machine = QScxmlStateMachine.fromFile(filePath)
-        self._machine.reachedStableState.connect(self._reachedStable())
+        super().setUp()
+        filePath = Path(__file__).parent / "trafficlight.scxml"
+        self.assertTrue(filePath.is_file())
+        self._machine = QScxmlStateMachine.fromFile(os.fspath(filePath))
+        self._receiver = Receiver()
+        self._machine.connectToEvent("*", self._receiver,
+                                     SLOT("handleEvent(QScxmlEvent)"))
+        self._machine.reachedStableState.connect(self._receiver.slotReachedStable)
         self.assertTrue(not self._machine.parseErrors())
         self.assertTrue(self._machine)
 
-    def _reachedStable(self):
-        self.app.quit()
-
     def test(self):
         self._machine.start()
+        while not self._receiver.reachedStable and not self._receiver.eventReceived:
+            QCoreApplication.processEvents()
 
 
 if __name__ == '__main__':
