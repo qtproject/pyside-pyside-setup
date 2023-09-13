ef9f3b63b277642b6b93fe4fbc5de3b8f16214e1
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-09-13 12:44:50 +0200

    shiboken6: Introduce simple option struct
    Add a struct Options to replace the struct CommandLineArguments
    from main.cpp with built-in handling of the project file.
    
    Port the generators over.
    
    Pick-to: 6.6
    Change-Id: I3012d211761e7a43c709f2754e6846f008ff2b0d
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>

--- sources/shiboken6/ApiExtractor/optionsparser.cpp
+++ sources/shiboken6/ApiExtractor/optionsparser.cpp
@@ -2,11 +2,93 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include "optionsparser.h"
+#include "messages.h"
+#include "exception.h"
 
 #include <QtCore/QDir>
 #include <QtCore/QTextStream>
 
 using namespace Qt::StringLiterals;
+
+template <class Stream> void formatBoolOption(Stream &s, const BoolOption &bo)
+{
+    switch (bo.source) {
+    case OptionSource::CommandLine:
+        s << "--";
+        break;
+    case OptionSource::CommandLineSingleDash:
+        s << '-';
+        break;
+    default:
+        break;
+    }
+    s << bo.option;
+    if (bo.source == OptionSource::ProjectFile)
+        s << " (project)";
+}
+
+template <class Stream> void formatOptionValue(Stream &s, const OptionValue &ov)
+{
+    switch (ov.source) {
+    case OptionSource::CommandLine:
+        s << "--";
+        break;
+    case OptionSource::CommandLineSingleDash:
+        s << '-';
+        break;
+    default:
+        break;
+    }
+    s << ov.option << '=' << ov.value;
+    if (ov.source == OptionSource::ProjectFile)
+        s << " (project)";
+}
+
+QTextStream &operator<<(QTextStream &s, const BoolOption &bo)
+{
+    formatBoolOption(s, bo);
+    return s;
+}
+
+QTextStream &operator<<(QTextStream &s, const OptionValue &ov)
+{
+    formatOptionValue(s, ov);
+    return s;
+}
+
+QDebug operator<<(QDebug debug, const BoolOption &bo)
+{
+    QDebugStateSaver saver(debug);
+    debug.noquote();
+    debug.nospace();
+    formatBoolOption(debug, bo);
+    return debug;
+}
+
+QDebug operator<<(QDebug debug, const OptionValue &v)
+{
+    QDebugStateSaver saver(debug);
+    debug.noquote();
+    debug.nospace();
+    formatOptionValue(debug, v);
+    return debug;
+}
+
+QDebug operator<<(QDebug debug, const Options &v)
+{
+    QDebugStateSaver saver(debug);
+    debug.noquote();
+    debug.nospace();
+    debug << "Options(";
+    if (!v.boolOptions.isEmpty())
+        debug << "bools=" << v.boolOptions;
+    if (!v.valueOptions.isEmpty())
+        debug << ", option values=" << v.valueOptions;
+    if (!v.positionalArguments.isEmpty())
+        debug << ", pos=" << v.positionalArguments;
+    debug << ')';
+    return debug;
+}
 
 QTextStream &operator<<(QTextStream &s, const OptionDescription &od)
 {
@@ -54,6 +136,20 @@ bool OptionsParser::handleOption(const QString &, const QString &, OptionSource)
     return false;
 }
 
+void OptionsParser::process(Options *o)
+{
+    for (auto i = o->boolOptions.size() - 1; i >= 0; --i) {
+        const auto &opt = o->boolOptions.at(i);
+        if (handleBoolOption(opt.option, opt.source))
+            o->boolOptions.removeAt(i);
+    }
+    for (auto i = o->valueOptions.size() - 1; i >= 0; --i) {
+        const auto &opt = o->valueOptions.at(i);
+        if (handleOption(opt.option, opt.value, opt.source))
+            o->valueOptions.removeAt(i);
+    }
+}
+
 bool OptionsParserList::handleBoolOption(const QString &key, OptionSource source)
 {
     for (const auto &p : std::as_const(m_parsers)) {
@@ -70,4 +166,56 @@ bool OptionsParserList::handleOption(const QString &key, const QString &value, O
             return true;
     }
     return false;
+}
+
+static void processOption(const QString &o, OptionSource source,
+                          BoolOptions *bools, OptionValues *values)
+{
+    const auto equals = o.indexOf(u'=');
+    if (equals == -1) {
+        bools->append({o.trimmed(), source});
+    } else {
+        QString key = o.left(equals).trimmed();
+        QString value = o.mid(equals + 1).trimmed();
+        if (!value.isEmpty())
+            values->append({key, value, source});
+    }
+}
+
+static void readProjectFile(const QString &name, Options *o)
+{
+    const auto startMarker = "[generator-project]"_ba;
+
+    QFile file(name);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        throw Exception(msgCannotOpenForReading(file));
+
+    if (file.atEnd() || file.readLine().trimmed() != startMarker)
+        throw Exception(msgMissingProjectFileMarker(name, startMarker));
+
+    while (!file.atEnd()) {
+        const QByteArray lineB = file.readLine().trimmed();
+        if (!lineB.isEmpty() && !lineB.startsWith('#')) {
+            processOption(QString::fromUtf8(lineB), OptionSource::ProjectFile,
+                          &o->boolOptions, &o->valueOptions);
+        }
+    }
+}
+
+void Options::setOptions(const QStringList &argv)
+{
+    const auto projectFileOption = "--project-file="_L1;
+    for (const auto &o : argv) {
+        if (o.startsWith(projectFileOption)) {
+            readProjectFile(o.sliced(projectFileOption.size()), this);
+        } else if (o.startsWith(u"--")) {
+            processOption(o.sliced(2), OptionSource::CommandLine,
+                          &boolOptions, &valueOptions);
+        } else if (o.startsWith(u'-')) {
+            processOption(o.sliced(1), OptionSource::CommandLineSingleDash,
+                          &boolOptions, &valueOptions);
+        } else {
+            positionalArguments.append(o);
+        }
+    }
 }
