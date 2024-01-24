5929c612b5bebb8e2bfa703e9db5ec20a077a0a0
Friedemann Kleint <Friedemann.Kleint@qt.io>
2024-01-24 11:43:16 +0100

    PySide6: Fix build with namespaced Qt
    Task-number: PYSIDE-2590
    Pick-to: 6.6 6.5
    Change-Id: I977d3d93b4f8385329e154a493a12d24ee0ac196
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/pyside6/libpysideqml/pysideqmlregistertype.h
+++ sources/pyside6/libpysideqml/pysideqmlregistertype.h
@@ -7,11 +7,14 @@
 #include "pysideqmlmacros.h"
 
 #include <sbkpython.h>
+#include <QtCore/qtconfigmacros.h>
 
+QT_BEGIN_NAMESPACE
 namespace QQmlPrivate
 {
 struct RegisterTypeAndRevisions;
 }
+QT_END_NAMESPACE
 
 namespace PySide::Qml
 {
@@ -86,7 +89,7 @@ PYSIDEQML_API PyObject *qmlSingletonMacro(PyObject *pyObj);
 // Used by QtQuick module to fill the QQmlPrivate::RegisterType::parserStatusCast,
 // valueSourceCast and valueInterceptorCast fields with the correct values.
 using QuickRegisterItemFunction =
-    bool (*)(PyObject *pyObj, QQmlPrivate::RegisterTypeAndRevisions *);
+    bool (*)(PyObject *pyObj, QT_PREPEND_NAMESPACE(QQmlPrivate::RegisterTypeAndRevisions) *);
 
 PYSIDEQML_API QuickRegisterItemFunction getQuickRegisterItemFunction();
 PYSIDEQML_API void setQuickRegisterItemFunction(QuickRegisterItemFunction function);
