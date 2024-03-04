31fac9b035594c12d75ac7ece4aab304ba4c135e
Friedemann Kleint <Friedemann.Kleint@qt.io>
2024-03-04 08:44:11 +0100

    shiboken6: Remove unneeded source file
    Change-Id: I1d13a228f221d837abdd8326cb3e414d2a9a92be
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Christian Tismer <tismer@stackless.com>

--- sources/shiboken6/generators/shiboken/shiboken.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
-
-#include "cppgenerator.h"
-#include "headergenerator.h"
-
-EXPORT_GENERATOR_PLUGIN(new CppGenerator << new HeaderGenerator)
