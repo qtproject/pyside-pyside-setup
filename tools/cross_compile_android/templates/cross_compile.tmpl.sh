e440ccb245c22de13674c1d880f6fcda618f5261
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2024-03-15 15:49:39 +0100

    Android Wheels: Use config.guess script to find host system name
    - first of many patches to support macOS host for Qfp Android
      cross-compilation.
    - Use config.guess present in cpython repository to guess canonical
      name of the host system. This sets up build for hosts other than
      linux.
    - As a drive by, update the default api level to 26 to sync with Qt
      minimum version 6.7.0.
    
    Task-number: PYSIDE-2766
    Change-Id: Ifcf0921776e6a682d5724f439739ed098ccc6ef3
    Reviewed-by: Adrian Herrmann <adrian.herrmann@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit f205110cffe38524ab66b53494c83594aa6f9ce1)

--- tools/cross_compile_android/templates/cross_compile.tmpl.sh
+++ tools/cross_compile_android/templates/cross_compile.tmpl.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 set -x -e
 export HOST_ARCH={{ plat_name }}-linux-android
-export TOOLCHAIN={{ ndk_path }}/toolchains/llvm/prebuilt/linux-x86_64/bin
+export TOOLCHAIN={{ ndk_path }}/toolchains/llvm/prebuilt/{{ host_platform_name }}-x86_64/bin
 export TOOL_PREFIX=$TOOLCHAIN/$HOST_ARCH
 export PLATFORM_API={{ api_level }}
 {% if plat_name == "armv7a" -%}
@@ -20,7 +20,7 @@ export RANLIB=$TOOLCHAIN/llvm-ranlib
 export LD=$TOOLCHAIN/ld
 export READELF=$TOOLCHAIN/llvm-readelf
 export CFLAGS='-fPIC -DANDROID'
-./configure --host=$HOST_ARCH --target=$HOST_ARCH --build=x86_64-pc-linux-gnu \
+./configure --host=$HOST_ARCH --target=$HOST_ARCH --build={{ host_system_name }} \
 --with-build-python={{ host_python_path }}  --enable-shared \
 --enable-ipv6 ac_cv_file__dev_ptmx=yes ac_cv_file__dev_ptc=no --without-ensurepip \
 ac_cv_little_endian_double=yes
