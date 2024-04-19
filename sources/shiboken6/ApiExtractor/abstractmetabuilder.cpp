qtproject/pyside-pyside-setup

commit 5b8ce393310ed9b6ffa4b0add29d1bb164e37baa

shiboken6: Remove class attribute FinalInTargetLang
Similar to the function attribute, it does not have any impact on the
generated code.

Its only usage was in AbstractMetaClass::functionsInTargetLang() which
is used by the documentation generator only, but it did not seem to
have any impact.

Pick-to: 6.7
Task-number: PYSIDE-2602
Change-Id: I0a27dc046ce7fbb147c8032cccbf63257157fc89
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/shiboken6/ApiExtractor/abstractmetabuilder.cpp
@@ -1451,17 +1451,11 @@ void AbstractMetaBuilderPrivate::traverseFunctions(const ScopeModelItem& scopeIt
             }
         }
 
-        const bool isInvalidDestructor = metaFunction->isDestructor() && metaFunction->isPrivate();
-        const bool isInvalidConstructor = metaFunction->functionType() == AbstractMetaFunction::ConstructorFunction
-            && metaFunction->isPrivate();
-        if (isInvalidConstructor)
-            metaClass->setHasPrivateConstructor(true);
-        if ((isInvalidDestructor || isInvalidConstructor)
-            && !metaClass->hasNonPrivateConstructor()) {
-            *metaClass += AbstractMetaClass::FinalInTargetLang;
-        } else if (metaFunction->isConstructor() && !metaFunction->isPrivate()) {
-            *metaClass -= AbstractMetaClass::FinalInTargetLang;
-            metaClass->setHasNonPrivateConstructor(true);
+        if (metaFunction->functionType() == AbstractMetaFunction::ConstructorFunction) {
+            if (metaFunction->isPrivate())
+                metaClass->setHasPrivateConstructor(true);
+            else
+                metaClass->setHasNonPrivateConstructor(true);
         }
 
         if (!metaFunction->isDestructor()

sources/shiboken6/ApiExtractor/abstractmetalang.cpp
@@ -177,22 +177,16 @@ AbstractMetaFunctionCList AbstractMetaClass::functionsInTargetLang() const
     FunctionQueryOptions default_flags = FunctionQueryOption::NormalFunctions
         | FunctionQueryOption::Visible | FunctionQueryOption::NotRemoved;
 
-    // Only public functions in final classes
-    // default_flags |= isFinal() ? WasPublic : 0;
-    FunctionQueryOptions public_flags;
-    if (isFinalInTargetLang())
-        public_flags |= FunctionQueryOption::WasPublic;
-
     // Constructors
     AbstractMetaFunctionCList returned = queryFunctions(FunctionQueryOption::AnyConstructor
-                                                        | default_flags | public_flags);
+                                                        | default_flags);
 
     returned += queryFunctions(FunctionQueryOption::NonStaticFunctions
-                               | default_flags | public_flags);
+                               | default_flags);
 
     // Static functions
     returned += queryFunctions(FunctionQueryOption::StaticFunctions
-                               | default_flags | public_flags);
+                               | default_flags);
 
     // Empty, private functions, since they aren't caught by the other ones
     returned += queryFunctions(FunctionQueryOption::Empty | FunctionQueryOption::Invisible);
@@ -1167,9 +1161,6 @@ bool AbstractMetaClass::queryFunction(const AbstractMetaFunction *f, FunctionQue
     if (query.testFlag(FunctionQueryOption::Empty) && !f->isEmptyFunction())
         return false;
 
-    if (query.testFlag(FunctionQueryOption::WasPublic) && !f->wasPublic())
-        return false;
-
     if (query.testFlag(FunctionQueryOption::ClassImplements) && f->ownerClass() != f->implementingClass())
         return false;
 
@@ -1467,12 +1458,6 @@ void AbstractMetaClass::fixFunctions(const AbstractMetaClassPtr &klass)
         // interrested in what each super class implements, not what
         // we may have propagated from their base classes again.
         AbstractMetaFunctionCList superFuncs;
-        // Super classes can never be final
-        if (superClass->isFinalInTargetLang()) {
-            qCWarning(lcShiboken).noquote().nospace()
-                << "Final class '" << superClass->name() << "' set to non-final, as it is extended by other classes";
-            *superClass -= AbstractMetaClass::FinalInTargetLang;
-        }
         superFuncs = superClass->queryFunctions(FunctionQueryOption::ClassImplements);
         // We are not interested in signals as no bindings are generated for them;
         // they cause documentation warnings.

sources/shiboken6/ApiExtractor/abstractmetalang.h
@@ -352,11 +352,6 @@ private:
     QScopedPointer<AbstractMetaClassPrivate> d;
 };
 
-inline bool AbstractMetaClass::isFinalInTargetLang() const
-{
-    return attributes().testFlag(FinalInTargetLang);
-}
-
 inline bool AbstractMetaClass::isAbstract() const
 {
     return attributes().testFlag(Abstract);

sources/shiboken6/ApiExtractor/abstractmetalang_enums.h
@@ -16,7 +16,6 @@ enum class FunctionQueryOption {
     Signals                      = 0x0000100, // Only signals
     NormalFunctions              = 0x0000200, // Only functions that aren't signals
     Visible                      = 0x0000400, // Only public and protected functions
-    WasPublic                    = 0x0001000, // Only functions that were originally public
     NonStaticFunctions           = 0x0004000, // No static functions
     Empty                        = 0x0008000, // Empty overrides of abstract functions
     Invisible                    = 0x0010000, // Only private functions

commit d66b276df502cd3a2196c69cbae8bdbd8ac08c7d

shiboken6: Fix warnings about unused init functions
Amends 5b8ce393310ed9b6ffa4b0add29d1bb164e37baa, which broke
a condition setting AbstractMetaClass::hasNonPrivateConstructor
which needs to take into account copy constructors.

Pick-to: 6.7
Task-number: PYSIDE-2602
Change-Id: Iba4a8070a984a5306fe5c73bf77bee0777719bb3
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/shiboken6/ApiExtractor/abstractmetabuilder.cpp
@@ -1451,12 +1451,12 @@ void AbstractMetaBuilderPrivate::traverseFunctions(const ScopeModelItem& scopeIt
             }
         }
 
-        if (metaFunction->functionType() == AbstractMetaFunction::ConstructorFunction) {
-            if (metaFunction->isPrivate())
-                metaClass->setHasPrivateConstructor(true);
-            else
-                metaClass->setHasNonPrivateConstructor(true);
+        if (metaFunction->functionType() == AbstractMetaFunction::ConstructorFunction
+            && metaFunction->isPrivate()) {
+            metaClass->setHasPrivateConstructor(true);
         }
+        if (metaFunction->isConstructor() && !metaFunction->isPrivate()) // Including Copy CT
+            metaClass->setHasNonPrivateConstructor(true);
 
         if (!metaFunction->isDestructor()
             && !(metaFunction->isPrivate() && metaFunction->functionType() == AbstractMetaFunction::ConstructorFunction)) {