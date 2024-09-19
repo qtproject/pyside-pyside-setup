8f4ac6b0a6295ab71cb3aacd504a0e748fc2c953
Cristián Maureira-Fredes <cristian.maureira-fredes@qt.io>
2024-09-19 09:25:05 +0200

    Fix condition to allow 3.13 for wheel creation
    Task-number: PYSIDE-2751
    Change-Id: Iaf6e914bf798371faf5066f4a2d70fe3e2c124e5
    Reviewed-by: Christian Tismer <tismer@stackless.com>
    (cherry picked from commit a86135803fe51e5702444f8b7dcb2a4dccd3a209)
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- build_scripts/config.py
+++ build_scripts/config.py
@@ -117,7 +117,7 @@ class Config(object):
         setup_kwargs['zip_safe'] = False
         setup_kwargs['cmdclass'] = cmd_class_dict
         setup_kwargs['version'] = package_version
-        setup_kwargs['python_requires'] = ">=3.9, <3.13"
+        setup_kwargs['python_requires'] = ">=3.9, <3.14"
 
         if log_level == LogLevel.QUIET:
             # Tells setuptools to be quiet, and only print warnings or errors.
