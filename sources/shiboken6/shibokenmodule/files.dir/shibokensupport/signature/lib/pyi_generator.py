1080236f3b263c23b0a63621eb9f37a008176aec
Christian Tismer <tismer@stackless.com>
2024-09-20 13:53:25 +0200

    type hints: Obey the rules concerning denoting enums
    Enums should not contain explicit type annotations.
    
    Task-number: PYSIDE-2486
    Change-Id: I610bc917d76a43a95c24ae2bb9262f829c9aacca
    Fixes: PYSIDE-2869
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
@@ -193,7 +193,7 @@ class Formatter(Writer):
     def enum(self, class_name, enum_name, value):
         spaces = indent * self.level
         hexval = hex(value)
-        self.print(f"{spaces}{enum_name:25}: {class_name} = ... # {hexval}")
+        self.print(f"{spaces}{enum_name:25} = ...  # {hexval if value >= 0 else value}")
         yield
 
     @contextmanager
