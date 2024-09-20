facad37a8a62d84865b88e3bd4e38eb9a3e0419b
Christian Tismer <tismer@stackless.com>
2024-09-20 13:25:58 +0200

    type hints: Fix a bug introduced by PlaceHolderType
    PlaceHolderType is used in QtCore, only. But it is
    unconditionally inserted into every module without importing
    which creates errors.
    
    Task-number: PYSIDE-2846
    Change-Id: I86c6bdf04a089ff755399945b3fa3dee8a6e0bbe
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
@@ -335,9 +335,10 @@ def generate_pyi(import_name, outpath, options):
                 wr.print()
                 wr.print("NoneType: typing.TypeAlias = type[None]")
                 # We use it only in QtCore at the moment, but this
-                # could be extended to other modules.
-                wr.print("PlaceHolderType = typing.TypeVar(\"PlaceHolderType\", "
-                         "bound=PySide6.QtCore.QObject)")
+                # could be extended to other modules. (must import QObject then)
+                if import_name == "PySide6.QtCore":
+                    wr.print("PlaceHolderType = typing.TypeVar(\"PlaceHolderType\", "
+                             "bound=PySide6.QtCore.QObject)")
                 wr.print()
             else:
                 wr.print(line)
