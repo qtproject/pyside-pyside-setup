d5b180c5de8e256f56d8bf5e974a32e999eaea07
Ece Cinucen <ece.cinucen@qt.io>
2025-04-02 10:25:55 +0200

    type hints: Correct type signature for QObject class
    connect, disconnect and receivers functions accept input str instead of byte
    
    Fixes: PYSIDE-3059
    Pick-to: 6.8
    Change-Id: I41eacdc4386fe5e9e6e83e73299fb99c474cd73d
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit 00686a7ed000479f59ba879dd0c702ecf01b7b3a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/mapping.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/mapping.py
@@ -547,6 +547,9 @@ def init_PySide6_QtCore():
     type_map_tuple.update({("PySide6.QtCore.QObject.setProperty", "char*"): str})
     type_map_tuple.update({("PySide6.QtCore.QObject.property", "char*"): str})
     type_map_tuple.update({("PySide6.QtCore.QObject.inherits", "char*"): str})
+    type_map_tuple.update({("PySide6.QtCore.QObject.connect", "char*"): str})
+    type_map_tuple.update({("PySide6.QtCore.QObject.disconnect", "char*"): str})
+    type_map_tuple.update({("PySide6.QtCore.QObject.receivers", "char*"): str})
     type_map_tuple.update({("PySide6.QtCore.qtTrId", "char*"): str})
 
     return locals()
