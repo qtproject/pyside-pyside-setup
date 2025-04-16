6e7fe0b2ceab7bfe2d39532f3e5ecf7520ad034d
Simo Fält <simo.falt@qt.io>
2025-04-16 11:29:30 +0300

    CI: Use master.qt.io instead of mirror for downloading libclang
    Change-Id: I8cc018ef70f453be97b34f2aadcaf56118ca42f0
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit 7df394cf7479a3419d45e34b930d3c59e6d84fe9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/fetch_libclang_arm64.ps1
+++ coin/fetch_libclang_arm64.ps1
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 # Download the file
 
-wget https://download.qt.io/development_releases/prebuilt/libclang/libclang-release_19.1.0-based-windows-vs2022_arm64.7z -o libclang.7z
+wget https://master.qt.io/development_releases/prebuilt/libclang/libclang-release_19.1.0-based-windows-vs2022_arm64.7z -o libclang.7z
 # Unzip the contents to /home/qt
 7z x libclang.7z -o/utils
 Remove-Item libclang.7z
--- coin/fetch_libclang_arm64.sh
+++ coin/fetch_libclang_arm64.sh
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 # Download the file
-wget -q https://download.qt.io/development_releases/prebuilt/libclang/libclang-release_18.1.7-based-linux-Debian-11.6-gcc10.2-arm64.7z
+wget -q https://master.qt.io/development_releases/prebuilt/libclang/libclang-release_18.1.7-based-linux-Debian-11.6-gcc10.2-arm64.7z
 if [ $? -ne 0 ]; then
     echo "Error: Failed to download libclang archive" >&2
     exit 1
