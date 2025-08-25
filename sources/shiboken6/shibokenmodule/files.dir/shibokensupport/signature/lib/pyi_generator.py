9554a0eaba2803824b90edf3a19adcc93738d0c3
Ece Cinucen <ece.cinucen@qt.io>
2025-08-25 15:44:33 +0200

    type hints: remove 'from __future__ import annotations' from stub files
    Since having 'from __future__ import annotations' is redundant in .pyi files, this statement can be omitted.
    
    Pick-to: 6.8
    Fixes: PYSIDE-3162
    Change-Id: I956ea7f1f7587ef2ddf942ca84b0941bd4bcc492
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit d33bdcd000500a3aacdc6530e399657b57bf33fb)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/pyi_generator.py
@@ -1,7 +1,6 @@
 LICENSE_TEXT = """
 # Copyright (C) 2022 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-from __future__ import annotations
 """
 MYPY_TEXT = """
 # mypy: disable-error-code="override, overload-overlap"
