6d44a1a5014e73e9729e3e3ea01d0b793bc55cad
Ece Cinucen <ece.cinucen@qt.io>
2025-10-06 13:40:15 +0300

    type hints: Fix type annotations of QtCore.Slot
    This change updates the function signature to typing.Union[type, str]
    for both types and result.
    
    Fixes: PYSIDE-3174
    Change-Id: Ic034ed725d774d94513d75014499667b6d7acdce
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit d5811530d1120c60e2125ccd34565982f72de530)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/libpyside/pysideslot.cpp
+++ sources/pyside6/libpyside/pysideslot.cpp
@@ -172,7 +172,7 @@ DataList *dataListFromCapsule(PyObject *capsule)
 }
 
 static const char *Slot_SignatureStrings[] = {
-    "PySide6.QtCore.Slot(self,*types:type,name:str=nullptr,result:type=nullptr)",
+    "PySide6.QtCore.Slot(self,*types:typing.Union[type,str],name:str=nullptr,result:typing.Union[type,str]=nullptr)",
     "PySide6.QtCore.Slot.__call__(self,function:collections.abc.Callable[...,typing.Any])->typing.Any",
     nullptr}; // Sentinel
 
