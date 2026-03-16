da1ac168e4ce675cdfd233ca03679553b6678f95
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-03-16 08:14:59 +0100

    Update the basicsortfiltermodel example
    Port qtbase/fd92a630296cb37f984553314b5f8dc0371b8ee6
    qtbase/c54b5ed5bed441746afac4f387c8792e9c6a77f9:
    
    - Introduce QRangeModel
    - Introduce Enum for Syntax as in C++
    - Use a QFormLayout and its automatic buddy functionality
    
    Change-Id: Icb8e9bda07a57cb7d0da35d7c08d26de1946c666
    Reviewed-by: Ece Cinucen <ece.cinucen@qt.io>
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit 18134d709532820965f3e38a4ecdbb0623162627)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/widgets/itemviews/basicfiltermodel/basicsortfiltermodel.py
+++ examples/widgets/itemviews/basicfiltermodel/basicsortfiltermodel.py
@@ -4,17 +4,55 @@
 from __future__ import annotations
 
 import sys
-from PySide6.QtCore import (QDate, QDateTime, QRegularExpression,
+from enum import Enum
+from PySide6.QtCore import (QDate, QDateTime, QRangeModel, QRegularExpression,
                             QSortFilterProxyModel, QTime, Qt, Slot)
-from PySide6.QtGui import QStandardItemModel
-from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QGridLayout,
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFormLayout,
                                QGroupBox, QHBoxLayout, QLabel, QLineEdit,
                                QTreeView, QVBoxLayout, QWidget)
 
 
-REGULAR_EXPRESSION = 0
-WILDCARD = 1
-FIXED_STRING = 2
+class Syntax(Enum):
+    REGULAR_EXPRESSION = 0
+    WILDCARD = 1
+    FIXED_STRING = 2
+
+
+HEADERS = ["Subject", "Sender", "Date"]
+
+
+MAILS = [
+    ["RE: Sports", "Petra Schmidt <petras@nospam.com>",
+     QDateTime(QDate(2007, 1, 5), QTime(12, 1))],
+    ["AW: Sports", "Rolf Newschweinstein <rolfn@nospam.com>",
+     QDateTime(QDate(2007, 1, 5), QTime(12, 0))],
+    ["Sports", "Linda Smith <linda.smith@nospam.com>",
+     QDateTime(QDate(2007, 1, 5), QTime(11, 33))],
+    ["Re: Accounts", "Andy <andy@nospam.com>",
+     QDateTime(QDate(2007, 1, 3), QTime(14, 26))],
+    ["Re: Accounts", "Joe Bloggs <joe@bloggs.com>",
+     QDateTime(QDate(2007, 1, 3), QTime(14, 18))],
+    ["Re: Expenses", "Andy <andy@nospam.com>",
+     QDateTime(QDate(2007, 1, 2), QTime(16, 5))],
+    ["Expenses", "Joe Bloggs <joe@bloggs.com>",
+     QDateTime(QDate(2006, 12, 25), QTime(11, 39))],
+    ["Accounts", "pascale@nospam.com",
+     QDateTime(QDate(2006, 12, 31), QTime(12, 50))],
+    ["Radically new concept", "Grace K. <grace@software-inc.com>",
+     QDateTime(QDate(2006, 12, 22), QTime(9, 44))],
+    ["Happy New Year!", "Grace K. <grace@software-inc.com>",
+     QDateTime(QDate(2006, 12, 31), QTime(17, 3))]
+]
+
+
+class MailModel(QRangeModel):
+    def __init__(self, parent=None):
+        super().__init__(MAILS, parent)
+
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+        if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
+            return HEADERS[section]
+        return None
 
 
 class Window(QWidget):
@@ -42,25 +80,19 @@ class Window(QWidget):
 
         self._filter_pattern_line_edit = QLineEdit()
         self._filter_pattern_line_edit.setClearButtonEnabled(True)
-        self._filter_pattern_label = QLabel("&Filter pattern:")
-        self._filter_pattern_label.setBuddy(self._filter_pattern_line_edit)
 
         self._filter_syntax_combo_box = QComboBox()
         self._filter_syntax_combo_box.addItem("Regular expression",
-                                              REGULAR_EXPRESSION)
+                                              Syntax.REGULAR_EXPRESSION)
         self._filter_syntax_combo_box.addItem("Wildcard",
-                                              WILDCARD)
+                                              Syntax.WILDCARD)
         self._filter_syntax_combo_box.addItem("Fixed string",
-                                              FIXED_STRING)
-        self._filter_syntax_label = QLabel("Filter &syntax:")
-        self._filter_syntax_label.setBuddy(self._filter_syntax_combo_box)
+                                              Syntax.FIXED_STRING)
 
         self._filter_column_combo_box = QComboBox()
-        self._filter_column_combo_box.addItem("Subject")
-        self._filter_column_combo_box.addItem("Sender")
-        self._filter_column_combo_box.addItem("Date")
+        for header in HEADERS:
+            self._filter_column_combo_box.addItem(header)
         self._filter_column_label = QLabel("Filter &column:")
-        self._filter_column_label.setBuddy(self._filter_column_combo_box)
 
         self._filter_pattern_line_edit.textChanged.connect(self.filter_reg_exp_changed)
         self._filter_syntax_combo_box.currentIndexChanged.connect(self.filter_reg_exp_changed)
@@ -68,29 +100,30 @@ class Window(QWidget):
         self._filter_case_sensitivity_check_box.toggled.connect(self.filter_reg_exp_changed)
         self._sort_case_sensitivity_check_box.toggled.connect(self.sort_changed)
 
-        source_layout = QHBoxLayout()
+        source_layout = QHBoxLayout(self._source_group_box)
         source_layout.addWidget(self._source_view)
-        self._source_group_box.setLayout(source_layout)
 
-        proxy_layout = QGridLayout()
-        proxy_layout.addWidget(self._proxy_view, 0, 0, 1, 3)
-        proxy_layout.addWidget(self._filter_pattern_label, 1, 0)
-        proxy_layout.addWidget(self._filter_pattern_line_edit, 1, 1, 1, 2)
-        proxy_layout.addWidget(self._filter_syntax_label, 2, 0)
-        proxy_layout.addWidget(self._filter_syntax_combo_box, 2, 1, 1, 2)
-        proxy_layout.addWidget(self._filter_column_label, 3, 0)
-        proxy_layout.addWidget(self._filter_column_combo_box, 3, 1, 1, 2)
-        proxy_layout.addWidget(self._filter_case_sensitivity_check_box, 4, 0, 1, 2)
-        proxy_layout.addWidget(self._sort_case_sensitivity_check_box, 4, 2)
-        self._proxy_group_box.setLayout(proxy_layout)
+        proxy_layout = QVBoxLayout(self._proxy_group_box)
+        proxy_layout.addWidget(self._proxy_view)
 
-        main_layout = QVBoxLayout()
+        form_layout = QFormLayout()
+        form_layout.addRow("&Filter pattern:", self._filter_pattern_line_edit)
+        form_layout.addRow("Filter &syntax:", self._filter_syntax_combo_box)
+        form_layout.addRow("Filter &column:", self._filter_column_combo_box)
+        proxy_layout.addLayout(form_layout)
+
+        checkbox_layout = QHBoxLayout()
+        checkbox_layout.addWidget(self._filter_case_sensitivity_check_box)
+        checkbox_layout.addWidget(self._sort_case_sensitivity_check_box)
+        proxy_layout.addLayout(checkbox_layout)
+
+        main_layout = QVBoxLayout(self)
         main_layout.addWidget(self._source_group_box)
         main_layout.addWidget(self._proxy_group_box)
-        self.setLayout(main_layout)
 
         self.setWindowTitle("Basic Sort/Filter Model")
-        self.resize(500, 450)
+        screen_geometry = self.screen().geometry()
+        self.resize(screen_geometry.width() / 2, screen_geometry.height() * 2 / 3)
 
         self._proxy_view.sortByColumn(1, Qt.SortOrder.AscendingOrder)
         self._filter_column_combo_box.setCurrentIndex(1)
@@ -105,12 +138,12 @@ class Window(QWidget):
 
     @Slot()
     def filter_reg_exp_changed(self):
-        syntax_nr = self._filter_syntax_combo_box.currentData()
         pattern = self._filter_pattern_line_edit.text()
-        if syntax_nr == WILDCARD:
-            pattern = QRegularExpression.wildcardToRegularExpression(pattern)
-        elif syntax_nr == FIXED_STRING:
-            pattern = QRegularExpression.escape(pattern)
+        match self._filter_syntax_combo_box.currentData():
+            case Syntax.WILDCARD:
+                pattern = QRegularExpression.wildcardToRegularExpression(pattern)
+            case Syntax.FIXED_STRING:
+                pattern = QRegularExpression.escape(pattern)
 
         reg_exp = QRegularExpression(pattern)
         if not self._filter_case_sensitivity_check_box.isChecked():
@@ -133,47 +166,9 @@ class Window(QWidget):
         self._proxy_model.setSortCaseSensitivity(case_sensitivity)
 
 
-def add_mail(model, subject, sender, date):
-    model.insertRow(0)
-    model.setData(model.index(0, 0), subject)
-    model.setData(model.index(0, 1), sender)
-    model.setData(model.index(0, 2), date)
-
-
-def create_mail_model(parent):
-    model = QStandardItemModel(0, 3, parent)
-
-    model.setHeaderData(0, Qt.Orientation.Horizontal, "Subject")
-    model.setHeaderData(1, Qt.Orientation.Horizontal, "Sender")
-    model.setHeaderData(2, Qt.Orientation.Horizontal, "Date")
-
-    add_mail(model, "Happy New Year!", "Grace K. <grace@software-inc.com>",
-             QDateTime(QDate(2006, 12, 31), QTime(17, 3)))
-    add_mail(model, "Radically new concept", "Grace K. <grace@software-inc.com>",
-             QDateTime(QDate(2006, 12, 22), QTime(9, 44)))
-    add_mail(model, "Accounts", "pascale@nospam.com",
-             QDateTime(QDate(2006, 12, 31), QTime(12, 50)))
-    add_mail(model, "Expenses", "Joe Bloggs <joe@bloggs.com>",
-             QDateTime(QDate(2006, 12, 25), QTime(11, 39)))
-    add_mail(model, "Re: Expenses", "Andy <andy@nospam.com>",
-             QDateTime(QDate(2007, 1, 2), QTime(16, 5)))
-    add_mail(model, "Re: Accounts", "Joe Bloggs <joe@bloggs.com>",
-             QDateTime(QDate(2007, 1, 3), QTime(14, 18)))
-    add_mail(model, "Re: Accounts", "Andy <andy@nospam.com>",
-             QDateTime(QDate(2007, 1, 3), QTime(14, 26)))
-    add_mail(model, "Sports", "Linda Smith <linda.smith@nospam.com>",
-             QDateTime(QDate(2007, 1, 5), QTime(11, 33)))
-    add_mail(model, "AW: Sports", "Rolf Newschweinstein <rolfn@nospam.com>",
-             QDateTime(QDate(2007, 1, 5), QTime(12, 0)))
-    add_mail(model, "RE: Sports", "Petra Schmidt <petras@nospam.com>",
-             QDateTime(QDate(2007, 1, 5), QTime(12, 1)))
-
-    return model
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Window()
-    window.set_source_model(create_mail_model(window))
+    window.set_source_model(MailModel(window))
     window.show()
     sys.exit(app.exec())
