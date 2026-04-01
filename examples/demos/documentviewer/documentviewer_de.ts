e494cb1e6404af03bb2355a009b9f272bed2deb4
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-04-01 10:26:35 +0200

    Document Viewer Example: Add translation infrastructure
    Add the German .ts file and an empty file for English.
    
    Task-number: QTBUG-71856
    Change-Id: I800646323792fdd0928692aad7ab0fc368ee3a53
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit 28b2850448cf043d21ea86f5aa1dfbce41f60058)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ examples/demos/documentviewer/documentviewer_de.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>AbstractViewer</name>
+    <message>
+        <location filename="abstractviewer.py" line="141"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="147"/>
+        <source>Printing</source>
+        <translation>Drucken</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="149"/>
+        <source>No content to print.</source>
+        <translation>Kein Inhalt zum Drucken vorhanden.</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="153"/>
+        <source>Print Document</source>
+        <translation>Dokument drucken</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="157"/>
+        <source>Printing canceled!</source>
+        <translation>Druckvorgang abgebrochen!</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="162"/>
+        <source>Printing aborted.</source>
+        <translation>Druckvorgang abgebrochen.</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="164"/>
+        <source>Printing active.</source>
+        <translation>Druckvorgang läuft.</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="166"/>
+        <source>Printing completed.</source>
+        <translation>Druckvorgang abgeschlossen.</translation>
+    </message>
+    <message>
+        <location filename="abstractviewer.py" line="168"/>
+        <source>Printing error.</source>
+        <translation>Druckfehler.</translation>
+    </message>
+</context>
+<context>
+    <name>ImageViewer</name>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="28"/>
+        <source>unknown</source>
+        <translation>unbekannt</translation>
+    </message>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="56"/>
+        <source>Images</source>
+        <translation>Bilder</translation>
+    </message>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="60"/>
+        <source>Zoom &amp;In</source>
+        <translation>&amp;Vergrößern</translation>
+    </message>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="67"/>
+        <source>Zoom &amp;Out</source>
+        <translation>&amp;Verkleinern</translation>
+    </message>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="74"/>
+        <source>Reset Zoom</source>
+        <translation>Zoom zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="imageviewer/imageviewer.py" line="100"/>
+        <source>Cannot read file {}:
+{}</source>
+        <translation>Die Datei {} kann nicht gelesen werden:
+{}</translation>
+    </message>
+</context>
+<context>
+    <name>JsonViewer</name>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="188"/>
+        <source>Json</source>
+        <translation>Json</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="189"/>
+        <source>Json Actions</source>
+        <translation>Json-Aktionen</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="192"/>
+        <source>&amp;+Expand all</source>
+        <translation>&amp;+Alle erweitern</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="198"/>
+        <source>&amp;-Collapse all</source>
+        <translation>&amp;-Alle reduzieren</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="219"/>
+        <source>Bookmarks</source>
+        <translation>Lesezeichen</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="273"/>
+        <source>Json document {} opened</source>
+        <translation>Json-Dokument {} wurde geöffnet</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="277"/>
+        <source>Unable to parse Json document from {}: {}</source>
+        <translation>Json-Dokument aus {} konnte nicht geparst werden: {}</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="332"/>
+        <source>Add bookmark</source>
+        <translation>Lesezeichen hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="jsonviewer/jsonviewer.py" line="350"/>
+        <source>Delete bookmark</source>
+        <translation>Lesezeichen löschen</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.py" line="67"/>
+        <source>Open Document</source>
+        <translation>Dokument öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="78"/>
+        <source>File {} could not be opened</source>
+        <translation>Datei {} konnte nicht geöffnet werden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="90"/>
+        <source>File {} can&apos;t be opened.</source>
+        <translation>Datei {} kann nicht geöffnet werden.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="118"/>
+        <source>About Document Viewer Demo</source>
+        <translation>Über Dokumentenanzeige-Demo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Document Viewer Demo</source>
+        <translation>Dokumentenanzeige-Demo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>Pages</source>
+        <translation>Seiten</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="47"/>
+        <source>Bookmarks</source>
+        <translation>Lesezeichen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="96"/>
+        <source>&amp;File</source>
+        <translation>&amp;Datei</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <source>ToolBar</source>
+        <oldsource>toolBar</oldsource>
+        <translation>Werkzeugleiste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="137"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="140"/>
+        <source>Ctrl+O</source>
+        <translation>Strg+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <source>About Document Viewer</source>
+        <translation>Über Dokumentenanzeige</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="151"/>
+        <source>Show information about the Document Viewer deomo.</source>
+        <translation>Informationen über die Dokumentenanzeige-Demo anzeigen.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="154"/>
+        <source>Ctrl+H</source>
+        <translation>Strg+H</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>Forward</source>
+        <oldsource>actionForward</oldsource>
+        <translation>Vorwärts</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <source>One step forward</source>
+        <translation>Einen Schritt vorwärts</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="168"/>
+        <source>Right</source>
+        <translation>Rechts</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="176"/>
+        <source>Back</source>
+        <translation>Zurück</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="179"/>
+        <source>One step back</source>
+        <translation>Einen Schritt zurück</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="193"/>
+        <source>&amp;Print</source>
+        <translation>&amp;Drucken</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="196"/>
+        <source>Print current file</source>
+        <translation>Aktuelle Datei drucken</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="199"/>
+        <source>Ctrl+P</source>
+        <translation>Strg+P</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="209"/>
+        <source>About Qt</source>
+        <translation>Über Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="212"/>
+        <source>Show Qt license information</source>
+        <translation>Qt-Lizenzinformationen anzeigen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="215"/>
+        <source>Ctrl+I</source>
+        <translation>Strg+I</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="223"/>
+        <source>&amp;Recently opened...</source>
+        <translation>&amp;Zuletzt geöffnet...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="226"/>
+        <source>Meta+R</source>
+        <translation>Meta+R</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="234"/>
+        <location filename="mainwindow.ui" line="237"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Beenden</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="240"/>
+        <source>Exits the application</source>
+        <translation>Beendet die Anwendung</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="243"/>
+        <source>Ctrl+Q</source>
+        <translation>Strg+Q</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="60"/>
+        <source>%n recent files</source>
+        <translation>letzte Datei</translation>
+    </message>
+</context>
+<context>
+    <name>PdfViewer</name>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="46"/>
+        <source>PDF</source>
+        <translation>PDF</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="66"/>
+        <source>Zoom in</source>
+        <translation>Vergrößern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="102"/>
+        <source>Pages</source>
+        <translation>Seiten</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="103"/>
+        <source>Bookmarks</source>
+        <translation>Lesezeichen</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="139"/>
+        <source>Opened PDF file {}</source>
+        <translation>Die PDF-Datei {} wurde geöffnet</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="67"/>
+        <source>Increase zoom level</source>
+        <translation>Vergrößern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="72"/>
+        <source>Zoom out</source>
+        <translation>Verkleinern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="73"/>
+        <source>Decrease zoom level</source>
+        <translation>Verkleinern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/pdfviewer.py" line="134"/>
+        <source>PDF Viewer</source>
+        <translation>PDF-Betrachter</translation>
+    </message>
+</context>
+<context>
+    <name>RecentFileMenu</name>
+    <message>
+        <location filename="recentfilemenu.py" line="25"/>
+        <source>&lt;no recent files&gt;</source>
+        <translation>&lt;keine zuletzt geöffneten Dateien&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TxtViewer</name>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="30"/>
+        <location filename="txtviewer/txtviewer.py" line="31"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="34"/>
+        <source>C&amp;ut</source>
+        <translation>&amp;Ausschneiden</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="36"/>
+        <source>Cut the current selection&apos;s contents to the clipboard</source>
+        <translation>Ausgewählten Inhalt in die Zwischenablage ausschneiden</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="43"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopieren</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="45"/>
+        <source>Copy the current selection&apos;s contents to the clipboard</source>
+        <translation>Ausgewählten Inhalt in die Zwischenablage kopieren</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="52"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Einfügen</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="54"/>
+        <source>Paste the clipboard&apos;s contents into the current selection</source>
+        <translation>Inhalt der Zwischenablage in die aktuelle Auswahl einfügen</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="94"/>
+        <source>Cannot read file {}:
+{}.</source>
+        <translation>Die Datei {} kann nicht gelesen werden:
+{}</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="107"/>
+        <source>File {} loaded.</source>
+        <translation>Datei {} geladen.</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="128"/>
+        <source>Cannot open file {} for writing:
+{}.</source>
+        <translation>Datei {} kann nicht zum Schreiben geöffnet werden:
+{}.</translation>
+    </message>
+    <message>
+        <location filename="txtviewer/txtviewer.py" line="135"/>
+        <source>File {} saved</source>
+        <translation>Datei {} gespeichert</translation>
+    </message>
+</context>
+<context>
+    <name>ViewerFactory</name>
+    <message>
+        <location filename="viewerfactory.py" line="98"/>
+        <source>Mime type {} not supported. Falling back to {}.</source>
+        <translation>MIME-Typ {} wird nicht unterstützt. Es wird auf {} zurückgegriffen.</translation>
+    </message>
+</context>
+<context>
+    <name>ZoomSelector</name>
+    <message>
+        <location filename="pdfviewer/zoomselector.py" line="21"/>
+        <location filename="pdfviewer/zoomselector.py" line="45"/>
+        <source>Fit Width</source>
+        <translation>An Breite anpassen</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer/zoomselector.py" line="22"/>
+        <location filename="pdfviewer/zoomselector.py" line="47"/>
+        <source>Fit Page</source>
+        <translation>An Seite anpassen</translation>
+    </message>
+</context>
+</TS>
