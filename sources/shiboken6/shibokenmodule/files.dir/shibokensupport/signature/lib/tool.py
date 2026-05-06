8de7e0023c64b9ee304900847baa4591d83c16d6
Ece Cinucen <ece.cinucen@qt.io>
2026-05-06 11:01:35 +0200

    type hints: Remove old code
    Removed the unused code which has no call anywhere in the repo.
    Its a copy from the Python2/3 compatibility library six. Since PySide6 does not support Python2 anymore, this function has no purpose.
    
    Change-Id: I2016eff0165c2285baf15b7d1e32d384108b03c9
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit a5d737c5ce891999c7ae8a46571e89d19f62c144)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/tool.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/lib/tool.py
@@ -87,23 +87,6 @@ def build_brace_pattern(level, separators):
     return pattern.replace("C", "{").replace("D", "}")
 
 
-# Copied from the six module:
-def with_metaclass(meta, *bases):
-    """Create a base class with a metaclass."""
-    # This requires a bit of explanation: the basic idea is to make a dummy
-    # metaclass for one level of class instantiation that replaces itself with
-    # the actual metaclass.
-    class metaclass(type):
-
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-
-        @classmethod
-        def __prepare__(cls, name, this_bases):
-            return meta.__prepare__(name, bases)
-    return type.__new__(metaclass, 'temporary_class', (), {})
-
-
 # A handy tool that shows the current line number and indents.
 def lno(level):
     lineno = currentframe().f_back.f_lineno
