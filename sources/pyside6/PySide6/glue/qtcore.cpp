760bd52f6cb50deb898f77a1d144a07184c9d7f0
James Clare <amuleteditor@gmail.com>
2026-05-25 10:57:09 +0100

    PySide6: Add exception logging to QThreadPool.start/tryStart
    There was no indication that an exception had occurred in the callback. This adds exception logging to make debugging easier.
    
    Change-Id: If8dd191d3d394da0d9d49e9c694cb5991b18bfd8
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    (cherry picked from commit a99affbacafe63cbd571edda45846557bd57ad1f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/pyside6/PySide6/glue/qtcore.cpp
+++ sources/pyside6/PySide6/glue/qtcore.cpp
@@ -1804,6 +1804,8 @@ auto cppCallback = [callable]()
     Shiboken::AutoDecRef arglist(PyTuple_New(0));
     Shiboken::AutoDecRef ret(PyObject_CallObject(callable, arglist));
     Py_DECREF(callable);
+    if (ret.isNull())
+        PyErr_Print();
 };
 // @snippet std-function-void-lambda
 
