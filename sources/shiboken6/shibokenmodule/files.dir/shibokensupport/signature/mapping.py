3e60a096e2b1e5df6f1403efe5db811240b68139
James Clare <amuleteditor@gmail.com>
2026-06-11 16:45:23 +0100

    Type hints: Fix QTreeWidgetItem.child return type hint
    Change-Id: I3f5ac146f027d0a61cf2e777c564f20ba1cddb10
    Reviewed-by: Ece Cinucen <ece.cinucen@qt.io>
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit 5b3fc4f303ecd5a46fb6efdd7ee9ac3d0a02a0c8)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/mapping.py
+++ sources/shiboken6/shibokenmodule/files.dir/shibokensupport/signature/mapping.py
@@ -799,6 +799,7 @@ missing_optional_return = {
     "PySide6.QtWidgets.QTreeWidget.itemAt",
     "PySide6.QtWidgets.QTreeWidget.takeTopLevelItem",
     "PySide6.QtWidgets.QTreeWidget.topLevelItem",
+    "PySide6.QtWidgets.QTreeWidgetItem.child",
     "PySide6.QtWidgets.QTreeWidgetItem.takeChild",
     "PySide6.QtWidgets.QTreeWidgetItem.parent",
     "PySide6.QtWidgets.QWidget.childAt",
