db3008a2c2fc29207476ddd2d690c45336e7be34
Simo Fält <simo.falt@qt.io>
2026-07-01 08:40:39 +0300

    CI: Initial axivion instructions
    Pick-to: 6.8
    Task-number: QTQAINFRA-5563
    Change-Id: I44c6990ad0a16e2ad52865ed8795ab11f2fd3ced
    Reviewed-by: Matti Paaso <matti.paaso@qt.io>
    (cherry picked from commit abe897166ac4ab29576d408713bd694dc3eed9c3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ coin/axivion/start_analysis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Copyright (C) 2024 The Qt Company Ltd
+#SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+$HOME/bauhaus-suite/setup.sh --non-interactive
+export PATH=/home/qt/bauhaus-suite/bin:$PATH
+export BAUHAUS_CONFIG=$(cd $(dirname $(readlink -f $0)) && pwd)
+export AXIVION_VERSION_NAME=$(git rev-parse HEAD)
+export EXCLUDE_FILES="src/3rdparty/*:/usr/include/*"
+export MODULE=$TESTED_MODULE_COIN
+export PACKAGE="Pyside"
+export IRNAME=build/$TESTED_MODULE_COIN.ir
+export INCLUDE_FILES=""
+axivion_ci "$@"
+
