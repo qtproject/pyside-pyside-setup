Stars1233/pyside-setup

Bulk fix some Axivion warnings
Fix some occurrences of Qt-RangeLoopDetach,
Qt-UnusedNonTrivialVariable, Qt-GlobalConstCharPointer.

Pick-to: 6.11
Task-number: PYSIDE-3375
Change-Id: I0aba90f08beffbaa4ae36f970dafca9894d252b9
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/pyside6/libpyside/pysideglobals_p.h
@@ -19,8 +19,8 @@ struct Globals // Per interpreter globals of libpyside
     int lastSelectedFeatureId = 0;
     PyTypeObject *qobjectType = nullptr;
     PyObject *emptyTuple = nullptr;
-    PyObject *pickleReduceFunc;
-    PyObject *pickleSafeLoadsFunc;
+    PyObject *pickleReduceFunc = nullptr;
+    PyObject *pickleSafeLoadsFunc = nullptr;
 };
 
 Globals *globals();

qtproject/pyside-pyside-setup

Qt-UnusedNonTrivialVariable
(cherry picked from commit ee37fc387c204d77c7c25a78c0cc4dc380ae3f65)

sources/pyside6/libpyside/pysideglobals_p.h
@@ -19,8 +19,8 @@ struct Globals // Per interpreter globals of libpyside
     int lastSelectedFeatureId = 0;
     PyTypeObject *qobjectType = nullptr;
     PyObject *emptyTuple = nullptr;
-    PyObject *pickleReduceFunc;
-    PyObject *pickleEvalFunc;
+    PyObject *pickleReduceFunc = nullptr;
+    PyObject *pickleEvalFunc = nullptr;
 };
 
 Globals *globals();