31e664941b1cf15eff029344b2b794d0856b8f3a
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-07-09 12:18:48 +0200

    shiboken6_generator: Fix blank lines following license header
    Pick-to: 6.11 6.8
    Task-number: PYSIDE-3394
    Change-Id: If41266155f3754a3e60e9fd9d12cba9510da6910
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/shiboken6_generator/generator/qtdoc/qtdocgenerator.h
+++ sources/shiboken6_generator/generator/qtdoc/qtdocgenerator.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
 #ifndef DOCGENERATOR_H
 #define DOCGENERATOR_H
 
