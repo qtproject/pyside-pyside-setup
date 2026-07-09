cf2b7dce98a2dc258a7823c71884049b9d4fda14
Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
2026-07-09 14:07:20 +0200

    Scenegraph customgeometry: Use @auto_properties and @effect
    - Property/Signal/Slot boilerplate for p1-p4 and segmentCount
      duplicated the same pattern five times across ~90 lines
    - Each setter previously called self.update() independently. @effect
      consolidates this into one reactive handler that fires whenever any
      control point or the segment count changes
    
    Task-number: PYSIDE-3302
    Task-number: PYSIDE-3301
    Change-Id: I9cb2be7b29732e9175d0da97b9d6f1bfaeb40f33
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    Reviewed-by: Ece Cinucen <ece.cinucen@qt.io>

--- examples/quick/scenegraph/scenegraph_customgeometry/main.py
+++ examples/quick/scenegraph/scenegraph_customgeometry/main.py
@@ -11,7 +11,8 @@ from PySide6.QtQuick import (QQuickView, QQuickItem, QSGNode, QSGGeometryNode,
                              QSGGeometry, QSGFlatColorMaterial)
 from PySide6.QtQml import QmlElement
 from PySide6.QtGui import QGuiApplication, QColor
-from PySide6.QtCore import (QPointF, QUrl, Property, Signal, Slot)
+from PySide6.QtCore import QPointF, QUrl
+from PySide6.QtQmlFeatures import auto_properties, effect
 
 # To be used on the @QmlElement decorator
 # (QML_IMPORT_MINOR_VERSION is optional)
@@ -19,82 +20,36 @@ QML_IMPORT_NAME = "CustomGeometry"
 QML_IMPORT_MAJOR_VERSION = 1
 
 
+# @auto_properties turns the plain self.p1/p2/p3/p4/segmentCount assignments
+# in __init__ into Q_PROPERTYs with change-notification signals, so QML can
+# bind to them. The @effect method centralises the self.update() call that
+# triggers a repaint whenever any control-point or segment-count changes.
 @QmlElement
+@auto_properties
 class BezierCurve(QQuickItem):
-    p1Changed = Signal()
-    p2Changed = Signal()
-    p3Changed = Signal()
-    p4Changed = Signal()
-    segmentCountChanged = Signal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self._p1 = QPointF(0, 0)
-        self._p2 = QPointF(1, 0)
-        self._p3 = QPointF(0, 1)
-        self._p4 = QPointF(1, 1)
-        self._segmentCount = 32
+        self.p1 = QPointF(0, 0)
+        self.p2 = QPointF(1, 0)
+        self.p3 = QPointF(0, 1)
+        self.p4 = QPointF(1, 1)
+        self.segmentCount = 32
 
         self._node = None
         self._geometry = None
         self.setFlag(QQuickItem.Flag.ItemHasContents, True)
 
-    def p1(self):
-        return self._p1
-
-    def p2(self):
-        return self._p2
-
-    def p3(self):
-        return self._p3
-
-    def p4(self):
-        return self._p4
-
-    def segmentCount(self):
-        return self._segmentCount
-
-    @Slot(QPointF)
-    def setP1(self, p):
-        if p != self._p1:
-            self._p1 = p
-            self.p1Changed.emit()
-            self.update()
-
-    @Slot(QPointF)
-    def setP2(self, p):
-        if p != self._p2:
-            self._p2 = p
-            self.p2Changed.emit()
-            self.update()
-
-    @Slot(QPointF)
-    def setP3(self, p):
-        if p != self._p3:
-            self._p3 = p
-            self.p3Changed.emit()
-            self.update()
-
-    @Slot(QPointF)
-    def setP4(self, p):
-        if p != self._p4:
-            self._p4 = p
-            self.p4Changed.emit()
-            self.update()
-
-    @Slot(int)
-    def setSegmentCount(self, p):
-        if p != self._segmentCount:
-            self._segmentCount = p
-            self.segmentCountChanged.emit()
-            self.update()
+    @effect("p1", "p2", "p3", "p4", "segmentCount")
+    def _on_geometry_changed(self):
+        self.update()
 
     def updatePaintNode(self, oldNode, updatePaintNodeData):
         self._node = oldNode
         if not self._node:
             self._default_attributes = QSGGeometry.defaultAttributes_Point2D()
-            self._geometry = QSGGeometry(self._default_attributes, self._segmentCount)
+            self._geometry = QSGGeometry(self._default_attributes, self.segmentCount)
             self._geometry.setLineWidth(2)
             self._geometry.setDrawingMode(QSGGeometry.DrawingMode.DrawLineStrip)
 
@@ -107,33 +62,25 @@ class BezierCurve(QQuickItem):
             self._node.setFlag(QSGNode.Flag.OwnsMaterial)
         else:
             self._geometry = self._node.geometry()
-            self._geometry.allocate(self._segmentCount)
+            self._geometry.allocate(self.segmentCount)
 
         item_size = self.size()
         item_width = float(item_size.width())
         item_height = float(item_size.height())
         vertices = self._geometry.vertexDataAsPoint2D()
-        for i in range(self._segmentCount):
-            t = float(i) / float(self._segmentCount - 1)
+        for i in range(self.segmentCount):
+            t = float(i) / float(self.segmentCount - 1)
             inv_t = 1 - t
-            pos = ((inv_t * inv_t * inv_t * self._p1)
-                   + (3 * inv_t * inv_t * t * self._p2)
-                   + (3 * inv_t * t * t * self._p3)
-                   + (t * t * t * self._p4))
+            pos = ((inv_t * inv_t * inv_t * self.p1)
+                   + (3 * inv_t * inv_t * t * self.p2)
+                   + (3 * inv_t * t * t * self.p3)
+                   + (t * t * t * self.p4))
             vertices[i].set(pos.x() * item_width, pos.y() * item_height)
 
         self._geometry.setVertexDataAsPoint2D(vertices)
 
         self._node.markDirty(QSGNode.DirtyStateBit.DirtyGeometry)
         return self._node
-
-    p1 = Property(QPointF, p1, setP1, notify=p1Changed)
-    p2 = Property(QPointF, p2, setP2, notify=p2Changed)
-    p3 = Property(QPointF, p3, setP3, notify=p3Changed)
-    p4 = Property(QPointF, p4, setP4, notify=p4Changed)
-
-    segmentCount = Property(int, segmentCount, setSegmentCount,
-                            notify=segmentCountChanged)
 
 
 if __name__ == "__main__":
