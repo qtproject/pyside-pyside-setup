71451f6d4c162047f2fa534c40be3d3d54764a7f
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-07-09 12:22:10 +0200

    shiboken6_generator: Mark as security-significant
    A few risks were identified, but none of them justifies
    a higher level. Use default.
    
    QUIP: 23
    Task-number: PYSIDE-3381
    Fixes: PYSIDE-3394
    Pick-to: 6.8
    Change-Id: I0f1297e93b49eeb1bfc23b3aec7b2d1c06f07f63
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
    (cherry picked from commit cb4a152ab23d5c9cff90560aa9934e2d0fe3a66f)

--- sources/shiboken6_generator/ApiExtractor/clangparser/clang_typedefs.h
+++ sources/shiboken6_generator/ApiExtractor/clangparser/clang_typedefs.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2026 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:significant reason:default
 
 #ifndef CLANG_TYPEDEFS_H
 #define CLANG_TYPEDEFS_H
--- sources/shiboken6_generator/ApiExtractor/parser/codemodel.h
+++ sources/shiboken6_generator/ApiExtractor/parser/codemodel.h
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2002-2005 Roberto Raggi <roberto@kdevelop.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+// Qt-Security score:significant reason:default
 
 #ifndef CODEMODEL_H
 #define CODEMODEL_H
