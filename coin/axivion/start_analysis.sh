8ca57220e377c447baee8e31c3ff73024510c272
Simo Fält <simo.falt@qt.io>
2026-08-28 09:33:34 +0300

    Scan only "Qt-Security score:critical" tagged files with Axivion
    Change-Id: I384de5d8f7db90c1f39761d1e72b167228d34697
    Reviewed-by: Matti Paaso <matti.paaso@qt.io>

--- coin/axivion/start_analysis.sh
+++ coin/axivion/start_analysis.sh
@@ -10,6 +10,20 @@ export EXCLUDE_FILES="build/*:/usr/include/*"
 export MODULE=$TESTED_MODULE_COIN
 export PACKAGE="Pyside"
 export IRNAME=build/$TESTED_MODULE_COIN.ir
-export INCLUDE_FILES=""
+
+ROOT_DIR=sources
+MAGIC="Qt-Security score:critical"
+INCLUDE_FILES=""
+while IFS= read -r file; do
+    if [ -z "$INCLUDE_FILES" ]; then
+        INCLUDE_FILES="$file"
+    else
+        INCLUDE_FILES="$INCLUDE_FILES:$file"
+    fi
+done <<EOF
+$(grep -rl "$MAGIC" "$ROOT_DIR")
+EOF
+
+export INCLUDE_FILES
 axivion_ci "$@"
 
