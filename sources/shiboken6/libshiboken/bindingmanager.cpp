ctismer/pyside6-freethreading

Take a reference at every hand-written wrapper lookup
Every place that reads the wrapper map by hand now asks for a
reference instead of the borrowed one the map holds: the virtual call
lookup in libshiboken, nine sites in libpyside, four more in
libshiboken, six glue snippets and the two custom widget plugins.

Most were the same shape - look up, then Py_INCREF - and several
spelled the dance out with a matching Py_DECREF around the work, which
the reference now does by itself. Three are worth naming.
Sbk_GetPyOverride() tested Py_REFCNT for zero after the lookup, two
steps where one is needed, and its thread state moves ahead of the
lookup because a reference count is bookkeeping that lives there; the
negative case keeps its old cost through hasWrapper().
getWrapperForQObject() incremented the borrow and returned it, on the
path every QObject takes. addMetaMethod() passed the result to
Object::hasCppWrapper() without a null check, which that function does
not do either.

The metacall entry points and the qgraphicsscene-clear snippet changed
slightly beyond the mechanical part: an empty result now also means
"the wrapper is already being deallocated", and a refcount threshold
moves by one because the lookup itself holds a reference.

Task-number: PYSIDE-2221
Change-Id: I1e96f9df5cd7bfbe8500d6afff5b87d76ea0002c

sources/shiboken6/libshiboken/bindingmanager.cpp
@@ -485,8 +485,8 @@ WrapperRef BindingManager::acquireWrapper(const void *cptr, PyTypeObject *typeOb
     }
     return {};
 }
+#endif
 
-#endif // Py_GIL_DISABLED
 SbkObject *BindingManager::retrieveWrapper(const void *cptr) const
 {
     std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
@@ -586,6 +586,25 @@ PyTypeObject *BindingManager::resolveType(void **cptr, PyTypeObject *type)
     return result.first != nullptr ? result.first : type;
 }
 
+#ifdef Py_GIL_DISABLED
+std::vector<WrapperRef> BindingManager::getAllPyObjects()
+{
+    std::vector<WrapperRef> result;
+    // Multiple inheritance registers one wrapper under several C++ pointers,
+    // so the same wrapper can be met more than once. The set the build with a
+    // GIL returns does that for free.
+    std::set<const SbkObject *> seen;
+    std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
+    for (const auto &entry : m_d->wrapperMapper) {
+        // Taking a reference under the lock is allowed - it runs no Python.
+        auto ref = WrapperRef::fromOwned(entry.second.acquire());
+        if (ref.isNull() || !seen.insert(ref.object()).second)
+            continue;   // already deallocating, or already collected
+        result.push_back(std::move(ref));
+    }
+    return result;
+}
+#else
 std::set<PyObject *> BindingManager::getAllPyObjects()
 {
     std::set<PyObject *> pyObjects;
@@ -603,12 +622,22 @@ std::set<PyObject *> BindingManager::getAllPyObjects()
 
     return pyObjects;
 }
+#endif
 
 void BindingManager::visitAllPyObjects(ObjectVisitor visitor, void *data)
 {
-    // The map has its own lock because C++ touches it without a thread state -
-    // releaseWrapper() runs from destructors. This was the one place that read
-    // it without taking that lock, both for the copy and for the check below.
+#ifdef Py_GIL_DISABLED
+    // Collect first, visit afterwards. The visitor runs Python and C++
+    // destructors, which take this lock again and mutate the map, so it must
+    // not run underneath it - and the references taken here are what keeps
+    // the collected wrappers alive until their turn comes.
+    std::vector<WrapperRef> wrappers = getAllPyObjects();
+    for (const auto &wrapper : wrappers)
+        visitor(wrapper.object(), data);
+#else
+    // The map has its own lock because C++ reaches it without a thread state -
+    // releaseWrapper() runs from destructors. The visitor stays outside it: it
+    // runs Python and C++ destructors, which take it again and mutate the map.
     WrapperMap copy;
     {
         std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
@@ -625,11 +654,10 @@ void BindingManager::visitAllPyObjects(ObjectVisitor visitor, void *data)
             std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
             present = m_d->findSbkObject(p.first, o) != m_d->wrapperMapper.cend();
         }
-        // Outside the lock: the visitor runs Python and C++ destructors, which
-        // take it again and mutate the map.
         if (present)
             visitor(o, data);
     }
+#endif
 }
 
 bool BindingManager::dumpTypeGraph(const char *fileName) const

sources/shiboken6/libshiboken/bindingmanager.h
@@ -14,6 +14,9 @@
 
 #include <set>
 #include <utility>
+#ifdef Py_GIL_DISABLED
+#  include <vector>
+#endif
 
 struct SbkObject;
 
@@ -48,6 +51,7 @@ public:
 
     void runDeletionInMainThread();
     void addToDeletionInMainThread(const DestructorEntry &);
+#ifdef Py_GIL_DISABLED
 
 #ifdef Py_GIL_DISABLED
     /// Look up a wrapper and take a reference to it in one step. Returns an
@@ -56,6 +60,7 @@ public:
     /// sites should use; see sbkwrapperref.h for why.
     [[nodiscard]] WrapperRef acquireWrapper(const void *cptr, PyTypeObject *typeObject) const;
     [[nodiscard]] WrapperRef acquireWrapper(const void *cptr) const;
+#endif
 
 #endif // Py_GIL_DISABLED
     /// \deprecated Hands out the borrowed reference the map holds, which the
@@ -83,7 +88,17 @@ public:
      */
     [[deprecated]] PyTypeObject *resolveType(void **cptr, PyTypeObject *type);
 
+#ifdef Py_GIL_DISABLED
+    /// Every live wrapper, each with a reference taken. Wrappers that are
+    /// already being deallocated are left out rather than handed over: the
+    /// caller could not tell them apart, and incrementing one afterwards is
+    /// the resurrection this class exists to prevent.
+#endif
+#ifdef Py_GIL_DISABLED
+    std::vector<WrapperRef> getAllPyObjects();
+#else
     std::set<PyObject *> getAllPyObjects();
+#endif
 
     /**
      * Calls the function \p visitor for each object registered on binding manager.

qtproject/pyside-pyside-setup

Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/shiboken6/libshiboken/bindingmanager.cpp
@@ -522,8 +522,8 @@ AcquiredWrapper BindingManager::registerWrapperUnlessPresent(SbkObject *pyObj, v
     m_d->assignWrapper(pyObj, cptr, d->mi_offsets);
     return {};
 }
-
 #endif // Py_GIL_DISABLED
+
 SbkObject *BindingManager::retrieveWrapper(const void *cptr) const
 {
     std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
@@ -623,6 +623,25 @@ PyTypeObject *BindingManager::resolveType(void **cptr, PyTypeObject *type)
     return result.first != nullptr ? result.first : type;
 }
 
+#ifdef Py_GIL_DISABLED
+std::vector<AcquiredWrapper> BindingManager::getAllPyObjects()
+{
+    std::vector<AcquiredWrapper> result;
+    // Multiple inheritance registers one wrapper under several C++ pointers,
+    // so the same wrapper can be met more than once. The set the build with a
+    // GIL returns does that for free.
+    std::set<const SbkObject *> seen;
+    std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
+    for (const auto &entry : m_d->wrapperMapper) {
+        // Taking a reference under the lock is allowed - it runs no Python.
+        auto ref = AcquiredWrapper::fromOwned(entry.second.acquire());
+        if (ref.isNull() || !seen.insert(ref.object()).second)
+            continue;   // already deallocating, or already collected
+        result.push_back(std::move(ref));
+    }
+    return result;
+}
+#else // Py_GIL_DISABLED
 std::set<PyObject *> BindingManager::getAllPyObjects()
 {
     std::set<PyObject *> pyObjects;
@@ -640,12 +659,22 @@ std::set<PyObject *> BindingManager::getAllPyObjects()
 
     return pyObjects;
 }
+#endif // Py_GIL_DISABLED
 
 void BindingManager::visitAllPyObjects(ObjectVisitor visitor, void *data)
 {
-    // The map has its own lock because C++ touches it without a thread state -
-    // releaseWrapper() runs from destructors. This was the one place that read
-    // it without taking that lock, both for the copy and for the check below.
+#ifdef Py_GIL_DISABLED
+    // Collect first, visit afterwards. The visitor runs Python and C++
+    // destructors, which take this lock again and mutate the map, so it must
+    // not run underneath it - and the references taken here are what keeps
+    // the collected wrappers alive until their turn comes.
+    std::vector<AcquiredWrapper> wrappers = getAllPyObjects();
+    for (const auto &wrapper : wrappers)
+        visitor(wrapper.object(), data);
+#else
+    // The map has its own lock because C++ reaches it without a thread state -
+    // releaseWrapper() runs from destructors. The visitor stays outside it: it
+    // runs Python and C++ destructors, which take it again and mutate the map.
     WrapperMap copy;
     {
         std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
@@ -662,11 +691,10 @@ void BindingManager::visitAllPyObjects(ObjectVisitor visitor, void *data)
             std::lock_guard<std::recursive_mutex> guard(m_d->wrapperMapLock);
             present = m_d->findSbkObject(p.first, o) != m_d->wrapperMapper.cend();
         }
-        // Outside the lock: the visitor runs Python and C++ destructors, which
-        // take it again and mutate the map.
         if (present)
             visitor(o, data);
     }
+#endif // Py_GIL_DISABLED
 }
 
 bool BindingManager::dumpTypeGraph(const char *fileName) const

sources/shiboken6/libshiboken/bindingmanager.h
@@ -14,6 +14,7 @@
 
 #include <set>
 #include <utility>
+#include <vector>
 
 struct SbkObject;
 
@@ -68,8 +69,8 @@ public:
     /// promises true.
     [[nodiscard]] AcquiredWrapper registerWrapperUnlessPresent(SbkObject *pyObj, void *cptr,
                                                                PyTypeObject *typeObject);
-
 #endif // Py_GIL_DISABLED
+
     /// \deprecated Hands out the borrowed reference the map holds, which the
     /// caller cannot safely increment. Being replaced by acquireWrapper() one
     /// call site at a time; this declaration goes away with the last of them.
@@ -95,7 +96,15 @@ public:
      */
     [[deprecated]] PyTypeObject *resolveType(void **cptr, PyTypeObject *type);
 
+#ifdef Py_GIL_DISABLED
+    /// Every live wrapper, each with a reference taken. Wrappers that are
+    /// already being deallocated are left out rather than handed over: the
+    /// caller could not tell them apart, and incrementing one afterwards is
+    /// the resurrection this class exists to prevent.
+    std::vector<AcquiredWrapper> getAllPyObjects();
+#else
     std::set<PyObject *> getAllPyObjects();
+#endif
 
     /**
      * Calls the function \p visitor for each object registered on binding manager.